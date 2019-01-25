b6e13a1133842d4b736fa48cfd0776b76bbdb0c6
Sachin Singh <ssingh259@sapient.com>
2019-01-25 17:11:44 +0530

    Created sample components

--- tetrapak-customerhub/ui.clickdummy/src/main/content/jcr_root/clickdummy/source/templates/components/HelloWorldHandlebars/v1/HelloWorldHandlebars/HelloWorldHandlebars.js
+++ tetrapak-customerhub/ui.clickdummy/src/main/content/jcr_root/clickdummy/source/templates/components/HelloWorldHandlebars/v1/HelloWorldHandlebars/HelloWorldHandlebars.js
@@ -1,5 +1,30 @@
+import $ from 'jquery';
+import { $body } from '../../../../../scripts/utils/commonSelectors';
+
 class HelloWorldHandlebars {
-  init() {/*TODO*/}
+  constructor({ templates }) {
+    this.templates = templates;
+  }
+  cache = {};
+  init() {
+    this.initCache();
+    this.bindEvents();
+  }
+  initCache() {
+    this.cache.$dynamicText = $('.js-dynamic-text');
+    this.cache.$btn = $('.js-cuhu__btn');
+    this.cache.$helloWorldForm = $('.js-helloworld-form');
+  }
+  bindEvents() {
+    this.cache.$btn.on('click', this.loadForm);
+    $body.on('input', '.js-textfield', this.changeText);
+  }
+  loadForm = () => {
+    this.cache.$helloWorldForm.html(this.templates.changeTextForm()).find('.js-textfield').val(this.cache.$dynamicText.text());
+  }
+  changeText = (e) => {
+    this.cache.$dynamicText.text($(e.target).val());
+  }
 }
 
 export default HelloWorldHandlebars;
