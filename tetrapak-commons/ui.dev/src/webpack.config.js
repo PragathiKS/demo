9f551b5a95e46eaab3a3e5ac0decd42ede364370
Sachin Singh <ssingh259@sapient.com>
2019-01-27 13:29:07 +0530

    Common setup in progress

---
+++ tetrapak-commons/ui.dev/src/webpack.config.js
@@ -0,0 +1,113 @@
+const config = require("./config").webpack;
+const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+// Resolve entry points
+const entryPoints = (function () {
+  let entryPoints = {};
+  Object.keys(config.entry).forEach(key => {
+    entryPoints[key] = path.resolve(__dirname, config.entry[key]);
+  });
+  return entryPoints;
+}());
+
+const componentGroups = config.componentGroups;
+
+// Resolve cache groups
+const cacheGroups = (function () {
+  let cacheGroups = config.cacheGroups;
+  Object.keys(cacheGroups).forEach((cacheGroup) => {
+    let currentGroup = cacheGroups[cacheGroup];
+    if (currentGroup.modifiers) {
+      currentGroup.test = new RegExp(currentGroup.test, currentGroup.modifiers);
+    } else if (currentGroup.testMultiple) {
+      currentGroup.test = function (module) {
+        if (module.resource) {
+          return !!componentGroups[cacheGroup].filter(path => module.resource.includes(path)).length;
+        }
+        return false;
+      }
+    } else {
+      currentGroup.test = new RegExp(currentGroup.test);
+    }
+    delete currentGroup.modifiers;
+    delete currentGroup.testMultiple;
+  });
+  return cacheGroups;
+}());
+
+module.exports = {
+  output: {
+    filename: config.filePath,
+    chunkFilename: config.chunkFilePath,
+    path: path.resolve(__dirname, config.root),
+    publicPath: '/'
+  },
+  mode: config.modes.prod,
+  entry: entryPoints,
+  devtool: '(none)',
+  optimization: {
+    splitChunks: {
+      cacheGroups: cacheGroups
+    },
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          output: {
+            comments: false
+          }
+        }
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sc|sa|c)ss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCSSExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /(\.js|\.jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          "babel-loader"
+        ]
+      },
+      {
+        test: /\.hbs$/,
+        exclude: /node_modules/,
+        loader: "handlebars-loader",
+        options: {
+          helperDirs: [path.join(__dirname, 'source/scripts/helpers')],
+          precompileOptions: {
+            knownHelpersOnly: false
+          }
+        }
+      }
+    ]
+  },
+  stats: {
+    warnings: false
+  },
+  plugins: [
+    new CleanPlugin([
+      config.clean
+    ]),
+    new MiniCSSExtractPlugin({
+      filename: config.cssPath,
+      chunkFilename: config.cssChunkPath
+    })
+  ]
+}
