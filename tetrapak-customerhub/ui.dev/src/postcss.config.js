a13073bfc5e5a24b256580e2c3cf8f2c4622bbc0
Sachin Singh <ssingh259@sapient.com>
2019-02-14 16:30:42 +0530

    Page layout in progress

--- tetrapak-customerhub/ui.dev/src/postcss.config.js
+++ tetrapak-customerhub/ui.dev/src/postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: [
     require('precss'),
     require('autoprefixer')({
-      browsers: ['>1%', 'last 2 versions']
+      browsers: ['>1%', 'last 2 versions', 'not ie <= 10']
     })
   ]
 }
