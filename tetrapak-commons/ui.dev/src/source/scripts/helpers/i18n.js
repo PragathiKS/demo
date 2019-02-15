242805e2742f080d73f15eba1652cc3e42e4ce58
Sachin Singh <ssingh259@sapient.com>
2019-02-15 23:39:34 +0530

    Added new helpers

---
+++ tetrapak-commons/ui.dev/src/source/scripts/helpers/i18n.js
@@ -0,0 +1,17 @@
+import Handlebars from 'handlebars';
+import { getI18n } from '../common/common';
+
+export default function (options) {
+  const key = options.hash.key;
+  const replaceList = options.hash.replaceList;
+  let replaceListArr = [];
+  if (typeof replaceList === 'string') {
+    replaceListArr = replaceList.split(',').map(replacement => Handlebars.escapeExpression(replacement.trim()));
+  } else if (Array.isArray(replaceList)) {
+    replaceListArr = replaceList;
+  }
+  if (replaceListArr.length) {
+    return new Handlebars.SafeString(getI18n(key, replaceListArr, options.hash));
+  }
+  return new Handlebars.SafeString(getI18n(key, options.hash));
+}
---
+++ tetrapak-commons/ui.dev/src/source/scripts/helpers/if_notin.js
@@ -0,0 +1,15 @@
+import Handlebars from 'handlebars';
+import 'core-js/features/array/includes';
+
+export default function (available, value, opts) {
+  let compareValues = [];
+  if (typeof available === 'string') {
+    compareValues = available.split(',').map(value => Handlebars.escapeExpression(value.trim()));
+  } else if (Array.isArray(available)) {
+    compareValues = available;
+  }
+  if (!compareValues.includes(value)) {
+    return opts.fn(this);
+  }
+  return opts.inverse(this);
+}
