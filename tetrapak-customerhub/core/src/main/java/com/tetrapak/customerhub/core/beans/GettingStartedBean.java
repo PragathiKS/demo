964bff453fdea0710476defaa782cdb8a633271f
Nitin <nkumar64@sapient.com>
2019-02-19 18:12:28 +0530

    model for getting started component

---
+++ tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/beans/GettingStartedBean.java
@@ -0,0 +1,40 @@
+package com.tetrapak.customerhub.core.beans;
+
+public class GettingStartedBean {
+    private String imagePath;
+    private String imageAltI18n;
+    private String titleI18n;
+    private String descriptionI18n;
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getImageAltI18n() {
+        return imageAltI18n;
+    }
+
+    public void setImageAltI18n(String imageAltI18n) {
+        this.imageAltI18n = imageAltI18n;
+    }
+
+    public String getTitleI18n() {
+        return titleI18n;
+    }
+
+    public void setTitleI18n(String titleI18n) {
+        this.titleI18n = titleI18n;
+    }
+
+    public String getDescriptionI18n() {
+        return descriptionI18n;
+    }
+
+    public void setDescriptionI18n(String descriptionI18n) {
+        this.descriptionI18n = descriptionI18n;
+    }
+}
