3d2cda07f8a68b7120c07000e1f0edf45db8bd33
Abhinit Bhatnagar <abhinit.bhatnagar@3sharecorp.com>
2019-02-20 13:17:15 +0530

    Initial files for Footer component

---
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/footer/Footer.js
@@ -0,0 +1,16 @@
+class Footer {
+  cache = {};
+  initCache() {
+    /* Initialize cache here */
+  }
+  bindEvents() {
+    /* Bind jQuery events here */
+  }
+  init() {
+    /* Mandatory method */
+    this.initCache();
+    this.bindEvents();
+  }
+}
+
+export default Footer;
