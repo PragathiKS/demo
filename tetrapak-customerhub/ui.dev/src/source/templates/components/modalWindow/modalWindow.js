bfb145250b8de744617b51108583648e5a0aa34b
mn0851@agcompany.net <mn0851@agcompany.net>
2019-02-20 13:55:46 +0530

    Local Storage and Get Started Button

--- tetrapak-customerhub/ui.dev/src/source/templates/components/modalWindow/modalWindow.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/modalWindow/modalWindow.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'slick-carousel';
+import { storageUtil } from '../../../scripts/common/common';
 
 class modalWindow {
   cache = {};
@@ -9,27 +10,48 @@ class modalWindow {
   }
   bindEvents() {
     /* Bind jQuery events here */
-  }
-  init() {
-    /* Mandatory method */
-    this.initCache();
-    this.bindEvents();
-    $('#myModal').modal();
-    $('.single-item').slick({
-      dots: true,
-      appendDots: $('.slider-dots'),
-      prevArrow: false,
-      nextArrow: false,
-      customPaging: function () {
-        return '<button class="tp-dot"></button>'; // Remove button, customize content of "li"
-      }
-    });
     $('.slick-next').on('click', function () {
       $('.single-item').slick('slickNext');
     });
-    $('.icon-Close').on('click', function () {
+
+    $('.icon-Close, .get-started-btn').on('click', function () {
       $('#myModal').modal('toggle');
+      storageUtil.set('introScreen', true);
     });
+
+    $('.single-item').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+      console.log(slick.$slides.length +','+ currentSlide + ',' + nextSlide); // eslint-disable-line
+      if (slick.$slides.length === nextSlide+1) {
+        $('.slick-next').hide();
+        $('.get-started-btn').show();
+      } else {
+        $('.slick-next').show();
+        $('.get-started-btn').hide(); 
+      }
+    });
+  }
+  init() {
+    let introScreen = storageUtil.get('introScreen');
+
+    /* Mandatory method */
+    this.initCache();
+      
+    if (!introScreen) {
+      this.bindEvents();
+
+      $('#myModal').modal();
+
+      $('.single-item').slick({
+        dots: true,
+        infinite: false,
+        appendDots: $('.slider-dots'),
+        prevArrow: false,
+        nextArrow: false,
+        customPaging: function () {
+          return '<button class="tp-dot"></button>'; // Remove button, customize content of "li"
+        }
+      });
+    }
   }
 }
 
