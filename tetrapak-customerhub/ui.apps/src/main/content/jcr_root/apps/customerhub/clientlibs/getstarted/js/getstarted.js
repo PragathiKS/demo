f336d7817d07b9e0de4c08e5cb92c75ad538ca1b
Dhirendra Tiwari <dhirendra.tiwari@publicissapient.com>
2019-02-25 18:31:14 +0530

    Updated as per review comments

--- tetrapak-customerhub/ui.apps/src/main/content/jcr_root/apps/customerhub/clientlibs/getstarted/js/getstarted.js
+++ tetrapak-customerhub/ui.apps/src/main/content/jcr_root/apps/customerhub/clientlibs/getstarted/js/getstarted.js
@@ -1,23 +1,25 @@
- $(document).on("dialog-ready", function () {
-    var maxMultifieldAllowed = $("coral-multifield").attr("data-validation");
-     if ((typeof maxMultifieldAllowed  == "undefined") || (maxMultifieldAllowed.indexOf("multifield-max") == -1)){
+ var jqueryObj = $;
+ jqueryObj(document).on("dialog-ready", function () {
+    var maxMultifieldAllowed = jqueryObj("coral-multifield").attr("data-validation");
+
+     if ((typeof maxMultifieldAllowed  === "undefined") || (maxMultifieldAllowed.indexOf("multifield-max") === -1)){
          return;
      } 
-    if(parseInt($('.coral3-Multifield-item').size()) == parseInt(maxMultifieldAllowed.replace("multifield-max-", ""))){
-	$('button[coral-multifield-add]').hide();
+    if(parseInt(jqueryObj('.coral3-Multifield-item').size()) === parseInt(maxMultifieldAllowed.replace("multifield-max-", ""))){
+		 jqueryObj('button[coral-multifield-add]').attr('disabled',true);
     	}
     });
-$(window).adaptTo("foundation-registry").register("foundation.validation.validator", {
+jqueryObj(window).adaptTo("foundation-registry").register("foundation.validation.validator", {
   selector: "[data-foundation-validation^='multifield-max']",
   validate: function(el) {
     var validationName = el.getAttribute("data-validation");
-	if (typeof validationName  == "undefined"){
+	if (typeof validationName  === "undefined"){
          return;
      } 
-    if (parseInt(el.items.length) == parseInt(validationName.replace("multifield-max-", ""))) {
-        $('button[coral-multifield-add]').hide();
+    if (parseInt(el.items.length) === parseInt(validationName.replace("multifield-max-", ""))) {
+    	jqueryObj('button[coral-multifield-add]').attr('disabled',true);
      }else{
-         $('button[coral-multifield-add]').show();
+    	 jqueryObj('button[coral-multifield-add]').removeAttr("disabled");
      }
   }
 });
