ecbe4a36526d3eac220ae017cd39e937e7885a10
Dhirendra Tiwari <dhirendra.tiwari@publicissapient.com>
2019-02-26 10:25:45 +0530

    Updated as per review comment

--- tetrapak-customerhub/ui.apps/src/main/content/jcr_root/apps/customerhub/clientlibs/getstarted/js/getstarted.js
+++ tetrapak-customerhub/ui.apps/src/main/content/jcr_root/apps/customerhub/clientlibs/getstarted/js/getstarted.js
@@ -1,25 +1,27 @@
- var jqueryObj = $;
- jqueryObj(document).on("dialog-ready", function () {
-    var maxMultifieldAllowed = jqueryObj("coral-multifield").attr("data-validation");
+(function ($) {
+$(document).on("dialog-ready", function () {
+    var maxMultifieldAllowed = $("coral-multifield").attr("data-validation");
 
      if ((typeof maxMultifieldAllowed  === "undefined") || (maxMultifieldAllowed.indexOf("multifield-max") === -1)){
          return;
      } 
-    if(parseInt(jqueryObj('.coral3-Multifield-item').size()) === parseInt(maxMultifieldAllowed.replace("multifield-max-", ""))){
-		 jqueryObj('button[coral-multifield-add]').attr('disabled',true);
+    if(parseInt($('.coral3-Multifield-item').size()) === parseInt(maxMultifieldAllowed.replace("multifield-max-", ""))){
+		 $('button[coral-multifield-add]').attr('disabled',true);
     	}
     });
-jqueryObj(window).adaptTo("foundation-registry").register("foundation.validation.validator", {
+$(window).adaptTo("foundation-registry").register("foundation.validation.validator", {
   selector: "[data-foundation-validation^='multifield-max']",
   validate: function(el) {
     var validationName = el.getAttribute("data-validation");
 	if (typeof validationName  === "undefined"){
          return;
-     } 
+     }
     if (parseInt(el.items.length) === parseInt(validationName.replace("multifield-max-", ""))) {
-    	jqueryObj('button[coral-multifield-add]').attr('disabled',true);
+    	$('button[coral-multifield-add]').attr('disabled',true);
      }else{
-    	 jqueryObj('button[coral-multifield-add]').removeAttr("disabled");
+    	 $('button[coral-multifield-add]').removeAttr("disabled");
      }
   }
 });
+})(window.jQuery);
+
