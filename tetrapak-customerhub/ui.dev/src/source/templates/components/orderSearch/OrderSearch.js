fa3f94a92af09189efd79184fa2217a0e64f1860
mn0851@agcompany.net <mn0851@agcompany.net>
2019-03-01 15:15:40 +0530

    API call and HTML rendering

--- tetrapak-customerhub/ui.dev/src/source/templates/components/orderSearch/OrderSearch.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/orderSearch/OrderSearch.js
@@ -1,7 +1,13 @@
+import { render } from '../../../scripts/utils/render';
+
 class OrderSearch {
   cache = {};
+  constructor({ el }) {
+    this.root = $(el);
+  }
   initCache() {
     /* Initialize cache here */
+    this.cache.config = this.root.find('.order-search__config').text();
   }
   bindEvents() {
     /* Bind jQuery events here */
@@ -10,6 +16,20 @@ class OrderSearch {
     /* Mandatory method */
     this.initCache();
     this.bindEvents();
+
+    let config = JSON.parse(this.cache.config);
+
+    render.fn({
+      template: 'orderSearch',
+      url: config.apiURL,
+      target: '.order-search__form',
+      ajaxConfig: {
+        method: 'GET'
+      },
+      beforeRender(data) {
+        this.data = $.extend(data, config);
+      }
+    });
   }
 }
 
