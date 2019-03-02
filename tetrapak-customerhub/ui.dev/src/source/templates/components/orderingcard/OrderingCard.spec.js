9c99e9ea74059118787a563cdb0a3401456fc678
Sachin Singh <ssingh259@sapient.com>
2019-03-02 22:24:05 +0530

    Font file renaming

--- tetrapak-customerhub/ui.dev/src/source/templates/components/orderingcard/OrderingCard.spec.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/orderingcard/OrderingCard.spec.js
@@ -1,11 +1,11 @@
 import OrderingCard from './OrderingCard';
 import $ from 'jquery';
 import { ajaxWrapper } from '../../../scripts/utils/ajax';
+import { render } from '../../../scripts/utils/render';
 import orderingCardData from './data/orderingCardData.json';
 import orderingCardTemplate from '../../../test-templates-hbs/orderingcard.hbs';
 
 describe('OrderingCard', function () {
-
   before(function () {
     const jqRef = {};
     function ajaxResponse() {
@@ -16,14 +16,19 @@ describe('OrderingCard', function () {
     $(document.body).empty().html(orderingCardTemplate());
     this.orderingCard = new OrderingCard({ el: document.body });
     this.initSpy = sinon.spy(this.orderingCard, 'init');
+    this.renderSpy = sinon.spy(render, 'fn');
     this.ajaxStub = sinon.stub(ajaxWrapper, 'getXhrObj').yieldsTo('beforeSend', jqRef).returns(ajaxResponse());
     this.orderingCard.init();
   });
   after(function () {
     $(document.body).empty();
     this.initSpy.restore();
+    this.renderSpy.restore();
   });
   it('should initialize', function () {
     expect(this.orderingCard.init.called).to.be.true;
-  })
+  });
+  it('should render component on page load', function () {
+    expect(render.fn.called).to.be.true;
+  });
 });
