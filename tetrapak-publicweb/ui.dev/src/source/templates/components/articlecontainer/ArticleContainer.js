feb5143dd633e3e4d1df0d575bb9eeadf8b1b6ca
lpoveda <lucio.poveda@3sharecorp.com>
2019-03-06 22:41:26 +0100

    [TETRAP-1308] Adding FE changes

---
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/articlecontainer/ArticleContainer.js
@@ -0,0 +1,13 @@
+class ArticleContainer {
+  cache = {};
+  initCache() {
+  }
+  bindEvents() {
+  }
+  init() {
+    this.initCache();
+    this.bindEvents();
+  }
+}
+
+export default ArticleContainer;
