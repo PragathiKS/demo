166130c56165bf95dc4bcbe47b9f084df7e134e6
Manoj Varma <mn0851@agcompany.net>
2019-03-12 17:58:07 +0530

    Increased code coverage

--- tetrapak-customerhub/ui.dev/src/source/templates/components/ordersearch/OrderSearch.spec.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/ordersearch/OrderSearch.spec.js
@@ -1,20 +1,31 @@
 import $ from 'jquery';
 import OrderSearch from './OrderSearch';
+import orderSearchData from './data/orderSearchSummary.json';
+import { ajaxWrapper } from '../../../scripts/utils/ajax';
 import { render } from '../../../scripts/utils/render';
 import orderSearchTemplate from '../../../test-templates-hbs/ordersearch.hbs';
 
 describe('OrderSearch', function () {
+  const jqRef = {};
+  function ajaxResponse(response) {
+    const pr = $.Deferred();
+    pr.resolve(response, 'success', jqRef);
+    return pr.promise();
+  }
   before(function () {
     $(document.body).empty().html(orderSearchTemplate());
     this.orderSearch = new OrderSearch({ el: document.body });
     this.initSpy = sinon.spy(this.orderSearch, 'init');
     this.renderSpy = sinon.spy(render, 'fn');
+    this.ajaxStub = sinon.stub(ajaxWrapper, 'getXhrObj');
+    this.ajaxStub.yieldsTo('beforeSend', jqRef).returns(ajaxResponse(orderSearchData));
     this.orderSearch.init();
   });
   after(function () {
     $(document.body).empty();
     this.initSpy.restore();
     this.renderSpy.restore();
+    this.ajaxStub.restore();
   });
   it('should initialize', function () {
     expect(this.orderSearch.init.called).to.be.true;
