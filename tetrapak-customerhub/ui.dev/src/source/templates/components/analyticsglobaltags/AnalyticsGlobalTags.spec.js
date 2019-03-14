d587c80315b7294666880f0dfcbbc58bbd824926
Manoj Varma <mn0851@agcompany.net>
2019-03-14 15:18:51 +0530

    Analytics Global Tags

---
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/analyticsglobaltags/AnalyticsGlobalTags.spec.js
@@ -0,0 +1,9 @@
+import AnalyticsGlobalTags from './AnalyticsGlobalTags';
+
+describe('AnalyticsGlobalTags', function () {
+  before(function () {
+    this.analyticsGlobalTags = new AnalyticsGlobalTags({
+      el: document.body
+    });
+  });
+});
