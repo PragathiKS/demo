80d90ff6daa55d0e3e7f616a64c3120c55a27605
lpoveda <lucio.poveda@3sharecorp.com>
2019-03-18 12:26:00 +0100

    [TETRAP-2162] Injecting property

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/HeaderModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/HeaderModel.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.commons.json.JSONObject;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.Self;
@@ -17,7 +19,7 @@ import com.day.cq.commons.inherit.InheritanceValueMap;
 import com.tetrapak.publicweb.core.beans.NavigationLinkBean;
 import com.tetrapak.publicweb.core.utils.LinkUtils;
 
-@Model(adaptables = Resource.class)
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class HeaderModel {
 
 	private static final Logger log = LoggerFactory.getLogger(FooterModel.class);
@@ -26,14 +28,14 @@ public class HeaderModel {
 	private Resource resource;
 
 	private String loginTextI18n;
-	private Boolean enableHeroImage;
+	@Inject
+  private Boolean enableHeroImage;
 	private List<NavigationLinkBean> megaMenuLinksList = new ArrayList<>();
 
 	@PostConstruct
 	protected void init() {
 		InheritanceValueMap inheritanceValueMap = new HierarchyNodeInheritanceValueMap(resource);
 		loginTextI18n = inheritanceValueMap.getInherited("loginTextI18n", String.class);
-		enableHeroImage = inheritanceValueMap.getInherited("enableHeroImage", Boolean.class);
 		String[] megaMenuLinks = inheritanceValueMap.getInherited("megamenuLinks", String[].class);
 		LinkUtils.setMultifieldNavLinkItems(megaMenuLinks, megaMenuLinksList, log);
 	}
