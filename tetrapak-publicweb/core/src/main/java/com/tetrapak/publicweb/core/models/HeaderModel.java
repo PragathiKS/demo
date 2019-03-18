8c16c5867e475c09be60a7c5698e472a118de35f
lpoveda <lucio.poveda@3sharecorp.com>
2019-03-18 12:32:16 +0100

    [TETRAP-2162] Indentation dix

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/HeaderModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/HeaderModel.java
@@ -22,33 +22,33 @@ import com.tetrapak.publicweb.core.utils.LinkUtils;
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class HeaderModel {
 
-	private static final Logger log = LoggerFactory.getLogger(FooterModel.class);
+    private static final Logger log = LoggerFactory.getLogger(FooterModel.class);
 
-	@Self
-	private Resource resource;
+    @Self
+    private Resource resource;
 
-	private String loginTextI18n;
-	@Inject
-  private Boolean enableHeroImage;
-	private List<NavigationLinkBean> megaMenuLinksList = new ArrayList<>();
+    private String loginTextI18n;
+    @Inject
+    private Boolean enableHeroImage;
+    private List<NavigationLinkBean> megaMenuLinksList = new ArrayList<>();
 
-	@PostConstruct
-	protected void init() {
-		InheritanceValueMap inheritanceValueMap = new HierarchyNodeInheritanceValueMap(resource);
-		loginTextI18n = inheritanceValueMap.getInherited("loginTextI18n", String.class);
-		String[] megaMenuLinks = inheritanceValueMap.getInherited("megamenuLinks", String[].class);
-		LinkUtils.setMultifieldNavLinkItems(megaMenuLinks, megaMenuLinksList, log);
-	}
+    @PostConstruct
+    protected void init() {
+        InheritanceValueMap inheritanceValueMap = new HierarchyNodeInheritanceValueMap(resource);
+        loginTextI18n = inheritanceValueMap.getInherited("loginTextI18n", String.class);
+        String[] megaMenuLinks = inheritanceValueMap.getInherited("megamenuLinks", String[].class);
+        LinkUtils.setMultifieldNavLinkItems(megaMenuLinks, megaMenuLinksList, log);
+    }
 
-	public List<NavigationLinkBean> getMegaMenuLinksList() {
-		return megaMenuLinksList;
-	}
+    public List<NavigationLinkBean> getMegaMenuLinksList() {
+        return megaMenuLinksList;
+    }
 
-  public Boolean getEnableHeroImage() {
-    return enableHeroImage;
-  }
-	
-	public String getLoginTextI18n() {
-		return loginTextI18n;
-	}
+    public Boolean getEnableHeroImage() {
+        return enableHeroImage;
+    }
+
+    public String getLoginTextI18n() {
+        return loginTextI18n;
+    }
 }
