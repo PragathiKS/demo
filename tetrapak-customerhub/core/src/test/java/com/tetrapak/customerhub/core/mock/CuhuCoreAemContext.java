41f1d56944ba524ac31c5476ba55f5a3f9d7a866
Nitin <nitin.kumar1@publicissapient.com>
2019-03-20 08:20:51 +0530

    TETRAP-881 junit test cases for order search and ordering card model

--- tetrapak-customerhub/core/src/test/java/com/tetrapak/customerhub/core/mock/CuhuCoreAemContext.java
+++ tetrapak-customerhub/core/src/test/java/com/tetrapak/customerhub/core/mock/CuhuCoreAemContext.java
@@ -1,66 +1,71 @@
 package com.tetrapak.customerhub.core.mock;
 
-import java.io.IOException;
-import java.lang.reflect.Array;
-import java.util.List;
-import org.apache.commons.lang3.StringUtils;
-import org.apache.jackrabbit.api.security.user.UserManager;
-import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.testing.mock.sling.ResourceResolverType;
 import com.drew.lang.annotations.Nullable;
 import com.google.common.base.Function;
 import io.wcm.testing.mock.aem.junit.AemContext;
 import io.wcm.testing.mock.aem.junit.AemContextCallback;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.jackrabbit.api.security.user.UserManager;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.testing.mock.sling.ResourceResolverType;
+
+import java.util.List;
 
 public class CuhuCoreAemContext {
-    
+
     private CuhuCoreAemContext() {
-        
+
     }
-    
+
     public static AemContext getAemContext(String resourceJosnFileName, String resourcePath) {
         return new AemContext(new SetUpCallback<Object>(resourceJosnFileName, resourcePath),
                 ResourceResolverType.RESOURCERESOLVER_MOCK);
     }
-    
+
+    public static AemContext getAemContextWithJcrMock(String resourceJosnFileName, String resourcePath) {
+        return new AemContext(new SetUpCallback<Object>(resourceJosnFileName, resourcePath),
+                ResourceResolverType.JCR_MOCK, ResourceResolverType.RESOURCERESOLVER_MOCK);
+    }
+
     public static <T> AemContext getAemContext(String resourceJosnFileName, String resourcePath,
-            GenericServiceType<T> serviceType) {
+                                               GenericServiceType<T> serviceType) {
         return new AemContext(new SetUpCallback<T>(resourceJosnFileName, resourcePath, serviceType),
                 ResourceResolverType.RESOURCERESOLVER_MOCK);
     }
-    
+
     public static <T> AemContext getAemContext(String resourceJosnFileName, String resourcePath,
-            List<GenericServiceType<T>> serviceTypes) {
+                                               List<GenericServiceType<T>> serviceTypes) {
         return new AemContext(new SetUpCallback<T>(resourceJosnFileName, resourcePath, serviceTypes),
                 ResourceResolverType.RESOURCERESOLVER_MOCK);
     }
-    
+
     private static final class SetUpCallback<T> implements AemContextCallback {
-        
-        private String resourceJosnFileName = null;
-        private String resourcePath = null;
+
+        private String resourceJsonFileName;
+        private String resourcePath;
         GenericServiceType<T> serviceType = null;
         List<GenericServiceType<T>> serviceTypes = null;
-        
-        SetUpCallback(String resourceJosnFileName, String resourcePath) {
-            this.resourceJosnFileName = resourceJosnFileName;
+
+        SetUpCallback(String resourceJsonFileName, String resourcePath) {
+            this.resourceJsonFileName = resourceJsonFileName;
             this.resourcePath = resourcePath;
         }
-        
-        SetUpCallback(String resourceJosnFileName, String resourcePath, GenericServiceType<T> serviceType) {
-            this.resourceJosnFileName = resourceJosnFileName;
+
+        SetUpCallback(String resourceJsonFileName, String resourcePath, GenericServiceType<T> serviceType) {
+            this.resourceJsonFileName = resourceJsonFileName;
             this.resourcePath = resourcePath;
             this.serviceType = serviceType;
         }
-        
-        SetUpCallback(String resourceJosnFileName, String resourcePath, List<GenericServiceType<T>> serviceType) {
-            this.resourceJosnFileName = resourceJosnFileName;
+
+        SetUpCallback(String resourceJsonFileName, String resourcePath, List<GenericServiceType<T>> serviceType) {
+            this.resourceJsonFileName = resourceJsonFileName;
             this.resourcePath = resourcePath;
             this.serviceTypes = serviceType;
         }
-        
+
         @Override
-        public void execute(AemContext context) throws IOException {
+        public void execute(AemContext context) {
+
             // Register Adapter
             context.registerAdapter(ResourceResolver.class, UserManager.class,
                     new Function<ResourceResolver, UserManager>() {
@@ -70,20 +75,21 @@ public class CuhuCoreAemContext {
                             return new MockUserManager();
                         }
                     });
+
             //Register Single Service
             if (null != serviceType) {
                 context.registerService(serviceType.getClazzType(), serviceType.get());
             }
-            
+
             // Register multiple  Service
             if (null != serviceTypes && !serviceTypes.isEmpty()) {
                 serviceTypes.forEach(service -> context.registerService(service.getClazzType(), service.get()));
             }
-            
+
             // Register Sling Models
             context.addModelsForPackage("com.tetrapak.customerhub.core.models");
-            if (StringUtils.isNotBlank(resourceJosnFileName) && StringUtils.isNotBlank(resourcePath)) {
-                context.load().json("/" + resourceJosnFileName, resourcePath);
+            if (StringUtils.isNotBlank(resourceJsonFileName) && StringUtils.isNotBlank(resourcePath)) {
+                context.load().json("/" + resourceJsonFileName, resourcePath);
             }
         }
     }
