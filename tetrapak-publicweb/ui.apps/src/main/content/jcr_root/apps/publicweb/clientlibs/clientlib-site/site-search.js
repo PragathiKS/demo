daa00c1f627e4e1c03921723d10c2cdf93b54bb2
Abhinit Bhatnagar <abhinit.bhatnagar@3sharecorp.com>
2019-03-22 21:11:17 +0530

    TETRAP-2135 Added Search functionality, updated header with search config, added search result component

---
+++ tetrapak-publicweb/ui.apps/src/main/content/jcr_root/apps/publicweb/clientlibs/clientlib-site/site-search.js
@@ -0,0 +1,101 @@
+$(document).on('ready', function () {
+
+    var urlValues = getUrlVars();  
+
+    $('#f_search_inp').bind("enterKey",function(e){
+        var urlValue = window.location.href;
+        var queryTerm = $('#f_search_inp').val();
+
+        $("#f_search_inp").val("");              
+        var updatedURLAfterQueryTerm = updateQueryStringParameter(urlValue, "q", queryTerm);
+        
+        var state = {
+            "search": true
+        };    
+
+        if (!!(window.history && history.pushState)) {
+            e.preventDefault();
+            window.history.pushState(state, document.title, updatedURLAfterQueryTerm);
+        }
+
+        performSearch();
+    });
+
+    $('#f_search_inp').keyup(function(e){
+        if(e.keyCode == 13)
+        {
+          $(this).trigger("enterKey");
+        }
+    });
+
+    function performSearch() {
+        var searchRootPath = $("#search-data-rootpath").attr("data");
+        var searchResultsPath = $("#search-data-resultspath").attr("data");
+
+        var queryTerm = "";
+        var urlVals = getUrlVars(); 
+        var encodedQueryTerm = "";
+        
+        for (var i = 0; i < urlVals.length; i++) {  
+            var urlVal = urlVals[i];
+            if (urlVal === 'q'){
+                queryTerm = decodeURI(getUrlVars()[urlVal]);
+            } 
+        }
+        encodedQueryTerm = encodeURIComponent(queryTerm);
+
+        $.ajax({
+            url: '/bin/tetrapak/pw-search',
+            type: 'get',
+            async: false,
+            data: {
+                searchRootPath: searchRootPath,
+                searchResultsPath: searchResultsPath,
+                fulltextSearchTerm : encodedQueryTerm,
+            },
+            success: function(data) {               
+                        $('.search-results').html('');
+                        $.each(data, function(i, obj) {
+                            $('.search-results').append("<p><a href=\"" + obj.path + "\" >" + obj.title + "</a></p>");
+                    });
+
+                var totalResults = data.length;                
+                alert(totalResults + " results updated.");
+            }
+        });
+    };
+});
+
+//  Read a page's GET URL variables and return them as an associative array.
+function getUrlVars() {
+    var vars = [], hash;
+    if(window.location.href.indexOf("?") > -1) {
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++) {
+            hash = hashes[i].split('=');
+
+            if($.inArray(hash[0], vars)>-1) {
+                vars[hash[0]]+=","+hash[1];
+            } else {
+                vars.push(hash[0]);
+                vars[hash[0]] = hash[1];
+            }
+        }
+    }
+    return vars;
+}
+
+//  Send URL without Query String
+function getPathFromUrl(url) {
+    return url.split(/[?#]/)[0];
+}
+
+function updateQueryStringParameter(uri, key, value) {
+    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    if (uri.match(re)) {
+        return uri.replace(re, '$1' + key + "=" + value + '$2');
+    } else {
+        return uri + separator + key + "=" + value;
+    }
+}
