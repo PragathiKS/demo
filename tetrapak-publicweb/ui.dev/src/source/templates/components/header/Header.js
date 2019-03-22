d459f585a72d7675384ecf96579adcf6dc024fa5
lpoveda <lucio.poveda@3sharecorp.com>
2019-03-22 22:38:35 +0100

    [TETRAP_2135] adding FE for input search placeholder

---
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/header/Header.js
@@ -0,0 +1,37 @@
+import $ from 'jquery';
+import { $document } from '../../../scripts/utils/commonSelectors';
+
+class Header {
+  constructor({ templates }) {
+    this.templates = templates;
+  }
+  cache = {};
+  init() {
+    this.initCache();
+    this.bindEvents();
+  }
+  initCache() {
+    this.cache.$searchBoxToggle = $('.js-tp-pw-header__search-box-toggle');
+    this.cache.$searchBox = $('.js-tp-pw-search-box');
+    this.cache.$searchBoxInput = $('.js-tp-pw-search-box__input');
+    this.cache.$closeSearcBbox = $('.js-tp-pw-search-box__close-search-box');
+    this.setOverlayHeight();
+  }
+  bindEvents() {
+    this.cache.$searchBoxToggle.on('click', this.openSearchBox);
+    this.cache.$closeSearcBbox.on('click', this.closeSearchBox);
+  }
+  openSearchBox = () => {
+    this.cache.$searchBox.fadeIn(300, () => {
+      $('.js-tp-pw-search-box__input').focus();
+    });
+  };
+  closeSearchBox = () => {
+    this.cache.$searchBox.fadeOut(300);
+  };
+  setOverlayHeight = () => {
+    this.cache.$searchBox.css('height', $document.height());
+  }
+}
+
+export default Header;
