b8d42077f66f4cf5ce82bc365243b0fbc07aacb3
Nicolas Behotas <nicolas.behotas@3sharecorp.com>
2019-03-26 08:19:58 -0300

    carousel FE initial commit (WIP)

--- tetrapak-publicweb/ui.dev/src/source/templates/components/carousel/Carousel.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/carousel/Carousel.js
@@ -1,10 +1,22 @@
+import $ from 'jquery';
+import 'bootstrap';
+
 class Carousel {
+  constructor({ el }) {
+    this.root = $(el);
+  }
   cache = {};
   initCache() {
     /* Initialize cache here */
+    this.cache.$Carousel = $('.pw-carousel', this.root);
+    this.cache.$tabsPills = $('.pw-carousel .nav-pills a', this.root);
   }
   bindEvents() {
     /* Bind jQuery events here */
+    this.cache.$tabsPills.on('click', function (e) {
+      e.preventDefault();
+      $(this).tab('show');
+    });
   }
   init() {
     /* Mandatory method */
