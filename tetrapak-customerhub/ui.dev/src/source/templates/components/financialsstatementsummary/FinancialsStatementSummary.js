a5c17b830057325727a495252843370c989181e7
Manoj Varma <mn0851@agcompany.net>
2019-03-27 19:14:28 +0530

    PAKTETRAP-1151 : CFE- CuHu - Financials - Statement of Accounts

---
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/financialsstatementsummary/FinancialsStatementSummary.js
@@ -0,0 +1,115 @@
+import $ from 'jquery';
+import { render } from '../../../scripts/utils/render';
+import auth from '../../../scripts/utils/auth';
+import { apiHost } from '../../../scripts/common/common';
+import { ajaxMethods, API_FINANCIALS_STATEMENTS } from '../../../scripts/utils/constants';
+
+/**
+ * Processes order table data
+ * @param {object} order Order object
+ * @param {string[]} keys List of keys
+ * @param {string} orderDetailLink order detail link
+ */
+function _tableSort(order, keys) {
+  const dataObject = {
+    row: []
+  };
+  keys.forEach((key, index) => {
+    const value = order[key];
+    dataObject.row[index] = {
+      key,
+      value,
+      isRTE: ['contact'].includes(key)
+    };
+  });
+  return dataObject;
+}
+
+function _processTableData(data) {
+  let keys = [];
+  if (Array.isArray(data.statementSummary)) {
+    data.summary = data.statementSummary.map(order => {
+      keys = (keys.length === 0) ? Object.keys(order) : keys;
+      return _tableSort.call(this, order, keys);
+    });
+    data.summaryHeadings = keys.map(key => ({
+      key,
+      i18nKey: `cuhu.ordering.${key}`,
+      isSortable: ['orderDate'].includes(key),
+      sortOrder: 'desc'
+    }));
+  }
+}
+
+/**
+ * Renders table section
+ * @param {object} filterParams Selected filter parameters
+ */
+function _renderTable(filterParams) {
+  //const { $filters } = this.cache;
+  const $this = this;
+  //this.setSearchFields(filterParams);
+  //this.root.find('.js-pagination').trigger('ordersearch.pagedisabled');
+  auth.getToken(({ data: authData }) => {
+    render.fn({
+      template: 'financialsSummaryTable',
+      target: '.js-financials-summary__table',
+      url: {
+        path: `${apiHost}/${API_FINANCIALS_STATEMENTS}`,
+        data: filterParams
+      },
+      beforeRender(data) {
+        if (!data) {
+          this.data = data = {
+            isError: true
+          };
+        }
+        return _processTableData.apply($this, [data]);
+      },
+      ajaxConfig: {
+        beforeSend(jqXHR) {
+          jqXHR.setRequestHeader('Authorization', `Bearer ${authData.access_token}`);
+          jqXHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        },
+        method: ajaxMethods.GET,
+        cache: true,
+        showLoader: true,
+        cancellable: true
+      }
+    });
+  });
+}
+
+class FinancialsStatementSummary {
+  constructor({ el }) {
+    this.root = $(el);
+  }
+  cache = {};
+  initCache() {
+    /* Initialize selector cache here */
+    /**
+     * If component is added more than once please use "this.root" hook to prevent overlap issues.
+     * Example:
+     * this.cache.$submitBtn = this.root.find('.js-submit-btn');
+     */
+  }
+  bindEvents() {
+    /* Bind jQuery events here */
+    /**
+     * Example:
+     * const { $submitBtn } = this.cache;
+     * $submitBtn.on('click', () => { ... });
+     */
+  }
+  renderTable() {
+    return _renderTable.apply(this, arguments);
+  }
+  init() {
+    /* Mandatory method */
+    this.initCache();
+    this.bindEvents();
+    this.renderTable();
+  }
+}
+
+export default FinancialsStatementSummary;
---
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/financialsstatementsummary/FinancialsStatementSummary.spec.js
@@ -0,0 +1,9 @@
+import FinancialsStatementSummary from './FinancialsStatementSummary';
+
+describe('FinancialsStatementSummary', function () {
+  before(function () {
+    this.financialsStatementSummary = new FinancialsStatementSummary({
+      el: document.body
+    });
+  });
+});
