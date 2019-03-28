dc9e6a6bef105b48e8813bd37108038080bba097
Sachin Singh <ssingh259@sapient.com>
2019-03-29 00:18:08 +0530

    TETRAP-2084 - Test cases added

--- tetrapak-customerhub/ui.dev/src/source/templates/components/ordersearch/OrderSearch.spec.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/ordersearch/OrderSearch.spec.js
@@ -28,6 +28,7 @@ describe('OrderSearch', function () {
     this.renderSpy = sinon.spy(render, 'fn');
     this.dateRangeSpy = sinon.spy(this.orderSearch, 'openRangeSelector');
     this.calendarSpy = sinon.spy(this.orderSearch, 'submitDateRange');
+    this.navigateSpy = sinon.spy(this.orderSearch, 'navigateCalendar');
     this.ajaxStub = sinon.stub(ajaxWrapper, 'getXhrObj');
     this.ajaxStub.yieldsTo('beforeSend', jqRef).returns(ajaxResponse(orderSearchData));
     this.tokenStub = sinon.stub(auth, 'getToken').callsArgWith(0, {
@@ -58,6 +59,7 @@ describe('OrderSearch', function () {
     this.renderSpy.restore();
     this.dateRangeSpy.restore();
     this.calendarSpy.restore();
+    this.navigateSpy.restore();
     this.routeStub.restore();
     this.ajaxStub.restore();
     this.tokenStub.restore();
@@ -119,6 +121,10 @@ describe('OrderSearch', function () {
   it('should open date range selector on click of "date range" input', function () {
     $('.js-order-search__date-range').trigger('click');
     expect(this.dateRangeSpy.called).to.be.true;
+  });
+  it('should navigate calendar of click of calendar navigation buttons', function () {
+    $('.js-calendar-nav').eq(0).trigger('click');
+    expect(this.navigateSpy.called).to.be.true;
   });
   it('should select date range and close range selector on click of "Set Dates" button', function () {
     $('.js-calendar').trigger('click');
