7ca55e0eb213ab1f6a0f920fc6895f5c05013a48
Ruhee Sharna <ruhee.sharma@publicissapient.com>
2019-04-01 11:55:58 +0530

    TETRAP-1151-AEM changes

---
+++ tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/models/FinancialStatementModel.java
@@ -0,0 +1,99 @@
+package com.tetrapak.customerhub.core.models;
+
+import com.google.gson.Gson;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Model class for financial statement component
+ */
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class FinancialStatementModel {
+
+    @Self
+    private Resource resource;
+
+    @Inject
+    private String statementOfAccount;
+
+    @Inject
+    private String findCustomer;
+
+    @Inject
+    private String accountNumber;
+
+    @Inject
+    private String accountService;
+    
+    @Inject
+    private String selectCustomerLabel;
+
+    @Inject
+    private String selectStatusLabel;
+
+    @Inject
+    private String selectDateRangeLabel;
+    
+    @Inject
+    private String selectDocumentTypeLabel;
+
+    @Inject
+    private String placeholderForDocumentNumber;
+    
+    @Inject
+    private String searchInputLabel;
+    
+    @Inject
+    private String resetButtonText;
+
+    @Inject
+    private String searchButtonText;
+    
+    @Inject
+    private String summaryHeadingI18n;
+    
+    @Inject
+    private String documentHeadingI18n;
+    
+    
+
+    private String config;
+
+    @PostConstruct
+    protected void init() {
+   
+        Map<String, Object> i18KeyMap = new HashMap<String, Object>();
+        i18KeyMap.put("statementOfAccount", statementOfAccount);
+        i18KeyMap.put("findCustomer", findCustomer);
+        i18KeyMap.put("accountNumber", accountNumber);
+        i18KeyMap.put("accountService", accountService);
+        i18KeyMap.put("selectCustomerLabel", selectCustomerLabel);
+        i18KeyMap.put("selectStatusLabel", selectStatusLabel);
+        i18KeyMap.put("selectDateRangeLabel", selectDateRangeLabel);
+        i18KeyMap.put("selectDocumentTypeLabel", selectDocumentTypeLabel);
+        i18KeyMap.put("placeholderForDocumentNumber", placeholderForDocumentNumber);
+        i18KeyMap.put("searchInputLabel", searchInputLabel);
+        i18KeyMap.put("resetButtonText", resetButtonText);
+        i18KeyMap.put("searchButtonText", searchButtonText);
+        i18KeyMap.put("summaryHeadingI18n", summaryHeadingI18n);
+        i18KeyMap.put("documentHeadingI18n", documentHeadingI18n);
+        
+        
+        Gson gson = new Gson();
+        config = gson.toJson(i18KeyMap);
+    }
+
+    public String getConfig() {
+        return config;
+    }
+
+}
---
+++ tetrapak-customerhub/core/src/test/java/com/tetrapak/customerhub/core/models/FinancialStatementModelTest.java
@@ -0,0 +1,34 @@
+/*package com.tetrapak.customerhub.core.models;
+
+import com.tetrapak.customerhub.core.mock.CuhuCoreAemContext;
+import io.wcm.testing.mock.aem.junit.AemContext;
+import org.apache.sling.api.resource.Resource;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class FinancialStatementModelTest {
+
+    private FinancialStatementModel financialStatementModel = null;
+    private static final String CONTENT_ROOT = "/content/tetrapak/customerhub/global";
+    private static final String COMPONENT_PATH = "/content/tetrapak/customerhub/global/financials/jcr:content/root/responsivegrid/financialstatement";
+    private static final String RESOURCE_JSON = "allContent.json";
+
+    @Rule
+    public final AemContext aemContext = CuhuCoreAemContext.getAemContext(RESOURCE_JSON, CONTENT_ROOT);
+
+    @Before
+    public void setup() {
+        Resource resource = aemContext.currentResource(COMPONENT_PATH);
+        financialStatementModel = resource.adaptTo(FinancialStatementModel.class);
+    }
+
+    @Test
+    public void testGetStartedMessage() {
+        String config = financialStatementModel.getConfig();
+        Assert.assertTrue(config.contains("Search statements"));
+        Assert.assertTrue(config.contains("Reset search"));
+        
+    }
+}*/
