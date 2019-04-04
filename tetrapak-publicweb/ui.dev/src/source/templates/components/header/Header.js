494714fd26a62e1a36861c33e5f234de6ee6543f
lpoveda <lucio.poveda@3sharecorp.com>
2019-04-04 13:55:48 +0200

    [TETRAP-2135] Adding FE code and minor fix for hero image

--- tetrapak-publicweb/ui.dev/src/source/templates/components/header/Header.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/header/Header.js
@@ -1,6 +1,6 @@
-import deparam from 'jquerydeparam';
 import $ from 'jquery';
 import {$document} from '../../../scripts/utils/commonSelectors';
+//import { ajaxWrapper } from '../../../scripts/utils/ajax';
 
 class Header {
   constructor({templates, el}) {
@@ -20,13 +20,30 @@ class Header {
     this.cache.$searchBox = $('.js-tp-pw-search-box');
     this.cache.$searchBoxInput = $('.js-tp-pw-search-box__input');
     this.cache.$closeSearcBbox = $('.js-tp-pw-search-box__close-search-box');
+    this.cache.url = location.href.split('?')[1] || null;
     this.setOverlayHeight();
   }
 
   bindEvents() {
     this.cache.$searchBoxToggle.on('click', this.openSearchBox);
     this.cache.$closeSearcBbox.on('click', this.closeSearchBox);
+    this.cache.$searchBoxInput.on('keyup', (e) => {
+      if (e.which === 13) {
+        let searchTerm = $('.js-tp-pw-search-box__input').val();
+        this.search(searchTerm);
+      }
+    });
   }
+
+  search = (searchTerm) => {
+    let origin = window.location.origin;
+    let params = {};
+    let searchResultsPath = this.cache.$searchBoxToggle.data('resultsPath');
+    params.q = searchTerm;
+    //TODO Get existent param values modify q and then build the url again
+    let destinationURL = origin + searchResultsPath + '.html?q=' + searchTerm;
+    window.location.replace(destinationURL);
+  };
 
   openSearchBox = () => {
     this.cache.$searchBox.fadeIn(300, () => {
