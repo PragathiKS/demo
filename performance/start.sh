b5597de62c60ec10e106ef5e54f9110752cc2f72
Nitin <nitin.kumar1@publicissapient.com>
2019-04-08 10:54:16 +0530

    creating separate folders inside performance for both customer hub and public web and updating start.sh accordingly

--- performance/start.sh
+++ performance/start.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
-cd /performance/scripts
-echo "Starting Performance Tests for Tetra Pak..."
+
+# customer hub scripts under this section
+cd /performance/customerhub/scripts
+echo "Starting Performance Tests for Tetra Pak Customer Hub..."
+
 echo "Starting /opt/apache-jmeter-5.0/bin/jmeter: Home Page"
-nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "HomePage.jmx" &
+nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "customerhub/HomePage.jmx" &
 echo "Starting /opt/apache-jmeter-5.0/bin/jmeter: All Pages"
-nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "allPages.jmx" &
+nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "customerhub/allPages.jmx" &
+
+
+# public web scripts under this section
+cd /performance/publicweb/scripts
+echo "Starting Performance Tests for Tetra Pak Public Web..."
