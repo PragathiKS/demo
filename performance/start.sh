21e071d0209e89a40d722bcd23af32b915bf91db
Nitin <nitin.kumar1@publicissapient.com>
2019-04-08 11:41:16 +0530

    minor updates in start.sh

--- performance/start.sh
+++ performance/start.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
 # customer hub scripts under this section
-cd /performance/customerhub/scripts
 echo "Starting Performance Tests for Tetra Pak Customer Hub..."
 
 echo "Starting /opt/apache-jmeter-5.0/bin/jmeter: Home Page"
-nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "customerhub/HomePage.jmx" -Jjmeter.save.saveservice.output_format=csv -n -l HomePage.csv &
+nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "./performance/customerhub/scripts/HomePage.jmx" -Jjmeter.save.saveservice.output_format=csv -n &
 echo "Starting /opt/apache-jmeter-5.0/bin/jmeter: All Pages"
-nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "customerhub/allPages.jmx" -Jjmeter.save.saveservice.output_format=csv -n -l allPages.csv &
+nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "./performance/customerhub/scripts/allPages.jmx" -Jjmeter.save.saveservice.output_format=csv -n &
 
 
 # public web scripts under this section
-cd /performance/publicweb/scripts
 echo "Starting Performance Tests for Tetra Pak Public Web..."
