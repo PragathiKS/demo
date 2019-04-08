7b070f41fd96d418c5992147fe37f60dde6b1fdd
Nitin <nitin.kumar1@publicissapient.com>
2019-04-08 10:59:46 +0530

    changing output to csv format

--- performance/start.sh
+++ performance/start.sh
@@ -5,9 +5,9 @@ cd /performance/customerhub/scripts
 echo "Starting Performance Tests for Tetra Pak Customer Hub..."
 
 echo "Starting /opt/apache-jmeter-5.0/bin/jmeter: Home Page"
-nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "customerhub/HomePage.jmx" &
+nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "customerhub/HomePage.jmx" -Jjmeter.save.saveservice.output_format=csv -n -l HomePage.csv &
 echo "Starting /opt/apache-jmeter-5.0/bin/jmeter: All Pages"
-nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "customerhub/allPages.jmx" &
+nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "customerhub/allPages.jmx" -Jjmeter.save.saveservice.output_format=csv -n -l allPages.csv &
 
 
 # public web scripts under this section
