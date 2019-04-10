169efc2736fbfb76dbb0e1749497e857821dd97f
root <nitin.kumar1@publicissapient.com>
2019-04-10 12:58:57 +0530

    testing html reports for jmeter

--- performance/start.sh
+++ performance/start.sh
@@ -4,7 +4,7 @@
 echo "Starting Performance Tests for Tetra Pak Customer Hub..."
 
 echo "Starting /opt/apache-jmeter-5.0/bin/jmeter: Home Page"
-nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "./performance/customerhub/scripts/HomePage.jmx" -Jjmeter.save.saveservice.output_format=csv -o ./performance/customerhub/results/HtmlReport &
+nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "./performance/customerhub/scripts/HomePage.jmx" -Jjmeter.save.saveservice.output_format=csv -o "./performance/customerhub/results/HtmlReport" &
 
 #echo "Starting /opt/apache-jmeter-5.0/bin/jmeter: All Pages"
 #nohup /app/apache-jmeter-5.1/bin/jmeter -n -t "./performance/customerhub/scripts/allPages.jmx" -Jjmeter.save.saveservice.output_format=csv &
