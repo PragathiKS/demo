dd3619f70d8b82f9da6b6e48ce740e2a65363b7e
Nicolas Behotas <nicolas.behotas@3sharecorp.com>
2019-04-12 17:46:14 -0300

    Soft conversion form adding modal and tabs/steps funcionality

--- tetrapak-publicweb/ui.dev/src/source/templates/components/softConversionForm/SoftConversionForm.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/softConversionForm/SoftConversionForm.js
@@ -1,10 +1,39 @@
+import $ from 'jquery';
+import 'bootstrap';
+
 class SoftConversionForm {
   cache = {};
   initCache() {
     /* Initialize cache here */
+    this.cache.$selector = $('#softConversionModal');
   }
   bindEvents() {
     /* Bind jQuery events here */
+    $('body').bind('show.bs.tab', function(e){
+      let parentModal = e.target.closest('.modal');
+      if (!parentModal) {
+        return;
+      }
+      let grandParentId = $(parentModal).attr('id');
+      const grandParentIdSelector = '#'+grandParentId;
+
+      const $toggleBtns = $('[data-toggle="tab"]', grandParentIdSelector);
+
+      let selectedTarget = $(e.target).data('taget');
+      $toggleBtns.each(function() {
+        if ($(this).data('target') !== selectedTarget) {
+          $(this).removeClass('active show');
+        } else {
+          $(this).addClass('active show');
+        }
+      });
+    });
+
+    this.cache.$selector.on('hidden.bs.modal', function () {
+      $('[data-toggle="tab"]', '#softConversionModal').removeClass('active show');
+      $('.tab-pane', '#softConversionModal').removeClass('active');
+      $('.tab-content .tab-pane:first', '#softConversionModal').addClass('active');
+    });
   }
   init() {
     /* Mandatory method */
