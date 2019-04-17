3ddd62c03203fc49dfe2ccb617d66532c6d9bd41
lpoveda <lucio.poveda@3sharecorp.com>
2019-04-17 16:11:57 +0200

    saving changes

---
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/officeLocator/OfficeLocator.js
@@ -0,0 +1,19 @@
+//import tpOfficesData from './data/tp-offices';
+
+
+class OfficeLocator {
+  cache = {};
+  initCache() {
+    /* Initialize cache here */
+  }
+  bindEvents() {
+    /* Bind jQuery events here */
+  }
+  init() {
+    /* Mandatory method */
+    this.initCache();
+    this.bindEvents();
+  }
+}
+
+export default OfficeLocator;
