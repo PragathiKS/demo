cb6635b446e38a908aa4dda7c3f1b4de111f847c
Nitin <nitin.kumar1@publicissapient.com>
2019-04-17 22:27:29 +0530

    TETRAP-1240 refactoring for the table and sonar fixes

--- tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/utils/PDFUtil.java
+++ tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/utils/PDFUtil.java
@@ -3,6 +3,7 @@ package com.tetrapak.customerhub.core.utils;
 import com.tetrapak.customerhub.core.beans.pdf.Column;
 import com.tetrapak.customerhub.core.beans.pdf.Row;
 import com.tetrapak.customerhub.core.beans.pdf.Table;
+import com.tetrapak.customerhub.core.beans.pdf.TableBuilder;
 import com.tetrapak.customerhub.core.constants.CustomerHubConstants;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
@@ -23,7 +24,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -157,45 +157,27 @@ public final class PDFUtil {
      * @param startY        start y position
      */
     public static void drawTable(PDPageContentStream contentStream, Table table, int startY) {
-        // Calculate pagination
-        double d1 = Math.floor((double) table.getHeight() / (double) table.getRowHeight());
-        Integer rowsPerPage = (int) d1 - 1;
 
-        double d2 = Math.ceil((double) table.getNumberOfRows().floatValue() / rowsPerPage);
-        Integer numberOfPages = (int) d2;
+        try {
+            String[][] currentPageContent = table.getContent();
 
-        // Generate each page, get the content and draw it
-        for (int pageCount = 0; pageCount < numberOfPages; pageCount++) {
-            try {
-                String[][] currentPageContent = getContentForCurrentPage(table, rowsPerPage, pageCount);
-                drawOnSamePage(table, currentPageContent, contentStream, startY);
-            } catch (IOException e) {
-                LOGGER.error("IOException in PDF Util class while printing table {}", e);
-            }
-        }
-    }
+            double nextTextX = (double) table.getMargin() + (double) table.getCellMargin();
+            double nextTextY = startY - ((double) table.getRowHeight() / 2)
+                    - (((double) table.getTextFont().getFontDescriptor().getFontBoundingBox().getHeight()
+                    / 1000 * (double) table.getFontSize()) / 4);
 
-    private static void drawOnSamePage(Table table, String[][] currentPageContent, PDPageContentStream contentStream, int startY)
-            throws IOException {
-        double widthLandscape = (double) table.getPageSize().getWidth() - (double) table.getMargin();
-        double widthPortrait = (double) table.getPageSize().getHeight() - (double) table.getMargin();
-        double tableTopY = table.isLandscape() ? widthLandscape : widthPortrait;
-
-        double nextTextX = (double) table.getMargin() + (double) table.getCellMargin();
-        double nextTextY = tableTopY - ((double) table.getRowHeight() / 2)
-                - (((double) table.getTextFont().getFontDescriptor().getFontBoundingBox().getHeight()
-                / 1000 * (double) table.getFontSize()) / 4) - startY;
-
-        writeContentLine(table.getColumnsNamesAsArray(), contentStream, (float) nextTextX, (float) nextTextY, table);
-        nextTextY -= table.getRowHeight();
-        nextTextX = (double) table.getMargin() + (double) table.getCellMargin();
-
-        for (int i = 0; i < currentPageContent.length; i++) {
-            writeContentLine(currentPageContent[i], contentStream, (float) nextTextX, (float) nextTextY, table);
-            nextTextY -= (double) table.getRowHeight();
+            writeContentLine(table.getColumnsNamesAsArray(), contentStream, (float) nextTextX, (float) nextTextY, table);
+            nextTextY -= table.getRowHeight();
             nextTextX = (double) table.getMargin() + (double) table.getCellMargin();
-        }
 
+            for (int i = 0; i < currentPageContent.length; i++) {
+                writeContentLine(currentPageContent[i], contentStream, (float) nextTextX, (float) nextTextY, table);
+                nextTextY -= (double) table.getRowHeight();
+                nextTextX = (double) table.getMargin() + (double) table.getCellMargin();
+            }
+        } catch (IOException e) {
+            LOGGER.error("IOException in PDF Util class while printing table {}", e);
+        }
     }
 
     private static void writeContentLine(String[] lineContent, PDPageContentStream contentStream, double nextTextX, float nextTextY,
@@ -206,20 +188,11 @@ public final class PDFUtil {
             contentStream.beginText();
             contentStream.newLineAtOffset((float) nextTextX, nextTextY);
             contentStream.setFont(isBold ? table.getTextFontBold() : table.getTextFont(), table.getFontSize());
-            contentStream.showText(null == text ? "" : isBold ?
+            contentStream.showText(isBold ?
                     StringUtils.substringAfter(text, CustomerHubConstants.BOLD_IDENTIFIER) : text);
             contentStream.endText();
             nextTextX += table.getColumns().get(i).getWidth();
         }
-    }
-
-    private static String[][] getContentForCurrentPage(Table table, Integer rowsPerPage, int pageCount) {
-        int startRange = pageCount * rowsPerPage;
-        int endRange = (pageCount * rowsPerPage) + rowsPerPage;
-        if (endRange > table.getNumberOfRows()) {
-            endRange = table.getNumberOfRows();
-        }
-        return Arrays.copyOfRange(table.getContent(), startRange, endRange);
     }
 
     /**
@@ -274,18 +247,16 @@ public final class PDFUtil {
      * @param rowHeight   row height
      * @param muliRegular regular font
      * @param muliBold    bold font
-     * @return table
+     * @param fontSize    font size
+     * @return table table
      */
     public static Table getTable(List<Column> columns, String[][] content, double rowHeight,
                                  PDFont muliRegular, PDFont muliBold, double fontSize) {
         final float MARGIN = 65;
-        final boolean IS_LANDSCAPE = false;
         final float FONT_SIZE = (float) fontSize;
 
         final float ROW_HEIGHT = (float) rowHeight;
         final float CELL_MARGIN = 0;
-
-        float tableHeight = 500;
 
         double width = 8.5 * 72;
         double height = (double) 11 * 72;
@@ -294,13 +265,11 @@ public final class PDFUtil {
                 .setCellMargin(CELL_MARGIN)
                 .setColumns(columns)
                 .setContent(content)
-                .setHeight(tableHeight)
                 .setNumberOfRows(content.length)
                 .setRowHeight(ROW_HEIGHT)
                 .setMargin(MARGIN)
                 .setPageSize(new PDRectangle((float) width,
                         (float) height))
-                .setLandscape(IS_LANDSCAPE)
                 .setTextFont(muliRegular)
                 .setTextFontBold(muliBold)
                 .setFontSize(FONT_SIZE)
