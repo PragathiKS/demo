2a6a8b3e707a8abf67be011fecfef1edc6cd1b50
Ankur Gupta <ankur.gupta11@sapient.com>
2019-04-24 12:06:57 +0530

    review comment updates

--- tetrapak-customerhub/ui.dev/src/source/templates/components/financialstatement/FinancialStatement.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/financialstatement/FinancialStatement.js
@@ -272,7 +272,7 @@ function _downloadPdfExcel(...args) {
     requestBody.params = JSON.stringify(paramsData);
     requestBody.token = authData.access_token;
     const url = resolveQuery($this.cache.servletUrl, { extnType: type });
-    let form = $('<form/>', {
+    let form = $('<form class="d-noen"/>', {
       action: url,
       method: 'POST'
     });
