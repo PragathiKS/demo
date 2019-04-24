d68185f2beaf083312758eb3c05fa6fb5771bce0
Manoj Varma <mn0851@agcompany.net>
2019-04-24 17:20:32 +0530

    TETRAP-2780 : CFE- CuHu- Pagination for order detail page : Adding keys according to LLD

--- tetrapak-customerhub/ui.dev/src/source/templates/components/orderdetails/OrderDetails.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/orderdetails/OrderDetails.js
@@ -21,7 +21,7 @@ function _processPackmatData(data) {
     });
     data.orderSummaryHeadings = keys.map(key => ({
       key,
-      i18nKey: `cuhu.orderdetail.${key}`
+      i18nKey: `cuhu.orderDetail.orderSummary.${key}`
     }));
   }
 
@@ -40,13 +40,13 @@ function _processPackmatData(data) {
           if (key === 'quantityKPK') {
             return ({
               key,
-              i18nKey: `cuhu.orderdetail.packaging.delivery.${key}`,
+              i18nKey: `cuhu.orderDetail.deliveryList.products.${key}`,
               iconClassName: 'icon-Info tp-order-detail__icon-info js-icon-Info'
             });
           } else {
             return ({
               key,
-              i18nKey: `cuhu.orderdetail.packaging.delivery.${key}`
+              i18nKey: `cuhu.orderDetail.deliveryList.products.${key}`
             });
           }
         });
