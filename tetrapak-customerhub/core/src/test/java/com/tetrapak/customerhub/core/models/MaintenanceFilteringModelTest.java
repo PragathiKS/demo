5da55de99ec272b597c539d04345012f037e2004
Nitin <nitin.kumar1@publicissapient.com>
2019-04-29 14:32:35 +0530

    TETRAP-1114 Junit and Sonar

--- tetrapak-customerhub/core/src/test/java/com/tetrapak/customerhub/core/models/MaintenanceFilteringModelTest.java
+++ tetrapak-customerhub/core/src/test/java/com/tetrapak/customerhub/core/models/MaintenanceFilteringModelTest.java
@@ -10,6 +10,7 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -33,7 +34,25 @@ public class MaintenanceFilteringModelTest {
     }
 
     @Test
-    public void testGetI18nKeysContainsValidKey() {
-        assertTrue(maintenanceFilteringModel.getI18nKeys().contains("deliveryNumberLabel"));
+    public void testModelGetters() {
+        assertTrue("There should be site filter label in i18n keys string", maintenanceFilteringModel.getI18nKeys().contains("siteFilterLabel"));
+        assertEquals("There should be site filter label", "Site", maintenanceFilteringModel.getSiteFilterLabel());
+        assertEquals("There should be line filter label", "line", maintenanceFilteringModel.getLineFilterLabel());
+        assertEquals("There should be equipment filter label", "Equipment", maintenanceFilteringModel.getEquipmentFilterLabel());
+        assertEquals("There should be all option Text", "All", maintenanceFilteringModel.getAllOptionText());
+        assertEquals("There should be contacts heading", "tetra pak contact", maintenanceFilteringModel.getContactsHeading());
+        assertEquals("There should be schedule heading", "Maintenance Schedule", maintenanceFilteringModel.getScheduleHeading());
+        assertEquals("There should be events heading", "Maintenance events", maintenanceFilteringModel.getEventsHeading());
+        assertEquals("There should be status label", "Status", maintenanceFilteringModel.getStatusLabel());
+        assertEquals("There should be service agreement label", "Service agreement", maintenanceFilteringModel.getServiceAgreementLabel());
+        assertEquals("There should be planned duration label", "Planned duration", maintenanceFilteringModel.getPlannedDurationLabel());
+        assertEquals("There should be planned start label", "Planned start", maintenanceFilteringModel.getPlannedStartLabel());
+        assertEquals("There should be planned finished label", "Planned finished", maintenanceFilteringModel.getPlannedFinishedLabel());
+        assertEquals("There should be line label", "Line", maintenanceFilteringModel.getLineLabel());
+        assertEquals("There should be equipment label", "Equipment", maintenanceFilteringModel.getEquipmentLabel());
+        assertEquals("There should be service order label", "Service order", maintenanceFilteringModel.getServiceOrderLabel());
+        assertEquals("There should be operation hort text label", "Operation short text", maintenanceFilteringModel.getOperationShortTextLabel());
+        assertEquals("There should be no data message", "no data", maintenanceFilteringModel.getNoDataMsg());
+        assertEquals("There should be error message", "error", maintenanceFilteringModel.getErrorMsg());
     }
 }
