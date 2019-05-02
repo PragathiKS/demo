7a39c70092dfc3df36f1c17969abb8a4f453c405
Abhinit Bhatnagar <abhinit.bhatnagar@3sharecorp.com>
2019-05-02 10:15:14 +0530

    TETRAP-2478 Added inheritance to Contact Footer Form component

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/ContactFooterFormModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/ContactFooterFormModel.java
@@ -1,7 +1,6 @@
 package com.tetrapak.publicweb.core.models;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
@@ -10,6 +9,8 @@ import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.day.cq.commons.inherit.HierarchyNodeInheritanceValueMap;
+import com.day.cq.commons.inherit.InheritanceValueMap;
 import com.day.cq.wcm.api.Page;
 
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
@@ -20,73 +21,50 @@ public class ContactFooterFormModel {
 	@Self
     private Resource resource;
 	
-	@Inject
 	private String titleI18n;
 
-	@Inject
 	private String description;
 
-	@Inject
 	private String image;
 
-	@Inject
 	private String imageAltI18n;
 
-	@Inject
 	private String helpText;
 	
-	@Inject
 	private String privacyPolicyText;
 	
-	@Inject
 	private String thankYouHeadline;
 	
-	@Inject
 	private String thankYouMessage;
 	
-	@Inject
 	private String linkTextI18n;
 	
-	@Inject
 	private String linkPath;
 
-	@Inject
 	private String firstNameLabel;
 
-	@Inject
 	private String lastNameLabel;
 	
-	@Inject
 	private String phoneNumberLabel;
 
-	@Inject
 	private String emailAddressLabel;
 
-	@Inject
 	private String positionLabel;
 
-	@Inject
 	private String companyLabel;	
 
-	@Inject
 	private String contactUsLabel;	
 
-	@Inject
 	private String messageLabel;	
 
-	@Inject
 	private String previousButtonLabel;
 
-	@Inject
 	private String nextButtonLabel;
 	
-	@Inject
 	private String submitButtonLabel;
 	
-	@Inject
 	private String stepLabel;
 	
-	@Inject
     private Page currentPage;
 	
 	private Boolean hideContactFooterForm;
@@ -101,6 +79,31 @@ public class ContactFooterFormModel {
             hideContactFooterForm = basePageModel.getPageContent().getHideContactFooterForm();
             log.info("Value of hideContactFooterForm : {}", hideContactFooterForm);
         }
+        
+        InheritanceValueMap inheritanceValueMap1 = new HierarchyNodeInheritanceValueMap(resource);
+        titleI18n = inheritanceValueMap1.getInherited("titleI18n", String.class);
+        description = inheritanceValueMap1.getInherited("description", String.class);
+        image = inheritanceValueMap1.getInherited("image", String.class);
+        imageAltI18n = inheritanceValueMap1.getInherited("imageAltI18n", String.class);
+        helpText = inheritanceValueMap1.getInherited("helpText", String.class);
+        privacyPolicyText = inheritanceValueMap1.getInherited("privacyPolicyText", String.class);
+        thankYouHeadline = inheritanceValueMap1.getInherited("thankYouHeadline", String.class);
+        thankYouMessage = inheritanceValueMap1.getInherited("thankYouMessage", String.class);
+        linkTextI18n = inheritanceValueMap1.getInherited("linkTextI18n", String.class);
+        linkPath = inheritanceValueMap1.getInherited("linkPath", String.class);
+        firstNameLabel = inheritanceValueMap1.getInherited("firstNameLabel", String.class);
+        lastNameLabel = inheritanceValueMap1.getInherited("lastNameLabel", String.class);
+        phoneNumberLabel = inheritanceValueMap1.getInherited("phoneNumberLabel", String.class);
+        emailAddressLabel = inheritanceValueMap1.getInherited("emailAddressLabel", String.class);
+        positionLabel = inheritanceValueMap1.getInherited("positionLabel", String.class);
+        companyLabel = inheritanceValueMap1.getInherited("companyLabel", String.class);
+        contactUsLabel = inheritanceValueMap1.getInherited("contactUsLabel", String.class);
+        messageLabel = inheritanceValueMap1.getInherited("messageLabel", String.class);
+        previousButtonLabel = inheritanceValueMap1.getInherited("previousButtonLabel", String.class);
+        nextButtonLabel = inheritanceValueMap1.getInherited("nextButtonLabel", String.class);
+        submitButtonLabel = inheritanceValueMap1.getInherited("submitButtonLabel", String.class);
+        stepLabel = inheritanceValueMap1.getInherited("stepLabel", String.class);
+        
     }
 
 	public String getTitleI18n() {
