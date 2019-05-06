93299236114f239ed4b1cfb173e1663fc0f619dd
Ruhee Sharna <ruhee.sharma@publicissapient.com>
2019-05-06 13:56:38 +0530

    TETRAP_3196-LinkUtilty Sonar fixes

--- tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/utils/LinkUtil.java
+++ tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/utils/LinkUtil.java
@@ -6,29 +6,45 @@ import org.apache.sling.api.resource.ResourceResolver;
 
 import com.tetrapak.customerhub.core.constants.CustomerHubConstants;
 
+/**
+ * This is a Link util class to access link utility methods
+ *
+ * @author Ruhee sharma
+ */
 public class LinkUtil {
     
-    private static Boolean isExternalLink;
     private static String path;
     
-    public static String getValidLink(Resource resource, String pathField) {
-        if (null == pathField) {
+    private LinkUtil() {
+        throw new IllegalStateException("Utility class");
+      }
+    
+    /**
+     * Method to get Valid Link
+     *
+     * @param resource Resource
+     * @param pathField String
+     * @return String
+     */
+    public static String getValidLink(Resource resource, String pathField) {       
+        ResourceResolver resolver = resource.getResourceResolver();
+        if (null == pathField || null == resource || null == resolver) {
             return StringUtils.EMPTY;
         }
-        if (isInternalLink(pathField)) {
-            ResourceResolver resolver = resource.getResourceResolver();
-            if (null == resolver) {
-                return StringUtils.EMPTY;
-            }
+        if (isInternalLink(pathField)) {            
             String resolvedPath = resolver.map(pathField);
-            if (null == resolvedPath) {
-                return StringUtils.EMPTY;
-            }
             return resolvedPath + CustomerHubConstants.HTML_EXTENSION;
         } else {
             return path;
         }
     }
+    
+    /**
+     * Method to check Internal link starts with "/content and not blank
+     *
+     * @param path String
+     * @return boolean
+     */
     
     private static boolean isInternalLink(String path) {
         return StringUtils.isNotBlank(path) && path.startsWith("/content");
