73ba566dc422e959b8d587309dc02daebc59c3d0
Shilpa Rai <shirai@publicisgroupe.net>
2019-05-09 23:39:23 +0530

    leftnavbar changes

--- tetrapak-customerhub/ui.dev/src/source/templates/components/leftnavigation/LeftNavigation.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/leftnavigation/LeftNavigation.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import { $body } from '../../../scripts/utils/commonSelectors';
+import { logger } from '../../../scripts/utils/logger';
 
 
 class LeftNavigation {
@@ -9,6 +10,7 @@ class LeftNavigation {
   init() {
     this.root.find('.js-close-btn').on('click', this.closeSideNav);
     this.root.find('.js-left-nav__overlay').on('click', this.closeSideNav);
+    this.root.find('.tpatom-list-item__btn').on('click', this.openSubMenu);
     this.root.find('.tpmol-list-item', 'tpatom-list-item').on('click', (e) => {
       e.stopPropagation();
     });
@@ -27,6 +29,17 @@ class LeftNavigation {
     this.root.find('.tp-left-nav__container').addClass('translated');
     this.root.find('.tp-left-nav__overlay').addClass('color-transform');
     this.root.find('.tpatom-list-item__link--sticky').addClass('translated');
+  }
+  openSubMenu = () => {
+    // eslint-disable-next-line no-debugger
+    debugger;
+    // eslint-disable-next-line no-undef
+    logger.log('thisss', this);
+    if (this.root.find('.tpatom-list-item__btn').attr('aria-expanded') === 'false') {
+      this.root.find('.tpatom-list-item__btn').attr('aria-expanded', 'true');
+    } else {
+      this.root.find('.tpatom-list-item__btn').attr('aria-expanded', 'false');
+    }
   }
 }
 export default LeftNavigation;
