b3660e92bacca0b0b9969e3a028bab96d4bc6b44
Nitin <nitin.kumar1@publicissapient.com>
2019-05-14 12:16:26 +0530

    TETRAP-3078 revert back to Http Post

--- tetrapak-commons/core/src/main/java/com/tetrapak/commons/core/services/impl/DispatcherFlushServiceImpl.java
+++ tetrapak-commons/core/src/main/java/com/tetrapak/commons/core/services/impl/DispatcherFlushServiceImpl.java
@@ -8,8 +8,9 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
@@ -63,21 +64,24 @@ public class DispatcherFlushServiceImpl implements DispatcherFlushService {
     }
 
     private void flushDispatcher(HttpClient httpClient, String dispatcherHandle, String dispatcherHostURL) {
-        HttpGet httpGet = new HttpGet();
+        HttpPost httpPost = new HttpPost();
         try {
-            httpGet.setURI(new URI(dispatcherHostURL));
-            httpGet.setHeader("CQ-Action", "Activate");
-            httpGet.setHeader("CQ-Handle", dispatcherHandle);
-            httpGet.setHeader("CQ-Path", dispatcherHandle);
-            httpGet.setHeader("Host", "flush");
+            httpPost.setURI(new URI(dispatcherHostURL));
+            httpPost.setHeader("CQ-Action", "Activate");
+            httpPost.setHeader("CQ-Handle", dispatcherHandle);
+            httpPost.setHeader("Content-Length", "0");
 
             LOGGER.debug("DispatcherFlushService: : dispatcherHostURL is: {} and dispatcherHandle is: {}", dispatcherHostURL, dispatcherHandle);
-            HttpResponse httpResponse = httpClient.execute(httpGet);
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+
+            String responseString = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
+            LOGGER.debug("Response for the flush request : \n {}",responseString);
+
             int statusCode = httpResponse.getStatusLine().getStatusCode();
             if (statusCode == HttpStatus.SC_OK) {
-                LOGGER.debug("Dispatcher Cache flushed successfully. {} for the path {}", dispatcherHostURL, dispatcherHandle);
+                LOGGER.debug("Dispatcher Cache flushed successfully. {}", dispatcherHostURL);
             } else {
-                LOGGER.warn("Dispatcher Cache could not be flushed. {} for the path {}", dispatcherHostURL, dispatcherHandle);
+                LOGGER.warn("Dispatcher Cache could not be flushed. {}", dispatcherHostURL);
             }
         } catch (URISyntaxException e) {
             LOGGER.error("DispatcherFlushServiceImpl | URISyntaxException: {}", e);
@@ -86,7 +90,7 @@ public class DispatcherFlushServiceImpl implements DispatcherFlushService {
         } catch (IOException e) {
             LOGGER.error("IOException in DispatcherFlushServiceImpl {}", e);
         } finally {
-            httpGet.releaseConnection();
+            httpPost.releaseConnection();
         }
     }
 }
