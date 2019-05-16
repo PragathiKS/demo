1e33aebd82b4fd6fbce4ccb0d018354445ed8640
Manoj Varma <mn0851@agcompany.net>
2019-05-16 15:34:41 +0530

    TETRAP-1085 : CFE - CuHu - Dashboard - Maintenance Card

---
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/maintenancecard/MaintenanceCard.js
@@ -0,0 +1,86 @@
+import $ from 'jquery';
+import auth from '../../../scripts/utils/auth';
+import { render } from '../../../scripts/utils/render';
+import { ajaxMethods, API_MAINTENANCE_EVENTS } from '../../../scripts/utils/constants';
+import { apiHost } from '../../../scripts/common/common';
+import { logger } from '../../../scripts/utils/logger';
+
+/**
+ * Renders Maintenance Events
+ */
+function _renderMaintenanceEvents() {
+  const $this = this;
+  auth.getToken(({ data: authData }) => {
+    render.fn({
+      template: 'maintenanceCardEvents',
+      url: {
+        path: `${apiHost}/${API_MAINTENANCE_EVENTS}`
+      },
+      target: '.js-maintenance-card__events',
+      ajaxConfig: {
+        method: ajaxMethods.GET,
+        beforeSend(jqXHR) {
+          jqXHR.setRequestHeader('Authorization', `Bearer ${authData.access_token}`);
+          jqXHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        },
+        cache: true,
+        showLoader: true,
+        cancellable: true
+      },
+      beforeRender(data) {
+        if (!data) {
+          this.data = data = {
+            isError: true
+          };
+        } else {
+          const { i18nKeys } = $this.cache;
+          data.i18nKeys = i18nKeys;
+
+          //$this.processSiteData(data);
+          //$this.cache.data = data;
+        }
+      }
+    }, (data) => {
+      if (!data.isError && !data.noData) {
+        //$this.initPostCache();
+        //$this.renderMaintenanceContact();
+        //this.renderCalendar();
+        //$this.renderCalendarEventsDot();
+      }
+    });
+  });
+  debugger; //eslint-disable-line
+}
+
+class MaintenanceCard {
+  constructor({ el }) {
+    this.root = $(el);
+  }
+  cache = {};
+  initCache() {
+    this.cache.configJson = this.root.find('.js-maintenance-card__config').text();
+    try {
+      this.cache.i18nKeys = JSON.parse(this.cache.configJson);
+    } catch (e) {
+      this.cache.i18nKeys = {};
+      logger.error(e);
+    }
+  }
+  bindEvents() {
+    /* Bind jQuery events here */
+    /**
+     * Example:
+     * const { $submitBtn } = this.cache;
+     * $submitBtn.on('click', () => { ... });
+     */
+  }
+  renderMaintenanceEvents = () => _renderMaintenanceEvents.call(this);
+  init() {
+    /* Mandatory method */
+    this.initCache();
+    this.bindEvents();
+    this.renderMaintenanceEvents();
+  }
+}
+
+export default MaintenanceCard;
---
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/maintenancecard/MaintenanceCard.spec.js
@@ -0,0 +1,9 @@
+import MaintenanceCard from './MaintenanceCard';
+
+describe('MaintenanceCard', function () {
+  before(function () {
+    this.maintenanceCard = new MaintenanceCard({
+      el: document.body
+    });
+  });
+});
