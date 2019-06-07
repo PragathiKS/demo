cb69f1034ff92e3f96ac6ef7ea69797c071ccfb8
Shilpa Rai <shirai@publicisgroupe.net>
2019-06-07 16:56:27 +0530

    changes for image

--- tetrapak-customerhub/ui.dev/src/source/scripts/utils/dynamicMedia.js
+++ tetrapak-customerhub/ui.dev/src/source/scripts/utils/dynamicMedia.js
@@ -11,12 +11,26 @@ function _processImageAttributes(container) {
   $('.js-dynamic-media').each(function () {
     const $this = $(this);
     let desktopSrc = $this.attr('data-src_desktop');
+    let tabletLandSrc = $this.attr('data-src_tabletl');
+    let tabletPortSrc = $this.attr('data-src_tabletp');
     let mobileLandSrc = $this.attr('data-src_mobilel');
     let mobilePortSrc = $this.attr('data-src_mobilep');
 
+    let mobileBreakpoint = 1023;
+    let testTablet = tabletLandSrc || tabletPortSrc;
+    if (testTablet) {
+      mobileBreakpoint = 767;
+    }
+
     if (typeof desktopSrc !== 'undefined') {
+      if (typeof tabletLandSrc === 'undefined') {
+        tabletLandSrc = desktopSrc;
+      }
+      if (typeof tabletPortSrc === 'undefined') {
+        tabletPortSrc = tabletLandSrc;
+      }
       if (typeof mobileLandSrc === 'undefined') {
-        mobileLandSrc = desktopSrc;
+        mobileLandSrc = tabletPortSrc;
       }
       if (typeof mobilePortSrc === 'undefined') {
         mobilePortSrc = mobileLandSrc;
@@ -30,11 +44,25 @@ function _processImageAttributes(container) {
       // mobile portrait
       $this.attr('data-src', mobilePortSrc);
     } else if (
-      window.matchMedia('(min-width: 415px) and (max-width: 1023px)').matches
-      || window.matchMedia('(min-width: 415px) and (max-width: 1023px) and (-webkit-min-device-pixel-ratio: 2), (min-width: 415px) and (max-width: 1023px) and (min-resolution: 192dpi), (min-width: 415px) and (max-width: 1023px) and (min-resolution: 2dppx)').matches
+      window.matchMedia(`(min-width: 415px) and (max-width: ${mobileBreakpoint}px)`).matches
+      || window.matchMedia(`(min-width: 415px) and (max-width: ${mobileBreakpoint}px) and (-webkit-min-device-pixel-ratio: 2), (min-width: 415px) and (max-width: ${mobileBreakpoint}px) and (min-resolution: 192dpi), (min-width: 415px) and (max-width: ${mobileBreakpoint}px) and (min-resolution: 2dppx)`).matches
     ) {
       // mobile landscape
       $this.attr('data-src', mobileLandSrc);
+    } else if (
+      testTablet
+      && (
+        window.matchMedia('(min-width: 768px) and (max-width: 991px)').matches || window.matchMedia('(min-width: 768px) and (max-width: 991px) and (-webkit-min-device-pixel-ratio: 2), (min-width: 768px) and (max-width: 991px) and (min-resolution: 192dpi), (min-width: 768px) and (max-width: 991px) and (min-resolution: 2dppx)').matches
+      )
+    ) {
+      $this.attr('data-src', tabletPortSrc);
+    } else if (
+      testTablet
+      && (
+        window.matchMedia('(min-width: 992px) and (max-width: 1023px)').matches || window.matchMedia('(min-width: 992px) and (max-width: 1023px) and (-webkit-min-device-pixel-ratio: 2), (min-width: 992px) and (max-width: 1023px) and (min-resolution: 192dpi), (min-width: 992px) and (max-width: 1023px) and (min-resolution: 2dppx)').matches
+      )
+    ) {
+      $this.attr('data-src', tabletLandSrc);
     } else {
       //desktop xtra large
       $this.attr('data-src', desktopSrc);
