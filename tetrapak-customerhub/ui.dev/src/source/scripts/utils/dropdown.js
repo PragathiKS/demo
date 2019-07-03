eec56e6e8197bf10c0ea012e6f4edb92270a8263
Ankur Gupta <ankur.gupta11@sapient.com>
2019-07-03 16:18:38 +0530

    custom dropdown code updates

--- tetrapak-customerhub/ui.dev/src/source/scripts/utils/dropdown.js
+++
@@ -1,7 +0,0 @@
-import $ from 'jquery';
-
-export const bindDropdown = (listItemClass) => {
-  $(document).on('click', `.${listItemClass}`, function (e) {
-    $(this).parents('.dropdown').find('.dropdown-toggle').text(e.currentTarget.innerText);
-  });
-};
