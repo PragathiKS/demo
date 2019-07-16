5245be29a7ced1a35a1141c955acdb86a1ec1789
Tushar Tushar <tushar.tushar@publicissapient.com>
2019-07-16 11:37:37 +0530

    Removing PublicWeb code from R-0.5 branch.

--- tetrapak-publicweb/ui.dev/src/testcases.webpack.js
+++
@@ -1,3 +0,0 @@
-// Import test cases
-const context = require.context('./source/templates/components', true, /\.spec\.js$/);
-context.keys().forEach(context);
--- tetrapak-publicweb/ui.dev/src/webpack.dev.js
+++
@@ -1,7 +0,0 @@
-const commonConfig = require('./webpack.common');
-const config = require('./config').webpack;
-
-commonConfig.mode = config.modes.dev;
-commonConfig.devtool = 'source-map';
-
-module.exports = commonConfig;
