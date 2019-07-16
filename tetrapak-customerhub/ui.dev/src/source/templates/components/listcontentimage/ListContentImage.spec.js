7301debd54a4e125294a04c60542fb6778dd39ed
Ankur Gupta <ankur.gupta11@sapient.com>
2019-07-16 11:56:19 +0530

    tab list unit test

--- tetrapak-customerhub/ui.dev/src/source/templates/components/listcontentimage/ListContentImage.spec.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/listcontentimage/ListContentImage.spec.js
@@ -2,7 +2,7 @@ import ListContentImage from './ListContentImage';
 import $ from 'jquery';
 import listContentImageTemplate from '../../../test-templates-hbs/listContentImage.hbs';
 
-describe.only('ListContentImage', function () {
+describe('ListContentImage', function () {
   before(function () {
     $(document.body).empty().html(listContentImageTemplate());
     this.listContentImage = new ListContentImage({ el: $('[data-root]') });
--- tetrapak-customerhub/ui.dev/src/source/templates/components/tabslist/TabsList.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/tabslist/TabsList.js
@@ -8,15 +8,6 @@ class TabsList {
   constructor({ el }) {
     this.root = $(el);
   }
-  cache = {};
-  initCache() {
-    /* Initialize selector cache here */
-    /**
-     * If component is added more than once please use "this.root" hook to prevent overlap issues.
-     * Example:
-     * this.cache.$submitBtn = this.root.find('.js-submit-btn');
-     */
-  }
   bindEvents() {
     const $this = this;
     this.root
@@ -28,7 +19,6 @@ class TabsList {
   renderFirstTab = () => _renderFirstTab.call(this);
 
   init() {
-    this.initCache();
     this.bindEvents();
     this.renderFirstTab();
   }
