b059e02b10dcacd008308898dc369529477e93ee
Tushar Tushar <tushar.tushar@publicissapient.com>
2019-07-25 15:38:25 +0530

    Added code to provide unique component number.

--- tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/models/TabsListModel.java
+++ tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/models/TabsListModel.java
@@ -3,6 +3,7 @@ package com.tetrapak.customerhub.core.models;
 import com.tetrapak.customerhub.core.beans.ImageBean;
 import com.tetrapak.customerhub.core.beans.TabsListBean;
 import com.tetrapak.customerhub.core.utils.GlobalUtil;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
@@ -12,9 +13,11 @@ import org.apache.sling.models.annotations.injectorspecific.Self;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Model class for Tab list content component.
@@ -24,69 +27,81 @@ import java.util.List;
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class TabsListModel {
 
-    @Self
-    private Resource resource;
+	@Self
+	private Resource resource;
 
-    @Inject
-    private String heading;
+	@Inject
+	private String heading;
 
-    private List<TabsListBean> tabsListContent = new ArrayList<>();
+	private String componentId = "tushar";
 
-    private List<String> imageList = new ArrayList<>();
+	private List<TabsListBean> tabsListContent = new ArrayList<>();
 
-    /**
-     * Post construct method to get the tab content from the multifield property saved in CRX for
-     * each of the tab.
-     */
-    @PostConstruct
-    protected void init() {
-        Resource childResource = resource.getChild("tablist");
-        if (null != childResource) {
-            Iterator<Resource> itr = childResource.listChildren();
-            while (itr.hasNext()) {
-                Resource res = itr.next();
+	private List<String> imageList = new ArrayList<>();
 
-                ValueMap valueMap = res.getValueMap();
-                TabsListBean tabsListBean = new TabsListBean();
-                tabsListBean.setType((String) valueMap.get("type"));
-                if ("image".equalsIgnoreCase(tabsListBean.getType())) {
-                    imageList.add(res.getName() + "-image");
-                }
-                tabsListBean.setTabTitleI18n((String) valueMap.get("tabTitleI18n"));
-                tabsListBean.setMediaTitleI18n((String) valueMap.get("mediaTitleI18n"));
-                tabsListBean.setMediaDescriptionI18n((String) valueMap.get("mediaDescriptionI18n"));
-                tabsListBean.setVideoSource((String) valueMap.get("videoSource"));
-                tabsListBean.setDamVideoPath((String) valueMap.get("damVideoPath"));
-                String youtubeVideoID = (String) valueMap.get("youtubeVideoID");
-                if (StringUtils.isNotBlank(youtubeVideoID)) {
-                    tabsListBean.setYoutubeEmbedURL("https://www.youtube.com/embed/" + youtubeVideoID);
-                }
-                tabsListBean.setThumbnailPath((String) valueMap.get("thumbnailPath"));
+	/**
+	 * Post construct method to get the tab content from the multifield property
+	 * saved in CRX for each of the tab.
+	 */
+	@PostConstruct
+	protected void init() {
+		Resource childResource = resource.getChild("tablist");
+		if (null != childResource) {
+			Iterator<Resource> itr = childResource.listChildren();
+			while (itr.hasNext()) {
+				Resource res = itr.next();
 
-                Resource imageResource = GlobalUtil.getImageResource(res);
-                if (null != imageResource) {
-                    ImageBean imageBean = GlobalUtil.getImageBean(imageResource);
-                    tabsListBean.setImage(imageBean);
-                }
+				ValueMap valueMap = res.getValueMap();
+				TabsListBean tabsListBean = new TabsListBean();
+				tabsListBean.setType((String) valueMap.get("type"));
+				if ("image".equalsIgnoreCase(tabsListBean.getType())) {
+					imageList.add(res.getName() + "-image");
+				}
+				tabsListBean.setTabTitleI18n((String) valueMap.get("tabTitleI18n"));
+				tabsListBean.setMediaTitleI18n((String) valueMap.get("mediaTitleI18n"));
+				tabsListBean.setMediaDescriptionI18n((String) valueMap.get("mediaDescriptionI18n"));
+				tabsListBean.setVideoSource((String) valueMap.get("videoSource"));
+				tabsListBean.setDamVideoPath((String) valueMap.get("damVideoPath"));
+				String youtubeVideoID = (String) valueMap.get("youtubeVideoID");
+				if (StringUtils.isNotBlank(youtubeVideoID)) {
+					tabsListBean.setYoutubeEmbedURL("https://www.youtube.com/embed/" + youtubeVideoID);
+				}
+				tabsListBean.setThumbnailPath((String) valueMap.get("thumbnailPath"));
 
-                tabsListBean.setIsExternal(Boolean.valueOf((String) valueMap.get("isExternal")));
-                tabsListBean.setLinkTextI18n((String) valueMap.get("linkTextI18n"));
-                tabsListBean.setLinkURL((String) valueMap.get("linkURL"));
-                tabsListBean.setLinkType((String) valueMap.get("linkType"));
-                tabsListContent.add(tabsListBean);
-            }
-        }
-    }
+				Resource imageResource = GlobalUtil.getImageResource(res);
+				if (null != imageResource) {
+					ImageBean imageBean = GlobalUtil.getImageBean(imageResource);
+					tabsListBean.setImage(imageBean);
+				}
 
-    public String getHeading() {
-        return heading;
-    }
+				tabsListBean.setIsExternal(Boolean.valueOf((String) valueMap.get("isExternal")));
+				tabsListBean.setLinkTextI18n((String) valueMap.get("linkTextI18n"));
+				tabsListBean.setLinkURL((String) valueMap.get("linkURL"));
+				tabsListBean.setLinkType((String) valueMap.get("linkType"));
+				tabsListContent.add(tabsListBean);
+			}
+		}
+		componentId = UUID.randomUUID().toString().replace("-", "");
+	}
 
-    public List<TabsListBean> getTabs() {
-        return tabsListContent;
-    }
+	public String getHeading() {
+		return heading;
+	}
 
-    public List<String> getImageList() {
-        return imageList;
-    }
+	public List<TabsListBean> getTabs() {
+		return tabsListContent;
+	}
+
+	public List<String> getImageList() {
+		return imageList;
+	}
+
+	/**
+	 * Method to generate a unique string as component ID
+	 * 
+	 * @return String componentId
+	 */
+	public String getComponentId() {
+		return componentId;
+	}
 }
