b4e17eb01bdb0d9750d493e8c0d1db7fd8bceea7
Sachin Singh <ssingh259@sapient.com>
2019-07-28 19:15:38 +0530

    TETRAP-3884 - Coverage improved

--- tetrapak-customerhub/ui.dev/src/source/templates/components/tabslist/TabsList.spec.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/tabslist/TabsList.spec.js
@@ -10,18 +10,24 @@ describe('Tablist', function () {
     $(document.body).empty().html(tablistTemplate());
     this.initSpy = sinon.spy(this.tabList, "init");
     this.showTabDetailSpy = sinon.spy(this.tabList, "showTabDetail");
+    this.analyticsSpy = sinon.spy(this.tabList, 'trackAnalytics');
     this.tabList.init();
   });
   after(function () {
     this.initSpy.restore();
     this.showTabDetailSpy.restore();
+    this.analyticsSpy.restore();
   });
   it('should initialize', function () {
     expect(this.tabList.init.called).to.be.true;
   });
 
-  // it('should show tab content on click of tab', function () {
-  //   $('.js-tablist__event').trigger('click');
-  //   expect(this.tabList.showTabDetail.called).to.be.true;
-  // });
+  it('should show tab content on click of tab', function () {
+    $('.js-tablist__event').first().trigger('click');
+    expect(this.tabList.showTabDetail.called).to.be.true;
+  });
+  it('should track analytics on click of description link', function () {
+    $('.js-tablist__event-detail-description-link').first().trigger('click');
+    expect(this.analyticsSpy.called).to.be.true;
+  });
 });
