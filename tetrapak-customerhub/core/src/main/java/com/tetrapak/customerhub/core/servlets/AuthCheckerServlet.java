3aff8bba246cc4d4d961ce20f618bd1c4b011829
Nitin <nitin.kumar1@publicissapient.com>
2019-08-19 16:37:04 +0530

    adding cookie in auth checker servlet

--- tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/servlets/AuthCheckerServlet.java
+++ tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/servlets/AuthCheckerServlet.java
@@ -1,17 +1,22 @@
 package com.tetrapak.customerhub.core.servlets;
 
+import com.tetrapak.customerhub.core.constants.CustomerHubConstants;
+import com.tetrapak.customerhub.core.services.UserPreferenceService;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
 import javax.servlet.Servlet;
+import javax.servlet.http.Cookie;
 
 /**
  * Auth Checker Servlet performs the authentication and authorization of the user
@@ -26,6 +31,9 @@ import javax.servlet.Servlet;
         })
 public class AuthCheckerServlet extends SlingSafeMethodsServlet {
 
+    @Reference
+    private UserPreferenceService userPreferenceService;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthCheckerServlet.class);
     private static final long serialVersionUID = -8381109083575997038L;
 
@@ -36,6 +44,15 @@ public class AuthCheckerServlet extends SlingSafeMethodsServlet {
         if (null == session) {
             LOGGER.error("auth checker servlet exception: session is null");
             return;
+        }
+
+        Cookie languageCookie = request.getCookie("lang-code");
+        if (null != languageCookie) {
+            final String langCode = userPreferenceService.getSavedPreferences(session.getUserID(),
+                    CustomerHubConstants.LANGUGAGE_PREFERENCES);
+            if (StringUtils.isNotEmpty(langCode)) {
+                setLanguageCookie(request, response, langCode);
+            }
         }
 
         String uri = request.getParameter("uri");
@@ -51,5 +68,12 @@ public class AuthCheckerServlet extends SlingSafeMethodsServlet {
             LOGGER.info("auth checker says READ access DENIED! ", e);
             response.setStatus(SlingHttpServletResponse.SC_FORBIDDEN);
         }
+    }
+
+    private void setLanguageCookie(SlingHttpServletRequest request, SlingHttpServletResponse response, String langCode) {
+        Cookie cookie = new Cookie("lang-code", langCode);
+        cookie.setPath("/");
+        cookie.setDomain(request.getServerName());
+        response.addCookie(cookie);
     }
 }
