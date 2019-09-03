b1efce39043bcf6f6dfe66295b9d1b2eb10cd581
Sachin Singh <ssingh259@sapient.com>
2019-09-03 14:23:29 +0530

    Fixed issues with multiple open modals

--- tetrapak-customerhub/ui.dev/src/source/scripts/utils/domReady.js
+++ tetrapak-customerhub/ui.dev/src/source/scripts/utils/domReady.js
@@ -42,7 +42,12 @@ export default {
     }).on('show.bs.modal', function () {
       $(this).addClass('tp-no-backdrop');
     }).on('hidden.bs.modal', function () {
-      $(this).removeClass('tp-no-backdrop');
+      const $this = $(this);
+      if ($('.modal.show').length === 0) {
+        $this.removeClass('tp-no-backdrop');
+      } else {
+        $this.addClass('modal-open');
+      }
     });
     // Custom scrollbar cross-browser handling
     if (
