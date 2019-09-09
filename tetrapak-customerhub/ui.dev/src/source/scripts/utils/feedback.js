4e9ed0aae57044183bbf15e67833b0dfec40d738
Sachin Singh <ssingh259@sapient.com>
2019-09-09 12:21:47 +0530

    TETRAP-4102 - Site improve script integrated

---
+++ tetrapak-customerhub/ui.dev/src/source/scripts/utils/feedback.js
@@ -0,0 +1,13 @@
+import { logger } from './logger';
+
+export default {
+  init() {
+    // Include site improve script
+    const siteImproveScript = $('#siteImproveScript').val();
+    if (siteImproveScript) {
+      const allScripts = $('script');
+      allScripts.eq(allScripts.length - 1).after(`<script type="text/javascript" src="${siteImproveScript}" async="true"></script>`);
+      logger.log('[Webpack]: Siteimprove script added');
+    }
+  }
+};
