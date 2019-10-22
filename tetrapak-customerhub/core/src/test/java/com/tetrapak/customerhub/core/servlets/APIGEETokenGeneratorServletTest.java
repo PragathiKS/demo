03fcf67ef54d20b3f3825b3a2db372b345dc4194
Nitin <nitin.kumar1@publicissapient.com>
2019-10-22 16:57:28 +0530

    Junit fix for httpclient based APIGEE token generator servlet

--- tetrapak-customerhub/core/src/test/java/com/tetrapak/customerhub/core/servlets/APIGEETokenGeneratorServletTest.java
+++ tetrapak-customerhub/core/src/test/java/com/tetrapak/customerhub/core/servlets/APIGEETokenGeneratorServletTest.java
@@ -1,48 +1,73 @@
 package com.tetrapak.customerhub.core.servlets;
 
+import com.google.gson.JsonObject;
 import com.tetrapak.customerhub.core.mock.CuhuCoreAemContext;
 import com.tetrapak.customerhub.core.mock.GenericServiceType;
 import com.tetrapak.customerhub.core.mock.MockAPIGEEServiceImpl;
 import com.tetrapak.customerhub.core.services.APIGEEService;
+import com.tetrapak.customerhub.core.utils.HttpUtil;
 import io.wcm.testing.mock.aem.junit.AemContext;
-import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.StatusLine;
-import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.sling.api.servlets.HttpConstants;
 import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
 import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletResponse;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(value = {HttpClientBuilder.class, HttpUtil.class})
 public class APIGEETokenGeneratorServletTest {
 
     private static final String SERVLET_RESOURCE_PATH = "/bin/customerhub/token-generator";
     private static final String SERVLET_RESOURCE_JSON = "allContent.json";
+
+    @Mock
+    CloseableHttpClient httpClient;
+    @Mock
+    HttpPost httpPost;
+    @Mock
+    CloseableHttpResponse httpResponse;
+    @Mock
+    StatusLine statusLine;
+    @Mock
+    HttpClientBuilder httpClientBuilder;
 
     @Rule
     public final AemContext aemContext = CuhuCoreAemContext.getAemContext(SERVLET_RESOURCE_JSON, SERVLET_RESOURCE_PATH, getMultipleMockedService());
 
     @Before
     public void setup() throws IOException {
-        HttpClient httpClient = mock(HttpClient.class);
-        HttpPost httpGet = mock(HttpPost.class);
-        HttpResponse httpResponse = mock(HttpResponse.class);
-        StatusLine statusLine = mock(StatusLine.class);
+        PowerMockito.mockStatic(HttpUtil.class);
+        JsonObject jsonResponse = new JsonObject();
+        jsonResponse.addProperty("status", 200);
+        PowerMockito.when(HttpUtil.setJsonResponse(Mockito.anyObject(), Mockito.anyObject())).thenReturn(jsonResponse);
+
+        PowerMockito.mockStatic(HttpClientBuilder.class);
+        PowerMockito.when(HttpClientBuilder.create()).thenReturn(httpClientBuilder);
+        PowerMockito.when(httpClientBuilder.build()).thenReturn(httpClient);
 
         when(statusLine.getStatusCode()).thenReturn(200);
         when(httpResponse.getStatusLine()).thenReturn(statusLine);
-        when(httpClient.execute(httpGet)).thenReturn(httpResponse);
+        when(httpClient.execute(Mockito.anyObject())).thenReturn(httpResponse);
 
         aemContext.currentResource(SERVLET_RESOURCE_PATH);
         aemContext.request().setServletPath(SERVLET_RESOURCE_PATH);
@@ -56,7 +81,7 @@ public class APIGEETokenGeneratorServletTest {
         APIGEETokenGeneratorServlet apigeeTokenGeneratorServlet = aemContext.getService(APIGEETokenGeneratorServlet.class);
         aemContext.registerInjectActivateService(apigeeTokenGeneratorServlet);
         apigeeTokenGeneratorServlet.doGet(request, response);
-        assertEquals("status from response", HttpStatus.SC_INTERNAL_SERVER_ERROR, response.getStatus());
+        assertEquals("status from response", HttpStatus.SC_OK, response.getStatus());
     }
 
     public <T> List<GenericServiceType<T>> getMultipleMockedService() {
