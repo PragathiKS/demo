848ca5a0e4b54c9b067048883dba5ecb5d722f1a
Sachin Singh <ssingh259@sapient.com>
2020-01-20 12:33:24 +0530

    Added test cases for error page

--- tetrapak-publicweb/ui.dev/src/source/templates/components/errorPage/ErrorPage.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/errorPage/ErrorPage.js
@@ -1,41 +1,33 @@
 import $ from 'jquery';
-import {debounce} from '../../../scripts/utils/debounce';
+import { debounce } from '../../../scripts/utils/debounce';
+import { $win } from '../../../scripts/utils/commonSelectors';
 
 class ErrorPage {
-
   constructor({ el }) {
     this.root = $(el);
   }
-
   cache = {};
-
   initCache() {
-    this.cache.$window = $(window);
-    this.cache.$header = $('.js-tp-pw-header', this.root);
-    this.cache.$footer = $('.js-tp-pw-footer', this.root);
-    this.cache.$jsTpContainer = $('.js-tp-container', this.root);
+    this.cache.$header = this.root.find('.js-tp-pw-header');
+    this.cache.$footer = this.root.find('.js-tp-pw-footer');
+    this.cache.$tpContainer = this.root.find('.js-tp-container');
   }
-
   bindEvents() {
-
     this.initCache();
-    $(window).on('resize', debounce(this.setScreenInnerHeight, 200));
+    $win.on('resize',
+      debounce(this.setScreenInnerHeight, 200)
+    );
   }
-
   init() {
     this.bindEvents();
     this.setScreenInnerHeight();
   }
-
-
-
   setScreenInnerHeight = () => {
-    let headerHeight = this.cache.$header.height() || 0;
-    let footerHeight = this.cache.$footer.height() || 0;
-    let textContainerHeight = this.cache.$window.height() - headerHeight - footerHeight;
-    this.cache.$jsTpContainer.css('min-height', textContainerHeight);
+    const headerHeight = this.cache.$header.height() || 0;
+    const footerHeight = this.cache.$footer.height() || 0;
+    const textContainerHeight = $win.height() - headerHeight - footerHeight;
+    this.cache.$tpContainer.css('min-height', textContainerHeight);
   }
-
 }
 
 export default ErrorPage;
