3812d8e871e48b00e3a498b978f03a879248b8e9
Sachin Singh <ssingh259@sapient.com>
2020-01-31 14:28:09 +0530

    Karma fix

--- tetrapak-publicweb/ui.dev/src/karma.conf.js
+++ tetrapak-publicweb/ui.dev/src/karma.conf.js
@@ -13,7 +13,8 @@ module.exports = function (config) {
         flags: [
           '--headless',
           '--window-size=1920,1080',
-          '--remote-debugging-port=9222'
+          '--remote-debugging-port=9222',
+          '--no-sandbox'
         ]
       }
     },
