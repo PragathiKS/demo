05491ad08b314ad52cca1865a93aef25d1c2c9ac
Bimmi Soi <bimmi.soi@publicissapient.com>
2020-03-12 17:59:22 +0530

    junits added

---
+++ tetrapak-publicweb/core/src/test/java/com/tetrapak/publicweb/core/mock/MockDynamicMediaServiceImpl.java
@@ -0,0 +1,22 @@
+package com.tetrapak.publicweb.core.mock;
+
+import com.tetrapak.publicweb.core.services.DynamicMediaService;
+
+public class MockDynamicMediaServiceImpl implements DynamicMediaService {
+	@Override
+	public String getImageServiceUrl() {
+		return "http://s7g10.scene7.com/is/image";
+	}
+
+	@Override
+	public String[] getDynamicMediaConfMap() {
+		String[] a = {
+				"getstarted-desktop=1440\\,300,getstarted-mobileL=414\\,259\\,0.333\\,0\\,0.333\\,1,getstarted-mobileP=414\\,259\\,0.333\\,0\\,0.333\\,1" };
+		return a;
+	}
+
+	@Override
+	public String getRootPath() {
+		return "/tetrapak";
+	}
+}
---
+++ tetrapak-publicweb/core/src/test/java/com/tetrapak/publicweb/core/models/DynamicMediaImageModelTest.java
@@ -0,0 +1,71 @@
+package com.tetrapak.publicweb.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.tetrapak.publicweb.core.mock.MockDynamicMediaServiceImpl;
+import com.tetrapak.publicweb.core.services.DynamicMediaService;
+
+import io.wcm.testing.mock.aem.junit.AemContext;
+
+public class DynamicMediaImageModelTest {
+	@Rule
+	public AemContext context = new AemContext();
+
+	/** The Constant RESOURCE_CONTENT. */
+	private static final String RESOURCE_CONTENT = "/carousel/test-content.json";
+
+	/** The Constant TEST_CONTENT_ROOT. */
+	private static final String TEST_CONTENT_ROOT = "/content/publicweb/en";
+
+	/** The Constant RESOURCE. */
+	private static final String RESOURCE = TEST_CONTENT_ROOT + "/jcr:content/carousel";
+
+	/** The model. */
+	private DynamicMediaImageModel model;
+
+	/** The resource. */
+	private Resource resource;
+
+	private DynamicMediaService dynamicMediaService;
+
+	/**
+	 * Sets the up.
+	 *
+	 * @param context the new up
+	 * @throws Exception the exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		dynamicMediaService = new MockDynamicMediaServiceImpl();
+		context.registerService(DynamicMediaService.class, dynamicMediaService);
+		context.request().setAttribute("imagePath", "/content/dam/tetrapak/p2.png");
+		context.request().setAttribute("mobileImagePath", "/content/dam/tetrapak/p2.png");
+		context.request().setAttribute("mobileCroppingOption", "right");
+		Class<DynamicMediaImageModel> modelClass = DynamicMediaImageModel.class;
+		MockSlingHttpServletRequest request = context.request();
+		// load the resources for each object
+		context.load().json(RESOURCE_CONTENT, TEST_CONTENT_ROOT);
+		context.addModelsForClasses(modelClass);
+		context.request().setPathInfo(TEST_CONTENT_ROOT);
+		request.setResource(context.resourceResolver().getResource(RESOURCE));
+		resource = context.currentResource(RESOURCE);
+		model = request.adaptTo(modelClass);
+	}
+
+	/**
+	 * Test model, resource and all getters of the ArticleContainer model.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	public void simpleLoadAndGettersTest() throws Exception {
+		String[] methods = new String[] { "getDynamicMediaConfiguration", "getMobilePortraitUrl",
+				"getMobileLandscapeUrl", "getTabletPortraitUrl", "getTabletLandscapeUrl", "getDesktopUrl" };
+		UtilTest.testLoadAndGetters(methods, model, resource);
+	}
+	
+}
