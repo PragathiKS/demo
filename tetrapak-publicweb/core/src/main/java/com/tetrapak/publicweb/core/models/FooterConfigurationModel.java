c7aa187776587426519e029342ebad2a067feb17
Bimmi Soi <bimmi.soi@publicissapient.com>
2020-03-27 22:29:12 +0530

    changes for footer and footer configuration components

---
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/FooterConfigurationModel.java
@@ -0,0 +1,117 @@
+package com.tetrapak.publicweb.core.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.inject.Inject;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import com.tetrapak.publicweb.core.models.multifield.FooterLinkModel;
+import com.tetrapak.publicweb.core.models.multifield.SocialLinkModel;
+import com.tetrapak.publicweb.core.utils.LinkUtils;
+
+/**
+ * The Class FooterConfigurationModel.
+ */
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class FooterConfigurationModel {
+
+    /** The resource. */
+    @Self
+    private Resource resource;
+
+    /** The logo link. */
+    @ValueMapValue
+    private String logoLink;
+
+    /** The logo link target. */
+    @ValueMapValue
+    private String logoLinkTarget;
+
+    /** The logo alt. */
+    @ValueMapValue
+    private String logoAlt;
+
+    /** The social links. */
+    @Inject
+    private List<SocialLinkModel> socialLinks;
+
+    /** The footer links. */
+    @Inject
+    private List<FooterLinkModel> footerLinks;
+
+    /** The go to top label. */
+    @ValueMapValue
+    private String goToTopLabel;
+
+    /**
+     * Gets the logo link.
+     *
+     * @return the logo link
+     */
+    public String getLogoLink() {
+        return LinkUtils.sanitizeLink(logoLink);
+    }
+
+    /**
+     * Gets the logo link target.
+     *
+     * @return the logo link target
+     */
+    public String getLogoLinkTarget() {
+        return logoLinkTarget;
+    }
+
+    /**
+     * Gets the logo alt.
+     *
+     * @return the logo alt
+     */
+    public String getLogoAlt() {
+        return logoAlt;
+    }
+
+    /**
+     * Gets the social links.
+     *
+     * @return the social links
+     */
+    public List<SocialLinkModel> getSocialLinks() {
+        final List<SocialLinkModel> lists = new ArrayList<>();
+        if (Objects.nonNull(socialLinks)) {
+            lists.addAll(socialLinks);
+        }
+        return lists;
+
+    }
+
+    /**
+     * Gets the footer link.
+     *
+     * @return the footer link
+     */
+    public List<FooterLinkModel> getFooterLinks() {
+        final List<FooterLinkModel> lists = new ArrayList<>();
+        if (Objects.nonNull(footerLinks)) {
+            lists.addAll(footerLinks);
+        }
+        return lists;
+
+    }
+
+    /**
+     * Gets the go to top label.
+     *
+     * @return the go to top label
+     */
+    public String getGoToTopLabel() {
+        return goToTopLabel;
+    }
+
+}
