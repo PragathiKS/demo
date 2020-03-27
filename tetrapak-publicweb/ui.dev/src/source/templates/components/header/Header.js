1fa763815d86e9721c82c0da025efd7eb606aa77
Satish Sharma <satsharm1@publicisgroupe.net>
2020-03-27 20:22:42 +0530

    SMAR-11231 adding test cases

--- tetrapak-publicweb/ui.dev/src/source/templates/components/header/Header.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/header/Header.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
-import { $document, $win } from '../../../scripts/utils/commonSelectors';
-import { loc } from '../../../scripts/common/common';
+// import { $document, $win } from '../../../scripts/utils/commonSelectors';
+// import { loc } from '../../../scripts/common/common';
 
 class Header {
   constructor({ el }) {
@@ -11,60 +11,32 @@ class Header {
   cache = {};
 
   initCache() {
-    this.cache.$searchBoxToggle = this.root.find('.js-tp-pw-header__search-box-toggle');
-    this.cache.$searchBox = this.root.find('.js-tp-pw-search-box');
     this.cache.$mobileMenu = this.root.find('.js-tp-pw-mobile-navigation');
-    this.cache.$searchBoxInput = this.root.find('.js-tp-pw-search-box__input');
-    this.cache.$closeSearcBbox = this.root.find('.js-tp-pw-search-box__close-search-box');
     this.cache.$hamburgerToggle = this.root.find('.js-tp-pw-header__hamburger');
-    //this.cache.$hamburgerClose = this.root.find('.js-tp-pw-header__hamburger-close');
-    this.cache.secondaryNav = this.root.find('.js-tp-pw-header__secondary-navigation');
-    this.cache.searchBoxPointer = this.root.find('.js-tp-pw-search-box__pointer');
-    this.cache.pointedElement = this.root.find(`.${this.cache.searchBoxPointer.data('pointTo')}`);
-    this.cache.url = location.href.split('?')[1] || null;
-    this.setOverlayHeight();
+    this.cache.$headerLogoPlaceholder = this.root.find('.js-tp-pw-header-logo-digital-data');
   }
 
   bindEvents() {
-    const { $searchBoxToggle, $closeSearcBbox, $searchBoxInput, $hamburgerToggle } = this.cache;
-    $searchBoxToggle.on('click', this.openSearchBox);
-    $closeSearcBbox.on('click', this.closeSearchBox);
+    const { $hamburgerToggle, $headerLogoPlaceholder } = this.cache;
     $hamburgerToggle.on('click', this.openMobileMenuBoxToggle);
-    //$hamburgerClose.on('click', this.closeMobileMenuBox);
-    $searchBoxInput.on('key.return', () => {
-      const searchTerm = $('.js-tp-pw-search-box__input').val();
-      this.search(searchTerm);
-    });
-    $win.on('resize', () => {
-      this.movePointer();
+    $headerLogoPlaceholder.on('click', function (e) {
+      e.preventDefault();
+      const $this = $(this);
+      if (window.digitalData) {
+        $.extend(window.digitalData, {
+          linkClick: {
+            linkType: 'internal',
+            linkSection: $this.data('link-section'),
+            linkParentTitle: $this.data('link-parent-title'),
+            linkName: $this.data('link-name')
+          }
+        });
+        if (window._satellite) {
+          window._satellite.track('linkClicked');
+        }
+      }
     });
   }
-
-  search = (searchTerm) => {
-    const origin = window.location.origin;
-    const params = {};
-    const searchResultsPath = this.cache.$searchBoxToggle.data('resultsPath');
-    params.q = searchTerm;
-    //TODO Get existent param values modify q and then build the url again
-    const destinationURL = origin + searchResultsPath + '.html?q=' + searchTerm;
-    loc.replace(destinationURL);
-  };
-
-  openSearchBox = () => {
-    this.cache.$searchBox.fadeIn(300, () => {
-      $('.js-tp-pw-search-box__input').focus();
-    });
-  };
-  closeSearchBox = () => {
-    this.cache.$searchBox.fadeOut(300);
-  };
-  setOverlayHeight = () => {
-    this.cache.$searchBox.css('height', $document.height());
-  };
-  movePointer = () => {
-    const secNavWidth = this.cache.secondaryNav.width();
-    $('.js-tp-pw-search-box__pointer').css('right', secNavWidth + 10 + 'px');
-  };
 
   openMobileMenuBoxToggle = () => {
     if(!this.toggleFlag){
@@ -80,18 +52,32 @@ class Header {
     }
   }
 
-  // closeMobileMenuBox = () => {
+  // setLogoDigitalData = (e) => {
+  //   e.preventDefault();
+  //   const $this = $(this);
   //   // eslint-disable-next-line no-console
-  //   console.log('CloseMobileMenuBox called');
-  //   this.cache.$mobileMenu.fadeOut(300);
-  //   this.cache.$hamburgerToggle.fadeIn(100);
-  //   this.cache.$hamburgerClose.fadeOut(100);
+  //   console.log(this,'before linkClicked', this.cache.$headerLogoPlaceholder.data('link-section'), this.cache.$headerLogoPlaceholder.data('link-parent-title'));
+  //   if (window.digitalData) {
+  //     $.extend(window.digitalData, {
+  //       linkClick: {
+  //         linkType: 'internal',
+  //         linkSection: $this.data('link-section'),
+  //         linkParentTitle: $this.data('link-parent-title'),
+  //         linkName: $this.data('link-name')
+  //       }
+  //     });
+  //     if (window._satellite) {
+
+  //       // eslint-disable-next-line no-console
+  //       console.log('linkClicked');
+  //       window._satellite.track('linkClicked');
+  //     }
+  //   }
   // }
 
   init() {
     this.initCache();
     this.bindEvents();
-    this.movePointer(this.cache.searchBoxPointer, this.cache.pointedElement);
   }
 }
 
--- tetrapak-publicweb/ui.dev/src/source/templates/components/header/Header.spec.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/header/Header.spec.js
@@ -8,10 +8,10 @@ describe('Header', function () {
     $(document.body).empty().html(headerTemplate());
     this.header = new Header({ el: document.body });
     this.initSpy = sinon.spy(this.header, 'init');
-    this.openSearchBoxSpy = sinon.spy(this.header, 'openSearchBox');
-    this.closeSearchBoxSpy = sinon.spy(this.header, 'closeSearchBox');
+    // this.openSearchBoxSpy = sinon.spy(this.header, 'openSearchBox');
+    //this.closeSearchBoxSpy = sinon.spy(this.header, 'closeSearchBox');
     this.openMobileMenuBoxToggleSpy = sinon.spy(this.header, 'openMobileMenuBoxToggle');
-    this.searchSpy = sinon.spy(this.header, 'search');
+    //this.searchSpy = sinon.spy(this.header, 'search');
     this.replaceStub = sinon.stub(loc, 'replace');
     this.replaceStub.returns(true);
     this.header.init();
@@ -20,30 +20,43 @@ describe('Header', function () {
   after(function () {
     $(document.body).empty();
     this.initSpy.restore();
-    this.openSearchBoxSpy.restore();
-    this.closeSearchBoxSpy.restore();
+    // this.openSearchBoxSpy.restore();
+    //this.closeSearchBoxSpy.restore();
     this.openMobileMenuBoxToggleSpy.restore();
-    this.searchSpy.restore();
+    //this.searchSpy.restore();
     this.replaceStub.restore();
   });
   it('should initialize', function () {
     expect(this.header.init.called).to.be.true;
   });
-  it('should open search box on click', function () {
-    $('.js-tp-pw-header__search-box-toggle').trigger('click');
-    expect(this.header.openSearchBox.called).to.be.true;
-  });
+  // it('should open search box on click', function () {
+  //   $('.js-tp-pw-header__search-box-toggle').trigger('click');
+  //   expect(this.header.openSearchBox.called).to.be.true;
+  // });
   it('should open mobile menu box on click', function () {
     // this.header.toggleFlag = false;
     $('.js-tp-pw-header__hamburger').trigger('click');
     expect(this.header.openMobileMenuBoxToggle.called).to.be.true;
   });
-  it('should close search box on click', function () {
-    $('.js-tp-pw-search-box__close-search-box').trigger('click');
-    expect(this.header.closeSearchBox.called).to.be.true;
+
+  it('should close mobile menu box on click', function () {
+    this.header.toggleFlag = true;
+    $('.js-tp-pw-header__hamburger').trigger('click');
+    expect(this.header.openMobileMenuBoxToggle.called).to.be.true;
   });
-  it('should submit search term on enter key press', function () {
-    $('.js-tp-pw-search-box__input').trigger('key.return');
-    expect(this.searchSpy.called).to.be.true;
+
+  it('should track logo on click', function () {
+    //this.header.toggleFlag = true;
+    $('.js-tp-pw-header-logo-digital-data').trigger('click');
+    expect(true).to.be.true;
   });
+  // it('should close search box on click', function () {
+  //   $('.js-tp-pw-search-box__close-search-box').trigger('click');
+  //   expect(this.header.closeSearchBox.called).to.be.true;
+  // });
+
+  // it('should submit search term on enter key press', function () {
+  //   $('.js-tp-pw-search-box__input').trigger('key.return');
+  //   expect(this.searchSpy.called).to.be.true;
+  // });
 });
