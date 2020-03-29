cce32d90946774c47df4fff331e4d5324ee86188
sandip kumar <sandip.kumar@tetrapak.com>
2020-03-30 00:59:29 +0530

    SMAR-9533

---
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/CookieConsentModel.java
@@ -0,0 +1,68 @@
+package com.tetrapak.publicweb.core.models;
+
+import com.day.cq.wcm.api.Page;
+import com.tetrapak.publicweb.core.utils.PageUtil;
+import org.apache.commons.lang.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Model class for Consent Cookie component.
+ *
+ * @author Sandip kumar
+ */
+@Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class CookieConsentModel {
+
+    @SlingObject
+    private SlingHttpServletRequest request;
+
+    private Boolean cookieConsentDisabled;
+
+    private String cookieConsentText;
+
+    private String cookieConsentButtonText;
+
+    private String selectedLanguage;
+
+    @PostConstruct
+    protected void init() {
+        Page languagePage = PageUtil.getLanguagePage(request.getResource());
+        if (null != languagePage) {
+            selectedLanguage = PageUtil.getLanguageCode(languagePage);
+            Resource cookieConfigurationResource = request.getResourceResolver().getResource(languagePage.getPath()
+                    + "/jcr:content/root/responsivegrid/cookieconfiguration");
+            if (null != cookieConfigurationResource) {
+                ValueMap map = cookieConfigurationResource.getValueMap();
+                cookieConsentDisabled = Boolean.valueOf((String) map.get("cookieConsentDisabled"));
+                cookieConsentText = (String) map.get("cookieConsentText");
+                cookieConsentButtonText = (String) map.get("cookieConsentButtonText");
+            }
+        }
+    }
+
+    public Boolean isCookieConsentDisabled() {
+        return cookieConsentDisabled;
+    }
+
+    public String getCookieConsentText() {
+        return cookieConsentText;
+    }
+
+    public String getCookieConsentButtonText() {
+        return cookieConsentButtonText;
+    }
+
+    public String getLocale() {
+        if(StringUtils.isEmpty(selectedLanguage)) {
+            selectedLanguage = "en";
+        }
+        return selectedLanguage;
+    }
+}
---
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/utils/PageUtil.java
@@ -0,0 +1,92 @@
+package com.tetrapak.publicweb.core.utils;
+
+import java.util.Locale;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.resource.Resource;
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import com.tetrapak.publicweb.core.constants.PWConstants;
+
+/**
+ * The Class PageUtil.
+ * @author Sandip kumar
+ */
+public final class PageUtil {
+    
+    /**
+     * Get the current page.
+     * @param resource the resource
+     * @return the current page
+     */
+    public static Page getCurrentPage(final Resource resource) {
+        if (resource != null) {
+            final PageManager pageManager = resource.getResourceResolver().adaptTo(PageManager.class);
+            if (pageManager != null) {
+                return pageManager.getContainingPage(resource);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * This method returns page locale default is en-US.
+     * @param currentPage current page
+     * @return local current locale
+     */
+    public static Locale getPageLocale(final Page currentPage) {
+        if (currentPage != null) {
+            return currentPage.getLanguage(false);
+        }
+
+        return new Locale.Builder().setLanguage(PWConstants.ENGLISH_LANGUAGE_ISO_CODE)
+                .setRegion(PWConstants.GB_ISO_CODE).build();
+    }
+
+    /**
+     * @param currentPage
+     * @return language
+     */
+    public static Page getLanguagePage(final Page currentPage) {
+	if(currentPage != null) {
+	    return currentPage.getAbsoluteParent(PWConstants.LANGUAGE_PAGE_LEVEL);
+	}
+	return null;
+    }
+    
+    /**
+     * @param resource the resource
+     * @return language page
+     */
+    public static Page getLanguagePage(final Resource resource) {
+	Page currentPage = getCurrentPage(resource);
+	if(currentPage != null) {
+	    return currentPage.getAbsoluteParent(PWConstants.LANGUAGE_PAGE_LEVEL);
+	}
+	return null;
+    }
+
+    /**
+     * @param currentPage
+     * @return country page
+     */
+    public static Page getCountryPage(final Page currentPage) {
+	return currentPage.getAbsoluteParent(PWConstants.COUNTRY_PAGE_LEVEL);
+    }
+    
+    /**
+     * @param languagePage
+     * @return current language
+     */
+    public static String getLanguageCode(final Page languagePage) {
+	String langauge = PWConstants.ENGLISH_LANGUAGE_ISO_CODE;
+	if(languagePage != null) {
+	    langauge = languagePage.getLanguage().getLanguage();  
+	}
+	if(StringUtils.isEmpty(langauge)) {
+	    langauge = PWConstants.ENGLISH_LANGUAGE_ISO_CODE;
+	}
+	return langauge;
+    }
+
+}
