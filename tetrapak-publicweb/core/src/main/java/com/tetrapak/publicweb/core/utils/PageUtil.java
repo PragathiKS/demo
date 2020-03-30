4412675f0277b82f4ccc888b02a6537d1499b4e4
sandip kumar <sandip.kumar@tetrapak.com>
2020-03-30 15:19:39 +0530

    SMAR-9533

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/utils/PageUtil.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/utils/PageUtil.java
@@ -10,37 +10,40 @@ import com.tetrapak.publicweb.core.constants.PWConstants;
 
 /**
  * The Class PageUtil.
+ * 
  * @author Sandip kumar
  */
 public final class PageUtil {
-    
+
     /**
      * Get the current page.
+     * 
      * @param resource the resource
      * @return the current page
      */
     public static Page getCurrentPage(final Resource resource) {
-        if (resource != null) {
-            final PageManager pageManager = resource.getResourceResolver().adaptTo(PageManager.class);
-            if (pageManager != null) {
-                return pageManager.getContainingPage(resource);
-            }
-        }
-        return null;
+	if (resource != null) {
+	    final PageManager pageManager = resource.getResourceResolver().adaptTo(PageManager.class);
+	    if (pageManager != null) {
+		return pageManager.getContainingPage(resource);
+	    }
+	}
+	return null;
     }
 
     /**
      * This method returns page locale default is en-US.
+     * 
      * @param currentPage current page
      * @return local current locale
      */
     public static Locale getPageLocale(final Page currentPage) {
-        if (currentPage != null) {
-            return currentPage.getLanguage(false);
-        }
+	if (currentPage != null) {
+	    return currentPage.getLanguage(false);
+	}
 
-        return new Locale.Builder().setLanguage(PWConstants.ENGLISH_LANGUAGE_ISO_CODE)
-                .setRegion(PWConstants.GB_ISO_CODE).build();
+	return new Locale.Builder().setLanguage(PWConstants.ENGLISH_LANGUAGE_ISO_CODE)
+		.setRegion(PWConstants.GB_ISO_CODE).build();
     }
 
     /**
@@ -48,19 +51,19 @@ public final class PageUtil {
      * @return language
      */
     public static Page getLanguagePage(final Page currentPage) {
-	if(currentPage != null) {
+	if (currentPage != null) {
 	    return currentPage.getAbsoluteParent(PWConstants.LANGUAGE_PAGE_LEVEL);
 	}
 	return null;
     }
-    
+
     /**
      * @param resource the resource
      * @return language page
      */
     public static Page getLanguagePage(final Resource resource) {
 	Page currentPage = getCurrentPage(resource);
-	if(currentPage != null) {
+	if (currentPage != null) {
 	    return currentPage.getAbsoluteParent(PWConstants.LANGUAGE_PAGE_LEVEL);
 	}
 	return null;
@@ -73,17 +76,17 @@ public final class PageUtil {
     public static Page getCountryPage(final Page currentPage) {
 	return currentPage.getAbsoluteParent(PWConstants.COUNTRY_PAGE_LEVEL);
     }
-    
+
     /**
      * @param languagePage
      * @return current language
      */
     public static String getLanguageCode(final Page languagePage) {
 	String langauge = PWConstants.ENGLISH_LANGUAGE_ISO_CODE;
-	if(languagePage != null) {
-	    langauge = languagePage.getLanguage().getLanguage();  
+	if (languagePage != null) {
+	    langauge = languagePage.getLanguage().getLanguage();
 	}
-	if(StringUtils.isEmpty(langauge)) {
+	if (StringUtils.isEmpty(langauge)) {
 	    langauge = PWConstants.ENGLISH_LANGUAGE_ISO_CODE;
 	}
 	return langauge;
