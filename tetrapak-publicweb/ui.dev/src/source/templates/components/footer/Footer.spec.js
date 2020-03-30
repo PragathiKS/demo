6d85e7395e317ba69ee5d49e227956666bb3b366
Anshika <anshika1185@gmail.com>
2020-03-30 20:49:06 +0530

    Changes for footer SMAR-9246

--- tetrapak-publicweb/ui.dev/src/source/templates/components/footer/Footer.spec.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/footer/Footer.spec.js
@@ -1,20 +1,27 @@
 import $ from 'jquery';
 import Footer from './Footer';
+import footerTemplate from '../../../test-templates-hbs/footer.hbs';
 
 describe('Footer', function () {
   before(function () {
-    $(document.body).empty().html(`<div class="tp-pw-footer"><button class="tp_textBtn">Top</button></div>`);
+    $(document.body).empty().html(footerTemplate());
     this.footer = new Footer({
       el: document.body
     });
     this.initSpy = sinon.spy(this.footer, 'init');
+    this.trackAnalyticsSpy = sinon.spy(this.footer, 'trackAnalytics');
     this.goToTopSpy = sinon.spy(this.footer, 'goToTop');
+    window.digitalData = {};
+    window._satellite = {
+      track() { /* Dummy method */ }
+    };
     this.footer.init();
   });
   after(function () {
     $(document.body).empty();
     this.initSpy.restore();
     this.goToTopSpy.restore();
+    this.trackAnalyticsSpy.restore();
   });
   it('should initialize', function () {
     expect(this.initSpy.called).to.be.true;
@@ -22,5 +29,12 @@ describe('Footer', function () {
   it('should go to top on click of "top" button', function () {
     $('.tp_textBtn').trigger('click');
     expect(this.goToTopSpy.called).to.be.true;
+  });
+  it('should call track analytics on click', function () {
+    $('.tp-pw-footer__link').trigger('click');
+    expect(this.footer.trackAnalytics.called).to.be.true;
+  });
+  it('should set digitalData after track analytics call', function() {
+    expect(window.digitalData.linkClick.linkType).to.equal('internal');
   });
 });
