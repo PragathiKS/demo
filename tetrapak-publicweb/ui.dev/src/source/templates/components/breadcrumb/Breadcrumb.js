4cd0a31f15e31c60ab83190415cb0d9898230ea9
Satish Sharma <satsharm1@publicisgroupe.net>
2020-03-31 17:04:19 +0530

    SMAR-11234 bredcrumbs code implimentation

--- tetrapak-publicweb/ui.dev/src/source/templates/components/breadcrumb/Breadcrumb.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/breadcrumb/Breadcrumb.js
@@ -8,26 +8,18 @@ class Breadcrumb {
   cache = {};
 
   initCache() {
-    // eslint-disable-next-line no-console
-    console.log('breadcrumbs initCache');
-    // this.cache.$mobileMenu = this.root.find('.js-tp-pw-mobile-navigation');
-    // this.cache.$hamburgerToggle = this.root.find('.js-tp-pw-header__hamburger');
     this.cache.$breadcrumbLink = this.root.find('.js-tp_pw-breadcrumb__link');
   }
 
   bindEvents() {
     const { $breadcrumbLink } = this.cache;
-    // $hamburgerToggle.on('click', this.openMobileMenuBoxToggle);
     $breadcrumbLink.on('click', this.trackAnalytics);
-    // $(window).on('resize', this.hideMobileMenuOnResize);
   }
 
   trackAnalytics = (e) => {
     e.preventDefault();
     const $target = $(e.target);
     const $this = $target.closest('.js-tp_pw-breadcrumb__link');
-    // eslint-disable-next-line no-console
-    console.log('link clicked:::',$this.data('link-name'));
     if (window.digitalData) {
       $.extend(window.digitalData, {
         linkClick: {
@@ -38,13 +30,13 @@ class Breadcrumb {
         }
       });
       if (window._satellite) {
-        window._satellite.track('linkClicked');
+        window._satellite.track('linkClick');
       }
     }
 
     //if($this.data('link-section') === 'logo') {
-    var url = $this.attr('href');
-    window.open(url);
+    // var url = $this.attr('href');
+    // window.open(url);
     //}
   }
 
--- tetrapak-publicweb/ui.dev/src/source/templates/components/breadcrumb/Breadcrumb.spec.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/breadcrumb/Breadcrumb.spec.js
@@ -1,41 +1,36 @@
-//import Breadcrumb from './Breadcrumb';
-//import $ from 'jquery';
-//import breadcrumbTemplate from '../../../test-templates-hbs/breadcrumb.hbs';
+import Breadcrumb from './Breadcrumb';
+import $ from 'jquery';
+import breadcrumbTemplate from '../../../test-templates-hbs/breadcrumb.hbs';
 
 describe('Breadcrumb', function () {
   before(function () {
-    // $(document.body).empty().html(breadcrumbTemplate());
-    // this.breadcrumb = new Breadcrumb({ el: document.body });
-    // this.initSpy = sinon.spy(this.breadcrumb, 'init');
-    // //this.openMobileMenuBoxToggleSpy = sinon.spy(this.breadcrumb, 'openMobileMenuBoxToggle');
-    // //this.hideMobileMenuOnResizeSpy = sinon.spy(this.breadcrumb, 'hideMobileMenuOnResize');
-    // this.trackAnalyticsSpy = sinon.spy(this.breadcrumb, 'trackAnalytics');
-    // window.digitalData = {};
-    // window._satellite = {
-    //   track() { /* Dummy method */ }
-    // };
-    // this.header.init();
+    $(document.body).empty().html(breadcrumbTemplate());
+    this.breadcrumb = new Breadcrumb({ el: document.body });
+    this.initSpy = sinon.spy(this.breadcrumb, 'init');
+    this.trackAnalyticsSpy = sinon.spy(this.breadcrumb, 'trackAnalytics');
+    window.digitalData = {};
+    window._satellite = {
+      track() { /* Dummy method */ }
+    };
+    this.breadcrumb.init();
 
   });
   after(function () {
-    // $(document.body).empty();
-    // this.initSpy.restore();
-    // // this.openMobileMenuBoxToggleSpy.restore();
-    // //this.hideMobileMenuOnResizeSpy.restore();
-    // this.trackAnalyticsSpy.restore();
-    //this.replaceStub.restore();
+    $(document.body).empty();
+    this.initSpy.restore();
+    this.trackAnalyticsSpy.restore();
   });
-//   it('should initialize', function () {
-//     expect(this.header.init.called).to.be.true;
-//   });
+  it('should initialize', function () {
+    expect(this.breadcrumb.init.called).to.be.true;
+  });
   
-//   it('should call track analytics on click', function () {
-//     $('.js-tp-pw-header-logo-digital-data').trigger('click');
-//     expect(this.header.trackAnalytics.called).to.be.true;
-//   });
+  it('should call track analytics on click', function () {
+    $('.js-tp_pw-breadcrumb__link').trigger('click');
+    expect(this.breadcrumb.trackAnalytics.called).to.be.true;
+  });
 
-//   it('should set digitalData after track analytics call', function() {
-//     expect(window.digitalData.linkClick.linkType).to.equal('internal');
-//   });
+  it('should set digitalData after track analytics call', function() {
+    expect(window.digitalData.linkClick.linkType).to.equal('internal');
+  });
 
 });
