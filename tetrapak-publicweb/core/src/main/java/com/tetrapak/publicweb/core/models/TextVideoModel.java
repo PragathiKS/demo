473bc7890ae3d30c589cb26613bfa7b1fd164cb5
Abhishek Narula <abhishek.narula@publicissapient.com>
2020-04-08 17:04:07 +0530

    SMAR-11654 |

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/TextVideoModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/TextVideoModel.java
@@ -109,6 +109,9 @@ public class TextVideoModel {
     /** The Constant AUTHOR. */
     private static final String AUTHOR = "author";
 
+    /** The Constant FORWARD_SLASH. */
+    private static final String FORWARD_SLASH = "/";
+
     /**
      * The init method.
      */
@@ -296,5 +299,41 @@ public class TextVideoModel {
      */
     public String getPwDisplay() {
         return pwDisplay;
+    }
+
+    /**
+     * Gets the asset name.
+     *
+     * @return the asset name
+     */
+    public String getAssetName() {
+        String assetName = StringUtils.EMPTY;
+        if (StringUtils.isNotEmpty(linkURL)) {
+            assetName = getSubstringAfterLast(linkURL);
+        }
+        return assetName;
+    }
+
+    /**
+     * Gets the video name.
+     *
+     * @return the video name
+     */
+    public String getVideoName() {
+        String videoName = StringUtils.EMPTY;
+        if (StringUtils.isNoneEmpty(damVideoPath)) {
+            videoName = getSubstringAfterLast(damVideoPath);
+        }
+        return videoName;
+    }
+
+    /**
+     * Gets the substring after last.
+     *
+     * @param path the path
+     * @return the substring after last
+     */
+    private String getSubstringAfterLast(final String path) {
+        return StringUtils.substringAfterLast(path, FORWARD_SLASH);
     }
 }
--- tetrapak-publicweb/core/src/test/java/com/tetrapak/publicweb/core/models/TextVideoModelTest.java
+++ tetrapak-publicweb/core/src/test/java/com/tetrapak/publicweb/core/models/TextVideoModelTest.java
@@ -1,5 +1,8 @@
 package com.tetrapak.publicweb.core.models;
 
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.junit.Before;
 import org.junit.Rule;
@@ -15,13 +18,26 @@ public class TextVideoModelTest {
     /** The Constant RESOURCE_CONTENT. */
     private static final String RESOURCE_CONTENT = "/textvideo/test-content.json";
 
+    /** The Constant ANOTHER_RESOURCE_CONTENT. */
+    private static final String ANOTHER_RESOURCE_CONTENT = "/textvideo/text-video-content.json";
+
     /** The Constant TEST_CONTENT_ROOT. */
     private static final String TEST_CONTENT_ROOT = "/content/publicweb/en";
+
+    /** The Constant ANOTHER_TEST_CONTENT_ROOT. */
+    private static final String ANOTHER_TEST_CONTENT_ROOT = "/content/publicweb/en/home";
 
     /** The Constant TEXTVIDEO_RESOURCE. */
     private static final String RESOURCE_PATH = TEST_CONTENT_ROOT + "/jcr:content/textvideo";
 
+    /** The Constant ANOTHER_RESOURCE_PATH. */
+    private static final String ANOTHER_RESOURCE_PATH = ANOTHER_TEST_CONTENT_ROOT + "/jcr:content/textvideo";
+
+    /** The model. */
     private TextVideoModel model;
+
+    /** The text video model. */
+    private TextVideoModel textVideoModel;
 
     /** The resource. */
     private Resource resource;
@@ -34,13 +50,26 @@ public class TextVideoModelTest {
      */
     @Before
     public void setUp() throws Exception {
+        loadContextOne();
+        loadContextTwo();
+    }
 
+    private void loadContextOne() {
         final Class<TextVideoModel> modelClass = TextVideoModel.class;
         // load the resources for each object
         context.load().json(RESOURCE_CONTENT, TEST_CONTENT_ROOT);
         context.addModelsForClasses(modelClass);
         resource = context.currentResource(RESOURCE_PATH);
         model = resource.adaptTo(modelClass);
+    }
+
+    private void loadContextTwo() {
+        final Class<TextVideoModel> modelClass = TextVideoModel.class;
+        // load the resources for each object
+        context.load().json(ANOTHER_RESOURCE_CONTENT, ANOTHER_TEST_CONTENT_ROOT);
+        context.addModelsForClasses(modelClass);
+        resource = context.currentResource(ANOTHER_RESOURCE_PATH);
+        textVideoModel = resource.adaptTo(modelClass);
     }
 
     /**
@@ -54,5 +83,25 @@ public class TextVideoModelTest {
                 "getYoutubeVideoID", "getDamVideoPath", "getThumbnailPath", "getThumbnailAltText",
                 "getPwTheme", "getPwButtonTheme", "getPwLinkTheme", "getPwPadding", "getPwDisplay" };
         Util.testLoadAndGetters(methods, model, resource);
+    }
+
+    @Test
+    public void testNotEmptyAssetName() {
+        assertEquals("Petrol.pdf", model.getAssetName());
+    }
+
+    @Test
+    public void testNotEmptyVideoName() {
+        assertEquals("Video.mp4", model.getVideoName());
+    }
+
+    @Test
+    public void testEmptyAssetName() {
+        assertEquals(StringUtils.EMPTY, textVideoModel.getAssetName());
+    }
+
+    @Test
+    public void testEmptyVideoName() {
+        assertEquals(StringUtils.EMPTY, textVideoModel.getVideoName());
     }
 }
