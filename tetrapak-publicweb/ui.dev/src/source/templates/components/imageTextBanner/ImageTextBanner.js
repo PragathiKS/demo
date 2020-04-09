979fbb95f04b83f90b510d8025827c188bb98e35
Anshika <anshika1185@gmail.com>
2020-04-09 17:06:40 +0530

    SMAR-9546

--- tetrapak-publicweb/ui.dev/src/source/templates/components/imageTextBanner/ImageTextBanner.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/imageTextBanner/ImageTextBanner.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import { trackAnalytics } from '../../../scripts/utils/analytics';
 
 class ImageTextBanner {
   constructor({ el }) {
@@ -6,53 +7,69 @@ class ImageTextBanner {
   }
   cache = {};
   initCache() {
-    this.cache.$modalBtn = this.root.find('.opensoftc');
-    this.cache.itbLink = this.root.find('.itblink');
-    this.cache.$softConversionModal = $('#softConversionModal');
+    this.cache.$itbLink = this.root.find('.js-banner-analytics');
   }
   bindEvents() {
-    const { $modalBtn } = this.cache;
-    $modalBtn.on('click', this.softConversionFlow);
-    this.cache.itbLink.on('click', this.trackAnalytics);
+    const { $itbLink } = this.cache;
+    $itbLink.on('click', this.trackAnalytics);
   }
+
   trackAnalytics = (e) => {
+    e.preventDefault();
     const $target = $(e.target);
-    const banner = $target.closest('.pw-banner');
-    if (window.digitalData) {
-      $.extend(window.digitalData, {
-        linkClick: {
-          linkType: banner.attr('data-itb-linkType'),
-          linkSection: banner.attr('data-itb-linkSection'),
-          linkParentTitle: $.trim($target.closest('.pw-banner').find('.pw-banner__content__title').text()),
-          linkName: $.trim($target.text()),
-          contentName: $.trim($target.closest('.pw-banner').find('.pw-banner__content__subtitle').text())
-        }
-      });
-      if (window._satellite) {
-        window._satellite.track('linkClicked');
-      }
+    const $this = $target.closest('.js-banner-analytics');
+    let linkParentTitle = '';
+    let trackingObj = {};
+    const dwnType = 'ungated';
+    let eventType = 'content-load';
+    const linkType = $this.data('link-type');
+    const linkSection = $this.data('link-section');
+    const linkName = $this.data('link-name');
+    const imageText = $this.data('data-link-name');
+    const buttonLinkType = $this.data('button-link-type');
+    const downloadtype = $this.data('download-type');
+    const dwnDocName = $this.data('asset-name');
+
+    if(buttonLinkType==='secondary' && downloadtype ==='hyperlink'){
+      linkParentTitle = `CTA-Hyperlink_${imageText}`;
     }
-  }
-  softConversionFlow = () => {
-    const { $softConversionModal } = this.cache;
-    $softConversionModal.find('.softc-title-js').text(this.root.data('softc-title'));
-    $softConversionModal.find('.softc-desc-js').text(this.root.data('softc-desc'));
-    $softConversionModal.find('.softc-head-js').text(this.root.data('softc-head'));
-    $softConversionModal.find('.softc-last-js').text(this.root.data('softc-last'));
-    $softConversionModal.find('.softc-doc-js').val(this.root.data('softc-doc'));
-    if (window.digitalData) {
-      $.extend(window.digitalData, {
-        formInfo: {
-          contentName: this.root.data('softc-title'),
-          formName: 'gated content sign up',
-          stepName: 'gated content sign up start',
-          totalSteps: 4
-        }
-      });
-      if (window._satellite) {
-        window._satellite.track('signup_form_tracking');
-      }
+
+    if(buttonLinkType==='link' && downloadtype ==='hyperlink'){
+      linkParentTitle = `Text hyperlink_${imageText}`;
     }
+
+    if(buttonLinkType==='secondary' && downloadtype ==='download'){
+      linkParentTitle = `CTA-Download-pdf_${imageText}`;
+      eventType = 'download';
+    }
+
+    if(buttonLinkType==='link' && downloadtype ==='download'){
+      linkParentTitle = `Text hyperlink - Download-pdf_${imageText}`;
+      eventType = 'download';
+    }
+
+    if(downloadtype !=='download'){
+      trackingObj = {
+        linkType,
+        linkSection,
+        linkParentTitle,
+        linkName,
+        eventType
+      };
+    }else{
+      trackingObj = {
+        linkType,
+        linkSection,
+        linkParentTitle,
+        linkName,
+        dwnDocName,
+        dwnType,
+        eventType
+      };
+    }
+
+    trackAnalytics(trackingObj, 'linkClick', 'bannerClick', undefined, false);
+    window.open($this.attr('href'), $this.attr('target'));
   }
   init() {
     this.initCache();
