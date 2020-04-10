e582529737bf3363f9311eb6a9b321ab8876de31
Piyush Kapoor <piykapoo@publicisgroupe.net>
2020-04-10 14:30:36 +0530

    folder name change

---
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/textImage/TextImage.js
@@ -0,0 +1,43 @@
+import $ from 'jquery';
+import { trackAnalytics } from '../../../scripts/utils/analytics';
+
+
+class TextImage {
+  constructor({ el }) {
+    this.root = $(el);
+  }
+  cache = {};
+  initCache() {
+    this.cache.$textImageLink = this.root.find('.js-textImage-analytics');
+  }
+  bindEvents() {
+    this.cache.$textImageLink.on('click', this.trackAnalytics);
+  }
+
+
+
+  trackAnalytics = (e) => {
+    e.preventDefault();
+    const $target = $(e.target);
+    const $this = $target.closest('.js-textImage-analytics');
+    let trackingObj = {};
+    const eventType = 'content-load';
+    const downloadtype = $this.data('download-type');
+
+    if(downloadtype ==='download'){
+      trackingObj = {
+        eventType
+      };
+    }
+
+    trackAnalytics(trackingObj, 'linkClick', 'TextImageClick', undefined, false);
+    window.open($this.attr('href'), $this.attr('target'));
+  }
+
+  init() {
+    this.initCache();
+    this.bindEvents();
+  }
+}
+
+export default TextImage;
