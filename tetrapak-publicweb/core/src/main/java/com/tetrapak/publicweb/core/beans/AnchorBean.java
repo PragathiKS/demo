31aaf7236ca1bcbd61cd27169150ed5a514ac74f
Bimmi Soi <bimmi.soi@publicissapient.com>
2020-04-14 16:17:14 +0530

    Anchor component

---
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/beans/AnchorBean.java
@@ -0,0 +1,25 @@
+package com.tetrapak.publicweb.core.beans;
+
+public class AnchorBean {
+
+    private String anchorId;
+
+    private String anchorTitle;
+
+    public String getAnchorId() {
+        return anchorId;
+    }
+
+    public void setAnchorId(String anchorId) {
+        this.anchorId = anchorId;
+    }
+
+    public String getAnchorTitle() {
+        return anchorTitle;
+    }
+
+    public void setAnchorTitle(String anchorTitle) {
+        this.anchorTitle = anchorTitle;
+    }
+
+}
