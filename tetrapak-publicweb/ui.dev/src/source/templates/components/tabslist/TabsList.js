0d06932825a8a893186963f29c265af1ec6c683a
Satish Sharma <satsharm1@publicisgroupe.net>
2020-04-14 16:24:34 +0530

    feature/SMAR-11650 tablist without test case

--- tetrapak-publicweb/ui.dev/src/source/templates/components/tabslist/TabsList.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/tabslist/TabsList.js
@@ -18,36 +18,36 @@ function _renderFirstTab() {
  * Fire analytics on click of
  * any tab list
  */
-function _trackAnalytics(type, el) {
-  const { heading } = this.cache;
-  const currentEl = $(el);
+// function _trackAnalytics(type, el) {
+//   const { heading } = this.cache;
+//   const currentEl = $(el);
 
-  const analyticsData = {
-    linkType: 'internal',
-    linkSection: 'tablist'
-  };
+//   const analyticsData = {
+//     linkType: 'internal',
+//     linkSection: 'tablist'
+//   };
 
-  switch (type) {
-  case 'tabclick': {
-    analyticsData.linkParentTitle = $.trim(heading.toLowerCase());
-    analyticsData.linkName = $.trim(currentEl.text());
-    analyticsData.linkListPos = currentEl.data('index') + 1;
-    break;
-  }
-  case 'descriptionLink': {
-    const activeTab = currentEl.parents('.js-tablist').find('.js-tablist__event.active');
-    analyticsData.linkType = currentEl.attr('target') === '_blank' ? 'external' : 'internal';
-    analyticsData.contentName = $.trim(activeTab.text());
-    analyticsData.linkParentTitle = $.trim(activeTab.next('.show').find('.js-tablist__event-detail-title').text());
-    analyticsData.linkName = $.trim(currentEl.text());
-    break;
-  }
-  default:
-    break;
-  }
+//   switch (type) {
+//   case 'tabclick': {
+//     analyticsData.linkParentTitle = $.trim(heading.toLowerCase());
+//     analyticsData.linkName = $.trim(currentEl.text());
+//     analyticsData.linkListPos = currentEl.data('index') + 1;
+//     break;
+//   }
+//   case 'descriptionLink': {
+//     const activeTab = currentEl.parents('.js-tablist').find('.js-tablist__event.active');
+//     analyticsData.linkType = currentEl.attr('target') === '_blank' ? 'external' : 'internal';
+//     analyticsData.contentName = $.trim(activeTab.text());
+//     analyticsData.linkParentTitle = $.trim(activeTab.next('.show').find('.js-tablist__event-detail-title').text());
+//     analyticsData.linkName = $.trim(currentEl.text());
+//     break;
+//   }
+//   default:
+//     break;
+//   }
 
-  trackAnalytics(analyticsData, 'linkClick', 'linkClicked', undefined, false);
-}
+//   trackAnalytics(analyticsData, 'linkClick', 'linkClicked', undefined, false);
+// }
 
 class TabsList {
   constructor({ el }) {
@@ -57,23 +57,23 @@ class TabsList {
   initCache() {
     this.cache.selectedTabIndex = 0;
     this.cache.componentId = this.root.find('#tabComponentId').val();
+    this.cache.activeTheme = this.root.find('#activeTheme').val();
     this.cache.heading = $.trim(this.root.find('.js-tablist__heading').text());
   }
   bindEvents() {
     const $this = this;
+    const { activeTheme } = this.cache;
     this.root
       .on('click', '.js-tablist__event', function () {
         const self = $(this);
-        if (!self.hasClass('active')) {
+        if (!self.hasClass(`active--${activeTheme}`)) {
           $this.showTabDetail(self.data('target'));
-          $this.trackAnalytics('tabclick', this);
+          // $this.trackAnalytics('tabclick', this);
         }
-        $this.root.find('.js-tablist__event').removeClass('active');
-        self.addClass('active').toggleClass('m-active');
+        $this.root.find('.js-tablist__event').removeClass(`active--${activeTheme}`);
+        self.addClass(`active--${activeTheme}`).toggleClass('m-active');
       })
-      .on('click', '.js-tablist__event-detail-description-link', function () {
-        $this.trackAnalytics('descriptionLink', this);
-      })
+      .on('click', '.js-tablist__event-detail-description-link', this.trackAnalytics)
       .on('hidden.bs.collapse', '.collapse', this.pauseVideoIfExists);
   }
   showTabDetail = (el) => {
@@ -90,7 +90,54 @@ class TabsList {
     pauseVideosByReference($(this).find('.is-playing'));
   }
   renderFirstTab = () => _renderFirstTab.call(this);
-  trackAnalytics = (type, el) => _trackAnalytics.apply(this, [type, el]);
+  // trackAnalytics = (type, el) => _trackAnalytics.apply(this, [type, el]);
+  trackAnalytics = (e) => {
+    e.preventDefault();
+    const $target = $(e.target);
+    const $this = $target.closest('.js-tablist__event-detail-description-link');
+    let linkParentTitle = '';
+    let trackingObj = {};
+    const dwnType = 'ungated';
+    let eventType = 'content-load';
+    const linkType = $this.attr('target') === '_blank'?'external':'internal';
+    const linkSection = $this.data('link-section');
+    const linkName = $this.data('link-name');
+    // const videoName = $this.data('video-name');
+    const buttonLinkType = $this.data('button-link-type');
+    const downloadtype = $this.data('download-type');
+    const dwnDocName = $this.data('asset-name');
+    const tabType = $this.data('tab-type') === 'textVideo'?'TextVideoClick':'TextImageClick';
+
+    if(buttonLinkType==='secondary' && downloadtype ==='download'){
+      linkParentTitle = `CTA-Download-pdf_${dwnDocName}`;
+      eventType = 'download';
+    }
+
+    if(buttonLinkType==='link' && downloadtype ==='download'){
+      linkParentTitle = `Text hyperlink - Download-pdf_${dwnDocName}`;
+      eventType = 'download';
+    }
+
+    if(downloadtype ==='download'){
+      trackingObj = {
+        linkType,
+        linkSection,
+        linkParentTitle,
+        linkName,
+        dwnDocName,
+        dwnType,
+        eventType
+      };
+      trackAnalytics(trackingObj, 'linkClick', tabType, undefined, false);
+    }
+
+    if($this.attr('target')==='_blank'){
+      window._satellite.track('linkClick');
+    }
+
+    window.open($this.attr('href'), $this.attr('target'));
+  }
+
   init() {
     this.initCache();
     this.bindEvents();
