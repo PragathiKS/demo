460021c433ff98bb8eccbe74132253c062bef61f
Ashwani Payal <ashwani.payal@publicissapient.com>
2020-04-15 13:29:54 +0530

    SMAR - 9554

---
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/anchor/Anchor.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+import { scrollToElement } from '../../../scripts/common/common';
+
+class Anchor {
+  constructor({ el }) {
+    this.root = $(el);
+  }
+  cache = {};
+  initCache() {
+    this.cache.$anchorLink = this.root.find('a');
+  }
+  bindEvents() {
+    const { $anchorLink } = this.cache;
+    $anchorLink.on('click', this.scrollToSection);
+  }
+
+  scrollToSection = e => {
+    e.preventDefault();
+    const $this = $(e.target);
+    const anchorId = $this.data('link-section');
+    scrollToElement(null,`#${anchorId}`);
+  };
+
+  init() {
+    /* Mandatory method */
+    this.initCache();
+    this.bindEvents();
+  }
+}
+
+export default Anchor;
