1ce0cdfe5b2d93ac6f2bef579483c1f396be4719
sebansala <akash.bansal@tetrapak.com>
2020-04-22 22:13:19 +0530

    review comments fixes

--- tetrapak-publicweb/ui.apps/src/main/content/jcr_root/apps/publicweb/components/content/tabslist/clientLibs/listener.js
+++ tetrapak-publicweb/ui.apps/src/main/content/jcr_root/apps/publicweb/components/content/tabslist/clientLibs/listener.js
@@ -1,24 +1,107 @@
-(function(window, $) {
-  var SELECT_REQUIRED_VALIDATOR = "pw.semiauto",
-    foundationReg = $(window).adaptTo("foundation-registry");
-  foundationReg.register("foundation.validation.validator", {
-    selector: 'foundation-autocomplete[data-foundation-validation="pw.semiauto"]',
-    validate: function(el) {
-      var value = $(el).val();
+(function ($, $document) {
+    "use strict";
 
-      if (!value || !value.match("^/content")) {
-        return "Please enter valid existing relative page path only.";
-      }else{
-		var status = (function(){ return $.ajax({
-        type: 'GET',
-        url: value + '.json',
-        async: false}).status;
-      })();
-      if (status !== 200) {
-			 return "Please enter valid existing relative page path only.";
-      }
-      }
+    $document.on("dialog-ready", function() {
+      setTimeout(function(){
+        var $tabLayouts = $('.pw-tablist-multi');
+        if($tabLayouts){
+           $tabLayouts.each(function (i, tabLayout) {
+               var $tabLayoutCurrVal = $(tabLayout).val();
+               var $listContainers = $(tabLayout).parent().parent().find(".tabLayout-showhide-target");
+               if($listContainers){
+                   $listContainers.each(function (i, listContainer) {
+                   	var $showHideValue = $(listContainer).data("showhidetargetvalue");
+                   	if($showHideValue ==  $tabLayoutCurrVal){
+                        $(listContainer).removeClass("hide");
+                    }
+                   });
+               }
+          });
 
-    }
-  });
-})(window, jQuery);
+        }
+        var $vedioTypes = $('.pw-vedio-showhide');
+        if($vedioTypes){
+           $vedioTypes.each(function (i, vedioType) {
+               var $vedioTypeCurrVal = $(vedioType).val();
+               var $vedioContainers = $(vedioType).parent().parent().find(".videoType-showhide-target");
+               if($vedioContainers){
+                   $vedioContainers.each(function (i, vedioContainer) {
+                   	var $showHideValue = $(vedioContainer).data("showhidetargetvalue");
+                   	if($showHideValue ==  $vedioTypeCurrVal){
+                        $(vedioContainer).removeClass("hide");
+                    }
+                   });
+               }
+          });
+
+        }
+       }, 500);
+    });
+
+    $(document).on('foundation-contentloaded', function() {
+        var $tabLayouts = $('.pw-tablist-multi');
+        var $tabManual = $(".pw-tablist-manual-multi")
+        if($tabManual){
+            var $tabManualAddButton =  $tabManual.children('.coral3-Button');
+           $tabManualAddButton.bind( "click", function(e) {
+            setTimeout(function(){
+         		$tabManual.children(".coral3-Multifield-item").last().find(".pw-tablist-multi").change();
+                $tabManual.children(".coral3-Multifield-item").last().find(".pw-vedio-showhide").change();
+            }, 500);
+        });
+        }
+
+        if($tabLayouts){
+             $tabLayouts.on('change', function(event){
+               var $listContainers = $(this).parent().parent().find(".tabLayout-showhide-target");
+               if($listContainers){
+                   $listContainers.each(function (i, listContainer) {
+                   	var $showHideValue = $(listContainer).data("showhidetargetvalue");
+                   	if($showHideValue ==  event.target.value){
+                        $(listContainer).removeClass("hide");
+                    }
+                     else{
+                         $(listContainer).addClass("hide");
+                      }
+                   });
+               }
+             });
+        }
+
+		 var contentTypes = $('.tablist-contenttype');
+
+         if(contentTypes){
+             contentTypes.on('change', function(event){
+                 var contentTypesCurrVal = $(contentTypes).val();
+                   var tabsData = $(this).parent().parent().find(".contentType-showhide-target");
+                    tabsData.each(function (i, tabData) {
+                        if($(tabData).data("showhidetargetvalue") != contentTypesCurrVal){
+                            $(tabData).find(".coral3-Multifield-item").remove();
+                        }
+                        $(".pw-tablist-manual-multi").children('.coral3-Button').removeAttr("disabled");
+                        $(".pw-tablist-semi-multi").children('.coral3-Button').removeAttr("disabled");
+                    });
+             });
+        }
+
+        var $vedioTypes = $('.pw-vedio-showhide');
+        if($vedioTypes){
+             $vedioTypes.on('change', function(event){
+               var $vedioContainers = $(this).parent().parent().find(".videoType-showhide-target");
+               if($vedioContainers){
+                   $vedioContainers.each(function (i, vedioContainer) {
+                   	var $showHideValue = $(vedioContainer).data("showhidetargetvalue");
+                   	if($showHideValue ==  event.target.value){
+                        $(vedioContainer).removeClass("hide");
+                    }
+                     else{
+                         $(vedioContainer).addClass("hide");
+                      }
+                   });
+               }
+             });
+        }
+
+    });
+
+})($, $(document));
---
+++ tetrapak-publicweb/ui.apps/src/main/content/jcr_root/apps/publicweb/components/content/tabslist/clientLibs/semi-auto-pathvalidator.js
@@ -0,0 +1,24 @@
+(function(window, $) {
+  var SELECT_REQUIRED_VALIDATOR = "pw.semiauto",
+    foundationReg = $(window).adaptTo("foundation-registry");
+  foundationReg.register("foundation.validation.validator", {
+    selector: 'foundation-autocomplete[data-foundation-validation="pw.semiauto"]',
+    validate: function(el) {
+      var value = $(el).val();
+
+      if (!value || !value.match("^/content")) {
+        return "Please enter valid existing relative page path only.";
+      }else{
+		var status = (function(){ return $.ajax({
+        type: 'GET',
+        url: value + '.json',
+        async: false}).status;
+      })();
+      if (status !== 200) {
+			 return "Please enter valid existing relative page path only.";
+      }
+      }
+
+    }
+  });
+})(window, jQuery);
