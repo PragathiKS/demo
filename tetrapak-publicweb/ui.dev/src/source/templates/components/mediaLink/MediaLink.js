3450c492437b752a46acf538761113ef65f85a9b
Piyush Kapoor <piykapoo@publicisgroupe.net>
2020-04-28 20:29:25 +0530

    adding code for test cases

--- tetrapak-publicweb/ui.dev/src/source/templates/components/mediaLink/MediaLink.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/mediaLink/MediaLink.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
+import { trackAnalytics } from '../../../scripts/utils/analytics';
+
 
 class MediaLink {
   constructor({ el }) {
@@ -6,23 +8,44 @@ class MediaLink {
   }
   cache = {};
   initCache() {
-    /* Initialize selector cache here */
-    /**
-     * Use "this.root" to find elements within current component
-     * Example:
-     * this.cache.$submitBtn = this.root.find('.js-submit-btn');
-     */
+    this.cache.$textImageLink = this.root.find('.js-medialink-analytics');
   }
   bindEvents() {
-    /* Bind jQuery events here */
-    /**
-     * Example:
-     * const { $submitBtn } = this.cache;
-     * $submitBtn.on('click', () => { ... });
-     */
+    this.cache.$textImageLink.on('click', this.trackAnalytics);
   }
+
+  trackAnalytics = (e) => {
+    e.preventDefault();
+    const $target = $(e.target);
+    const $this = $target.closest('.js-medialink-analytics');
+    
+    const linkParentTitle = '';
+    let trackingObj = {};
+    const linkType = $this.hasClass('icon-external') ? 'external':'internal';
+    const linkName = $this.data('link-name');
+    const dwnDocName = $this.data('asset-name');
+    const dwnType = 'ungated';
+    const eventType = 'download';
+    const downloadtype = $this.hasClass('icon-download') ? 'download':'_download';
+    const linkSection = `medialink-${downloadtype}`;
+    if(downloadtype ==='download'){
+      trackingObj = {
+        linkType,
+        linkSection,
+        linkParentTitle,
+        linkName,
+        dwnDocName,
+        dwnType,
+        eventType
+      };
+      trackAnalytics(trackingObj, 'linkClick', 'downloadClick', undefined, false);
+    } else {
+      window._satellite.track('linkClick');
+    }
+    window.open($this.attr('href'), $this.attr('target'));
+  }
+
   init() {
-    /* Mandatory method */
     this.initCache();
     this.bindEvents();
   }
--- tetrapak-publicweb/ui.dev/src/source/templates/components/mediaLink/MediaLink.spec.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/mediaLink/MediaLink.spec.js
@@ -1,9 +1,30 @@
+import $ from 'jquery';
 import MediaLink from './MediaLink';
 
 describe('MediaLink', function () {
   before(function () {
-    this.mediaLink = new MediaLink({
+    $(document.body).empty().html('<a href="#" class="MediaLink js-medialink-analytics">Link</a>');
+    this.MediaLink = new MediaLink({
       el: document.body
     });
+    this.initSpy = sinon.spy(this.MediaLink, 'init');
+    this.analyticsSpy = sinon.spy(this.MediaLink, 'trackAnalytics');
+    window.digitalData = {};
+    window._satellite = {
+      track() { /* Dummy method */ }
+    };
+    this.MediaLink.init();
   });
-});
+  after(function () {
+    $(document.body).empty();
+    this.initSpy.restore();
+    this.analyticsSpy.restore();
+  });
+  it('should initialize', function () {
+    expect(this.initSpy.called).to.be.true;
+  });
+  it('should track analytics on click of "MediaLink" button', function () {
+    $('.js-medialink-analytics').trigger('click');
+    expect(this.analyticsSpy.called).to.be.true;
+  });
+})
