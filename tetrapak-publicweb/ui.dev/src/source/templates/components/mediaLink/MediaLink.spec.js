705f2c0713ab93782a93807fe437dc8d0d20891b
Piyush Kapoor <piykapoo@publicisgroupe.net>
2020-04-28 17:03:45 +0530

    changes code

---
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/mediaLink/MediaLink.spec.js
@@ -0,0 +1,9 @@
+import MediaLink from './MediaLink';
+
+describe('MediaLink', function () {
+  before(function () {
+    this.mediaLink = new MediaLink({
+      el: document.body
+    });
+  });
+});
