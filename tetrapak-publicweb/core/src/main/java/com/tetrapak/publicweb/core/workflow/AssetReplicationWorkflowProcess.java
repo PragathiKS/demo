5b90b97afb8ea66143c58c7432da244c99ee6e59
sebansala <akash.bansal@publicissapient.com>
2020-05-09 03:41:29 +0530

    updated test classes for Workflow process and changed logic to use resource than Node

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/workflow/AssetReplicationWorkflowProcess.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/workflow/AssetReplicationWorkflowProcess.java
@@ -1,14 +1,17 @@
 package com.tetrapak.publicweb.core.workflow;
 
-import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+
+import org.apache.sling.api.resource.ModifiableValueMap;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import com.adobe.granite.workflow.WorkflowSession;
 import com.adobe.granite.workflow.exec.WorkItem;
 import com.adobe.granite.workflow.exec.WorkflowProcess;
@@ -35,40 +38,41 @@ public class AssetReplicationWorkflowProcess implements WorkflowProcess {
     @Override
     public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap paramMetaDataMap) {
         LOGGER.debug("inside execute");
-        String payloadPath = workItem.getWorkflowData().getPayload().toString();
-        String metadataNodePath = payloadPath.replace("renditions/original", "metadata");
-        ResourceResolver resourceResolver = workflowSession.adaptTo(ResourceResolver.class);
+        final String payloadPath = workItem.getWorkflowData().getPayload().toString();
+        final String metadataNodePath = payloadPath.replace("renditions/original", "metadata");
+        final ResourceResolver resourceResolver = workflowSession.adaptTo(ResourceResolver.class);
         String pathToCheck = DEFAULT_PATH_TO_CHECK;
         if (paramMetaDataMap.containsKey(TYPE_PROCESS_ARGS)) {
             pathToCheck = paramMetaDataMap.get(TYPE_PROCESS_ARGS, String.class);
         }
         try {
             if (resourceResolver != null) {
-                    replicateAsset(metadataNodePath, resourceResolver, pathToCheck);
+                replicateAsset(metadataNodePath, resourceResolver, pathToCheck);
             }
-        } catch (RepositoryException | ReplicationException e) {
+        } catch (final ReplicationException e) {
             LOGGER.error("Error occured while asset replication :: {}", e.getMessage(), e);
         }
     }
 
     /**
      * Replicate the asset in payload
-     * 
+     *
      * @param metadataNodePath
      * @param resourceResolver
      * @param pathToCheck
-     * 
+     *
      * @throws RepositoryException
      * @throws ReplicationException
      */
-    private void replicateAsset(String metadataNodePath, ResourceResolver resourceResolver, String pathToCheck)
-            throws RepositoryException, ReplicationException {
-        Node node = resourceResolver.getResource(metadataNodePath).adaptTo(Node.class);
-        if (metadataNodePath.contains(pathToCheck) && (null != node && node.hasProperty(PWConstants.REPLICATION_PXP))) {
-            node.getProperty(PWConstants.REPLICATION_PXP).remove();
-            String assetPath = metadataNodePath.replace("/jcr:content/metadata", "");
+    private void replicateAsset(final String metadataNodePath, final ResourceResolver resourceResolver,
+            final String pathToCheck) throws ReplicationException {
+        final Resource resource = resourceResolver.getResource(metadataNodePath);
+        final ModifiableValueMap map = resource.adaptTo(ModifiableValueMap.class);
+        if (metadataNodePath.contains(pathToCheck) && map.containsKey(PWConstants.REPLICATION_PXP)) {
+            map.remove(PWConstants.REPLICATION_PXP);
+            final String assetPath = metadataNodePath.replace("/jcr:content/metadata", "");
             LOGGER.debug("trigerring replication : assetPath {} ", assetPath);
-            replicator.replicate( resourceResolver.adaptTo(Session.class), ReplicationActionType.ACTIVATE, assetPath);
+            replicator.replicate(resourceResolver.adaptTo(Session.class), ReplicationActionType.ACTIVATE, assetPath);
             LOGGER.debug("asset has been replicated : assetPath {} ", assetPath);
         }
     }
