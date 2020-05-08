cc6f5db755d1094bdc765a373ee161b4d5d0c451
Bimmi Soi <bimmi.soi@publicissapient.com>
2020-05-08 11:46:13 +0530

    PXP options oraganism

---
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/PXPOptionsModel.java
@@ -0,0 +1,170 @@
+package com.tetrapak.publicweb.core.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.settings.SlingSettingsService;
+
+import com.tetrapak.publicweb.core.beans.pxp.FeatureOption;
+import com.tetrapak.publicweb.core.models.multifield.TabModel;
+import com.tetrapak.publicweb.core.services.DynamicMediaService;
+import com.tetrapak.publicweb.core.utils.GlobalUtil;
+
+/**
+ * The Class PXPOptionsModel.
+ */
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class PXPOptionsModel {
+
+    /** The resource. */
+    @Self
+    private Resource resource;
+
+    /** The heading. */
+    @ValueMapValue
+    @Default(values = "Options")
+    private String heading;
+
+    /** The pw theme. */
+    @ValueMapValue
+    @Default(values = "grayscale-white")
+    private String pwTheme;
+
+    /** The pw display. */
+    @ValueMapValue
+    @Default(values = "display-row")
+    private String pwDisplay;
+
+    /** The anchor id. */
+    @ValueMapValue
+    private String anchorId;
+
+    /** The anchor title. */
+    @ValueMapValue
+    private String anchorTitle;
+
+    /** The tabs. */
+    private List<TabModel> tabs = new ArrayList<>();
+
+    /** The sling settings service. */
+    @OSGiService
+    private SlingSettingsService slingSettingsService;
+
+    /** The dynamic media service. */
+    @OSGiService
+    private DynamicMediaService dynamicMediaService;
+
+    /** The Constant AUTHOR. */
+    private static final String AUTHOR = "author";
+
+    /**
+     * Inits the.
+     */
+    @PostConstruct
+    protected void init() {
+        ProductModel productModel = resource.adaptTo(ProductModel.class);
+        if (Objects.nonNull(productModel)) {
+            List<FeatureOption> featureOptions = productModel.getOptions();
+            for (FeatureOption feature : featureOptions) {
+                TabModel tab = new TabModel();
+                tab.setTitle(feature.getHeader());
+                tab.setSubTitle(feature.getName());
+                tab.setDescription(feature.getBody());
+                if (StringUtils.isNotBlank(feature.getImage())) {
+                    tab.setFileReference(feature.getImage());
+                    tab.setAlt(feature.getName());
+                    tab.setTabType("imageText");
+                } else if (Objects.nonNull(feature.getVideo())) {
+                    String damVideoPath = feature.getVideo().getSrc();
+                    damVideoPath = setDynamicMediaVideoPath(damVideoPath);
+                    tab.setDamVideoPath(damVideoPath);
+                    tab.setThumbnailPath(feature.getVideo().getPoster());
+                    tab.setThumbnailAltText(feature.getName());
+                    tab.setTabType("videoText");
+                    tab.setVideoSource("damVideo");
+                }
+                tabs.add(tab);
+            }
+        }
+    }
+
+    /**
+     * Sets the dynamic media video path.
+     *
+     * @param damVideoPath
+     *            the dam video path
+     * @return the string
+     */
+    private String setDynamicMediaVideoPath(String damVideoPath) {
+        if (!slingSettingsService.getRunModes().contains(AUTHOR) && null != dynamicMediaService) {
+            damVideoPath = GlobalUtil.getVideoUrlFromScene7(damVideoPath, dynamicMediaService);
+        }
+        return damVideoPath;
+    }
+
+    /**
+     * Gets the heading.
+     *
+     * @return the heading
+     */
+    public String getHeading() {
+        return heading;
+    }
+
+    /**
+     * Gets the pw theme.
+     *
+     * @return the pw theme
+     */
+    public String getPwTheme() {
+        return pwTheme;
+    }
+
+    /**
+     * Gets the pw display.
+     *
+     * @return the pw display
+     */
+    public String getPwDisplay() {
+        return pwDisplay;
+    }
+
+    /**
+     * Gets the anchor id.
+     *
+     * @return the anchor id
+     */
+    public String getAnchorId() {
+        return anchorId;
+    }
+
+    /**
+     * Gets the anchor title.
+     *
+     * @return the anchor title
+     */
+    public String getAnchorTitle() {
+        return anchorTitle;
+    }
+
+    /**
+     * Get tabs.
+     *
+     * @return the list
+     */
+    public List<TabModel> getTabs() {
+        return new ArrayList<>(tabs);
+    }
+
+}
---
+++ tetrapak-publicweb/core/src/test/java/com/tetrapak/publicweb/core/models/PXPOptionsModelTest.java
@@ -0,0 +1,124 @@
+package com.tetrapak.publicweb.core.models;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.sling.api.resource.Resource;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.tetrapak.publicweb.core.constants.PWConstants;
+import com.tetrapak.publicweb.core.services.DynamicMediaService;
+import com.tetrapak.publicweb.core.services.impl.DynamicMediaServiceImpl;
+
+import io.wcm.testing.mock.aem.junit.AemContext;
+
+public class PXPOptionsModelTest {
+    /** The context. */
+    @Rule
+    public AemContext context = new AemContext();
+
+    /**
+     * The Constant PRODUCTS_DATA.
+     */
+    private static final String PRODUCTS_DATA = "/product/test-Products.json";
+
+    /** The Constant EN_LANGUAGE_RESOURCE_CONTENT. */
+    private static final String EN_LANGUAGE_RESOURCE_CONTENT = "/product/en.json";
+
+    /**
+     * The Constant EN_LANGUAGE_CONTENT_ROOT.
+     */
+    private static final String EN_LANGUAGE_CONTENT_ROOT = "/content/tetrapak/publicweb/lang-master/en";
+
+    /** The Constant TEST_RESOURCE_CONTENT1. */
+    private static final String TEST_RESOURCE_CONTENT = "/pxpoptions/test-content.json";
+
+    /**
+     * The Constant FILLING_MACHINE_CONTENT_ROOT.
+     */
+    private static final String FILLING_MACHINE_CONTENT_ROOT = "/content/tetrapak/publicweb/lang-master/en/filling-machine";
+
+    /** The model class. */
+    Class<PXPOptionsModel> modelClass = PXPOptionsModel.class;
+
+    /** The model. */
+    private PXPOptionsModel model;
+
+    /**
+     * The Constant PXP_FEATURES.
+     */
+    private static final String RESOURCE = FILLING_MACHINE_CONTENT_ROOT + "/jcr:content/pxpoptions";
+
+    /** The resource. */
+    private Resource resource;
+
+    private DynamicMediaService dynamicMediaService;
+
+    /** THE DYNAMIC MEDIA CONF. */
+    String[] dynamicMediaConfMap = {
+            "getstarted-desktop=1440\\,300,getstarted-mobileL=414\\,259\\,0.333\\,0\\,0.333\\,1,getstarted-mobileP=414\\,259\\,0.333\\,0\\,0.333\\,1" };
+
+    /**
+     * Sets the up.
+     *
+     * @throws Exception
+     *             the exception
+     */
+    @Before
+    public void setUp() throws Exception {
+
+        // Dynamic Media Serivce
+        dynamicMediaService = new DynamicMediaServiceImpl();
+        final Map<String, Object> configuraionServiceConfig = new HashMap<String, Object>();
+        configuraionServiceConfig.put("rootPath", "/tetrapak");
+        configuraionServiceConfig.put("dynamicMediaConfMap", dynamicMediaConfMap);
+        configuraionServiceConfig.put("imageServiceUrl", "https://s7g10.scene7.com/is/image");
+        configuraionServiceConfig.put("videoServiceUrl", "https://s7g10.scene7.com/is/content");
+        context.registerInjectActivateService(dynamicMediaService, configuraionServiceConfig);
+
+        // Set run modes
+        context.runMode("publish");
+
+        context.load().json(PRODUCTS_DATA, PWConstants.PXP_ROOT_PATH);
+        context.load().json(EN_LANGUAGE_RESOURCE_CONTENT, EN_LANGUAGE_CONTENT_ROOT);
+        context.load().json(TEST_RESOURCE_CONTENT, FILLING_MACHINE_CONTENT_ROOT);
+        context.addModelsForClasses(modelClass);
+    }
+
+    /**
+     * Test model, resource and all getters of the ImageTextBanner model.
+     *
+     * @throws Exception
+     *             the exception
+     */
+    @Test
+    public void testBannerMethods() throws Exception {
+        resource = context.currentResource(RESOURCE);
+        model = resource.adaptTo(modelClass);
+        assertEquals("PXPOptions", "Anchor title", model.getAnchorTitle());
+        assertEquals("PXPOptions", "anchor123", model.getAnchorId());
+        assertEquals("PXPOptions", "grayscale-white", model.getPwTheme());
+        assertEquals("PXPOptions", "display-row", model.getPwDisplay());
+        assertEquals("PXPOptions", "Options", model.getHeading());
+        assertEquals("PXPOptions",
+                "<p>The unit consists of a control cabinet, a heating element to sterilize air (or an inert gas) and a nozzle which injects the sterile air (or inert gas) to the product to create froth. When the froth settles down, it leaves a residual headspace which enables the content to be shaken and mixed. Using this method, products can be filled by weight so the choice of volumes is wider</p><p> </p>",
+                model.getTabs().get(0).getDescription());
+        assertEquals("PXPOptions", "Headspace by Injection", model.getTabs().get(0).getSubTitle());
+        assertEquals("PXPOptions", "videoText", model.getTabs().get(0).getTabType());
+        assertEquals("PXPOptions", "Headspace by Injection", model.getTabs().get(0).getThumbnailAltText());
+        assertEquals("PXPOptions",
+                "/content/dam/tetrapak/publicweb/pxp/fillingmachines/equipment1272/image/thumbnail-4322_0003.jpg",
+                model.getTabs().get(0).getThumbnailPath());
+        assertEquals("PXPOptions", "a3_option_headspace_for_injection_mp4_1350227217",
+                model.getTabs().get(0).getVideoName());
+        assertEquals("PXPOptions", "damVideo", model.getTabs().get(0).getVideoSource());
+        assertEquals("PXPOptions",
+                "https://s7g10.scene7.com/is/content/tetrapak/a3_option_headspace_for_injection_mp4_1350227217",
+                model.getTabs().get(0).getDamVideoPath());
+
+    }
+}
