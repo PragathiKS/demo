8021c60db160a380c6312f67357b259fabd08d5c
Ashwani Payal <ashwani.payal@publicissapient.com>
2020-05-14 14:22:22 +0530

    test cases updated

--- tetrapak-publicweb/ui.dev/src/source/templates/components/megamenusolution/MegaMenuSolution.spec.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/megamenusolution/MegaMenuSolution.spec.js
@@ -11,6 +11,7 @@ describe('MegaMenuSolution', function () {
     this.initSpy = sinon.spy(this.megaMenu, 'init');
     this.handleOpenEventSpy = sinon.spy(this.megaMenu, 'handleOpenEvent');
     this.handleCloseEventSpy = sinon.spy(this.megaMenu, 'handleCloseEvent');
+    this.trackAnalyticsSpy = sinon.spy(this.megaMenu, 'trackAnalytics');
     this.mobileModeStub = sinon.stub(this.megaMenu, 'isMobileMode');
     this.mobileModeStub.returns(true);
 
@@ -20,6 +21,7 @@ describe('MegaMenuSolution', function () {
     $(document.body).empty();
     this.initSpy.restore();
     this.handleOpenEventSpy.restore();
+    this.trackAnalyticsSpy.restore();
     this.mobileModeStub.restore();
   });
   it('should initialize', function () {
@@ -34,5 +36,9 @@ describe('MegaMenuSolution', function () {
   it('should call handleCloseEvent on click', function () {
     $('.js-close-menu').trigger('click');
     expect(this.megaMenu.handleCloseEvent.called).to.be.true;
+  });
+  it('should call trackAnalytics on click', function () {
+    $('.js-navigation-Link').trigger('click');
+    expect(this.megaMenu.trackAnalytics.called).to.be.true;
   });
 });
