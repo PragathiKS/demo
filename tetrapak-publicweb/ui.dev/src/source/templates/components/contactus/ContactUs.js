8f31a5e937d40bfe673fbc8bde380f616b40acc3
Shashi Mehra <shamehra@publicisgroupe.net>
2020-05-21 01:11:19 +0530

    adding call to submit btn

--- tetrapak-publicweb/ui.dev/src/source/templates/components/contactus/ContactUs.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/contactus/ContactUs.js
@@ -1,5 +1,7 @@
 import $ from 'jquery';
-// import { ajaxMethods, API_CONTACTUS_FORM } from '../../../scripts/utils/constants';
+
+import { ajaxWrapper } from '../../../scripts/utils/ajax';
+import { ajaxMethods, API_CONTACTUS_FORM, REG_EMAIL } from '../../../scripts/utils/constants';
 
 class ContactUs {
   constructor({ el }) {
@@ -14,16 +16,48 @@ class ContactUs {
      * this.cache.$submitBtn = this.root.find('.js-submit-btn');
      */
     this.cache.$submitBtn = $('button[type="submit"]', this.root);
-    this.cache.$nextbtn = this.root.find('.tpatom-btn');
+    this.cache.$nextbtn = this.root.find('.tpatom-btn[type=button]');
     this.cache.$radio = this.root.find('input[type=radio][name="purposeOfContactOptions"]');
     this.cache.$dropItem = $('.pw-form__dropdown a.dropdown-item', this.root);
     this.cache.$dropdown = $('.pw-form__dropdown', this.root);
+    this.cache.requestPayload = {
+      'purposeOfContact': '',
+      'country' : '',
+      'firstName': '',
+      'lastName': '',
+      'email': '',
+      'message' : '',
+      'countryTitle' : '',
+      'purposeOfContactTitle' : ''
+    };
   }
+
+  validEmail(email) {
+    return REG_EMAIL.test(email);
+  }
+
+  submitForm = () => {
+    ajaxWrapper.getXhrObj({
+      url: API_CONTACTUS_FORM,
+      method: ajaxMethods.POST,
+      data: {'inputJson': this.cache.requestPayload}
+    }).done(
+      () => {
+        $('.tab-pane' , this.root).removeClass('active');
+        $('#cf-step-final', this.root).addClass('active');
+      }
+    );
+  }
+
   bindEvents() {
     /* Bind jQuery events here */
     const self = this;
     this.cache.$radio.on('change', function() {
-      $('input[type=hidden][name="purposeOfContact"]').val(this.value);
+      const value = this.value;
+      const id = $(this).attr('id');
+      $('input[type=hidden][name="purposeOfContact"]').val(value);
+      self.cache.requestPayload['purposeOfContact'] = id;
+      self.cache.requestPayload['purposeOfContactTitle'] = value;
     });
 
     this.cache.$nextbtn.click(function (e) {
@@ -35,12 +69,17 @@ class ContactUs {
       if(!$(this).hasClass('previousbtn') && (input.length > 0 || textarea.length > 0)) {
         $('input, textarea', tab).each(function () {
           const fieldName = $(this).attr('name');
-          $('p.' + fieldName).text($(this).val());
-          if ($(this).prop('required') && $(this).val() === '') {
+          $('div.' + fieldName).text($(this).val());
+          if(fieldName in self.cache.requestPayload) {
+            self.cache.requestPayload[fieldName] = $(this).val();
+          }
+          if (($(this).prop('required') && $(this).val() === '') || (fieldName === 'email') && !self.validEmail($(this).val())) {
             isvalid = false;
             e.preventDefault();
             e.stopPropagation();
             $(this).closest('.form-group, .formfield').addClass('field-error');
+          } else {
+            $(this).closest('.form-group, .formfield').removeClass('field-error');
           }
         });
       }
@@ -54,32 +93,17 @@ class ContactUs {
     this.cache.$submitBtn.click(function (e) {
       e.preventDefault();
       e.stopPropagation();
-      const target = $(this).data('target');
-      $('.tab-pane').removeClass('active');
-      $(target).addClass('active');
-      var isvalid = true; //eslint-disable-line
-      // const parentTab = e.target.closest('.tab-pane');
+      var isvalid = true;
+      const honeyPotFieldValue = $('#contactUsHoneyPot', self.root).val();
       $('input, textarea').each(function () {
-        if ($(this).val() === '') {
-          isvalid = false; //eslint-disable-line
-          e.preventDefault();
-          e.stopPropagation();
-          $(this).closest('.form-group').addClass('field-error');
+        if ($(this).prop('required') && $(this).val() === '') {
+          isvalid = false;
+          $(this).closest('.form-group, .formfield').addClass('field-error');
         }
       });
-      // if (isvalid) {
-      //   e.preventDefault();
-      //   ajaxWrapper.getXhrObj({
-      //     url: API_CONTACTUS_FORM,
-      //     method: ajaxMethods.GET,
-      //     data: $('form.pw-form', self.root).serialize()
-      //   }).done(
-      //     () => {
-      //       $('.tab-pane', self.root).removeClass('active');
-      //       $('#cf-step-final', self.root).addClass('active');
-      //     }
-      //   );
-      // }
+      if (isvalid  && !honeyPotFieldValue) {
+        self.submitForm();   
+      }
     });
 
     this.cache.$dropdown.click(function () {
@@ -90,9 +114,12 @@ class ContactUs {
     this.cache.$dropItem.click(function (e) {
       e.preventDefault();
       const country = $(this).data('country');
+      const countryTitle = $(this).data('countrytitle');
       const parentDrop = $(this).closest('.dropdown');
       $('.dropdown-toggle', parentDrop).text(country);
       $('input', parentDrop).val(country);
+      self.cache.requestPayload['country'] = country;
+      self.cache.requestPayload['countryTitle'] = countryTitle;
       self.cache.$dropItem.removeClass('active');
       $(this).addClass('active');
       $(parentDrop).removeClass('show');
