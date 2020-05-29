e16ce799c7710be50fa163c15d1c096dac441eb7
Ashwani Payal <ashwani.payal@publicissapient.com>
2020-05-29 12:02:28 +0530

    code moved to common

--- tetrapak-publicweb/ui.dev/src/source/templates/components/textImage/TextImage.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/textImage/TextImage.js
@@ -1,7 +1,6 @@
 import $ from 'jquery';
-import { trackAnalytics } from '../../../scripts/utils/analytics';
 import { isExternal, isDownloable } from '../../../scripts/utils/updateLink';
-import { getDocType } from '../../../scripts/common/common';
+import { getLinkClickAnalytics } from '../../../scripts/common/common';
 
 class TextImage {
   constructor({ el }) {
@@ -32,54 +31,7 @@ class TextImage {
 
   trackAnalytics = e => {
     e.preventDefault();
-    const $target = $(e.target);
-    const $this = $target.closest('.js-textImage-analytics');
-    const downloadtype = $this.data('download-type');
-    let trackingKey = 'linkClick';
-    let linkParentTitle = `${$this.data('anchor-type')}_${$this.data(
-      'image-title'
-    )}`;
-    let linkSection = $this.data('link-section');
-    const linkName = $this.data('link-name');
-    const linkType =
-      $this.attr('target') === '_blank' ? 'external' : 'internal';
-
-    const trackingObj = {};
-    let eventObject = null;
-
-    if (downloadtype === 'download') {
-      trackingObj['dwnDocName'] = $this.data('asset-name');
-      linkSection = `${$this.data('anchor-type')}_Download`;
-      linkParentTitle = `${$this.data('anchor-type')}_Download_${getDocType(
-        $this.attr('href')
-      )}_${$this.data('image-title')}`;
-      trackingObj['eventType'] = 'download';
-      trackingObj['dwnType'] = 'ungated';
-      trackingKey = 'downloadClick';
-    }
-
-    if (downloadtype !== 'download') {
-      eventObject = {
-        eventType: 'linkClick',
-        event: 'Text & Image'
-      };
-    }
-
-    trackingObj['linkSection'] = linkSection;
-    trackingObj['linkName'] = linkName;
-    trackingObj['linkParentTitle'] = linkParentTitle;
-    trackingObj['linkType'] = linkType;
-
-    trackAnalytics(
-      trackingObj,
-      'linkClick',
-      trackingKey,
-      undefined,
-      false,
-      eventObject
-    );
-
-    window.open($this.attr('href'), $this.attr('target'));
+    getLinkClickAnalytics(e,'image-title','Text & Image','.js-textImage-analytics');
   };
 
   init() {
