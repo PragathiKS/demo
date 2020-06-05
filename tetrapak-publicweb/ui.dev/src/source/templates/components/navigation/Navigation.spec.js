4eeba788b4c1c1fcfbcd004b6f189227104b23be
Ashwani Payal <ashwani.payal@publicissapient.com>
2020-06-05 18:14:12 +0530

    test cases added

--- tetrapak-publicweb/ui.dev/src/source/templates/components/navigation/Navigation.spec.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/navigation/Navigation.spec.js
@@ -1,25 +1,33 @@
 import $ from 'jquery';
 import Navigation from './Navigation';
+import navigationTemplate from '../../../test-templates-hbs/navigation.hbs';
 
 describe('Navigation', function() {
   before(function() {
     this.enableTimeouts(false);
     $(document.body)
       .empty()
-      .html(
-        '<div class="body-content"><div class="sticky-section-menu"><div data-selected-header="Insights"></div></div></div>'
-      );
+      .html(navigationTemplate());
     this.navigation = new Navigation({
       el: document.body
     });
     this.initSpy = sinon.spy(this.navigation, 'init');
+    this.openMobileSectionMenuSpy = sinon.spy(
+      this.navigation,
+      'openMobileSectionMenu'
+    );
     this.navigation.init();
   });
   after(function() {
     $(document.body).empty();
     this.initSpy.restore();
+    this.openMobileSectionMenuSpy.restore();
   });
   it('should initialize', function() {
     expect(this.navigation.init.called).to.be.true;
+  });
+  it('should call openMobileSectionMenu on click', function() {
+    $('.collapse-button').trigger('click');
+    expect(this.navigation.openMobileSectionMenu.called).to.be.true;
   });
 });
