3ce42988f9951874cf4a28479948f2e6fb172bfb
Ashwani Payal <ashwani.payal@publicissapient.com>
2020-06-09 13:06:03 +0530

    test cases update

--- tetrapak-publicweb/ui.dev/src/source/templates/components/navigation/Navigation.spec.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/navigation/Navigation.spec.js
@@ -11,6 +11,8 @@ describe('Navigation', function() {
     this.navigation = new Navigation({
       el: document.body
     });
+    $('body').append( '<a class=\'js-tp-pw-header-logo-digital-data js-tp-pw-header-item js-tp-pw-header-item-desktop\' data-link-section=\'Header Menu Navigation\' data-link-name=\'Insights\'>Insights</a>' );
+    $('body').attr('style','overflow : auto');
     this.initSpy = sinon.spy(this.navigation, 'init');
     this.sectionMenuToggleClickSpy = sinon.spy(
       this.navigation,
@@ -29,5 +31,10 @@ describe('Navigation', function() {
   it('should call sectionMenuToggleClick on click', function() {
     $('.collapse-button').trigger('click');
     expect(this.navigation.sectionMenuToggleClick.called).to.be.true;
+  });
+  it('add class overflow auto in body when section menu closed ', function() {
+    $('.collapse-button').removeClass('collapsed');
+    $('.collapse-button').trigger('click');
+    expect(document.body.style.overflow).to.equal('auto');
   });
 });
