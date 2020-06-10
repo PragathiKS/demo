e5c5137888c2924cb47d440de4912c6702d869dc
Anshika <anshika1185@gmail.com>
2020-06-10 17:42:40 +0530

    Market selector fixed and navigation mobile fix

--- tetrapak-publicweb/ui.dev/src/source/templates/components/marketselector/Marketselector.spec.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/marketselector/Marketselector.spec.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import Marketselector from './Marketselector';
+import { trackAnalytics } from '../../../scripts/utils/analytics';
 import marketSelectorTemplate from '../../../test-templates-hbs/marketSelector.hbs';
 
 describe('Marketselector', function () {
@@ -10,18 +11,27 @@ describe('Marketselector', function () {
     });
     this.initSpy = sinon.spy(this.marketselector, 'init');
     this.hidePopUpSpy = sinon.spy(this.marketselector, 'hidePopUp');
+    this.trackAnalyticsSpy = sinon.spy(this.marketselector, 'trackMarketSelectorAnalytics');
+    this.openStub = sinon.stub(window, 'open');
     this.marketselector.init();
     this.marketselector.showPopup();
+
   });
 
   after(function () {
     $(document.body).empty();
     this.initSpy.restore();
     this.hidePopUpSpy.restore();
+    this.trackAnalyticsSpy.restore();
+    this.openStub.restore();
   });
 
   it('should initialize', function () {
     expect(this.marketselector.init.called).to.be.true;
+  });
+  it('should call track analytics on click', function () {
+    $('.js-lang-selector__btn > a').trigger('click');
+    expect(this.marketselector.trackMarketSelectorAnalytics.called).to.be.true;
   });
 
   it('should close popup when close button is clicked', function () {
