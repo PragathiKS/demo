0100ea05cd9b9ccead247bdf31a1e8bfb4d0f342
Piyush Kapoor <piykapoo@publicisgroupe.net>
2020-06-11 23:39:42 +0530

    adding form

--- tetrapak-publicweb/ui.dev/src/source/templates/components/businessinquiryform/businessinquiryform.analytics.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/businessinquiryform/businessinquiryform.analytics.js
@@ -1,83 +1,75 @@
 import { trackAnalytics } from '../../../scripts/utils/analytics';
 
 export const makeLoad = function () {
-  const digitalData = {
-    form: {
-      formName: 'Business Enquiry',
-      formStep: 'Step 1',
-      formType: '',
-      areaofInterest: '',
-      formField: []
-    },
-    event: {
-      eventType: 'formstart',
-      event: 'Hard Conversion Form'
-    }
+  const formObj = {
+    formName: 'Business Enquiry',
+    formStep: 'Step 1',
+    formType: '',
+    areaofInterest: '',
+    formField: []
   };
-  trackAnalytics(digitalData.form, 'formload', 'formload', undefined, false, digitalData.event);
+  const eventObj = {
+    eventType: 'formstart',
+    event: 'Hard Conversion Form'
+  };
+  trackAnalytics(formObj, 'form', 'formload', undefined, false, eventObj);
 };
 
 
 export const changeStepNext = function (formStep, formType, areaofInterest, dataObj) {
-  const digitalData = {
-    form: {
-      formName: 'Business Enquiry',
-      formStep: formStep,
-      formType: formType,
-      areaofInterest: areaofInterest,
-      formField: []
-    },
-    event: {
-      eventType: formStep + ' complete',
-      event: 'Hard Conversion Form'
-    }
+  const formObj = {
+    formName: 'Business Enquiry',
+    formStep: formStep,
+    formType: formType,
+    areaofInterest: areaofInterest,
+    formField: []
+  };
+  const eventObj = {
+    eventType: formStep + ' complete',
+    event: 'Hard Conversion Form'
   };
   Object.keys(dataObj).forEach(i => {
-    digitalData.form.formField.push({
+    formObj.formField.push({
       formFieldName: i,
       formFieldValue: dataObj[i]
     });
   });
-  trackAnalytics(digitalData.form, formStep + ' complete', formStep + ' complete', undefined, false, digitalData.event);
+  trackAnalytics(formObj, 'form', 'formclick', undefined, false, eventObj);
 };
 
 
 export const loadThankYou = function (formType, areaofInterest, dataObj) {
-  const digitalData = {
-    form: {
-      formName: 'Business Enquiry',
-      formStep: 'Step 4',
-      formType: 'Contact information',
-      areaofInterest: areaofInterest,
-      formField: []
-    },
-    event: {
-      eventType: 'formcomplete',
-      event: 'Hard Conversion Form'
-    }
+  const formObj = {
+    formName: 'Business Enquiry',
+    formStep: 'Step 4',
+    formType: 'Contact information',
+    areaofInterest: areaofInterest,
+    formField: []
+  };
+  const eventObj = {
+    eventType: 'formcomplete',
+    event: 'Hard Conversion Form'
   };
   Object.keys(dataObj).forEach(i => {
-    digitalData.form.formField.push({
+    formObj.formField.push({
       formFieldName: i,
       formFieldValue: dataObj[i]
     });
   });
-  trackAnalytics(digitalData.form, 'formcomplete', 'formcomplete', undefined, false, digitalData.event);
+  trackAnalytics(formObj, 'form', 'formcomplete', undefined, false, eventObj);
 };
 
 
 export const NewPage = function () {
-  const digitalData = {
-    linkClick: {
-      linkType: 'internal',
-      linkSection: 'Hard Conversion Form_button click',
-      linkParentTitle: '',
-      linkName: 'New Request'
-    },
-    event: {
-      eventType: 'linkClick',
-      event: 'Hard Conversion Form'
-    }
+  const linkClickObj = {
+    linkType: 'internal',
+    linkSection: 'Hard Conversion Form_button click',
+    linkParentTitle: '',
+    linkName: 'New Request'
   };
-  trackAnalytics(digitalData.linkClick, 'linkClick', 'formcomplete', undefined, false, digitalData.event);
+  const eventObj = {
+    eventType: 'linkClick',
+    event: 'Hard Conversion Form'
+  };
+  trackAnalytics(linkClickObj, 'linkclick', 'linkclick', undefined, false, eventObj);
 };
