621baa438ff0ec2fcaab9dfda1a5b9ff7374fa37
payalashwani <ashwani.payal@tetrapak.com>
2020-06-12 13:58:27 +0530

    deleting unwnted files

--- tetrapak-publicweb/ui.dev/src/source/templates/components/searchresult/Searchresult.spec.js
+++
@@ -1,9 +0,0 @@
-import Searchresult from './Searchresult';
-
-describe('Searchresult', function () {
-  before(function () {
-    this.searchresult = new Searchresult({
-      el: document.body
-    });
-  });
-});
