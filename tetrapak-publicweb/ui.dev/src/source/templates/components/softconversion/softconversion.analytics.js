f79c3c73acd3764f69bf360d84cd1b53245e93f5
Satish Sharma <satishsharma@Satishs-MacBook-Air.local>
2020-06-25 15:03:56 +0530

    SMAR-12847 analytics changes

--- tetrapak-publicweb/ui.dev/src/source/templates/components/softconversion/softconversion.analytics.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/softconversion/softconversion.analytics.js
@@ -1,6 +1,6 @@
 import { trackAnalytics } from '../../../scripts/utils/analytics';
 
-export const makeLoad = function (label, formName) {
+export const makeLoad = function (label, formName, eventType='formstart') {
   const formObj = {
     formName: formName,
     formStep: 'Step 1',
@@ -8,7 +8,7 @@ export const makeLoad = function (label, formName) {
     formField: []
   };
   const eventObj = {
-    eventType: 'formstart',
+    eventType: eventType,
     event: 'Soft Conversion Form'
   };
   trackAnalytics(formObj, 'form', 'formload', undefined, false, eventObj);
@@ -38,85 +38,74 @@ export const changeStepNext = function (formName, formStep, formType, dataObj) {
 };
 
 
-// export const changeStepPrev = function (formName, formStep, formType, areaofInterest) {
-//   const formObj = {
-//     formName: formName,
-//     formStep: formStep,
-//     formType: formType,
-//     areaofInterest: areaofInterest,
-//     formField: []
-//   };
-//   const eventObj = {
-//     eventType: `${formStep} previous`,
-//     event: 'Hard Conversion Form'
-//   };
-//   trackAnalytics(formObj, 'form', 'formclick', undefined, false, eventObj);
-// };
+export const changeStepPrev = function (formName, formStep, formType) {
+  const formObj = {
+    formName: formName,
+    formStep: formStep,
+    formType: formType,
+    formField: []
+  };
+  const eventObj = {
+    eventType: `${formStep} previous`,
+    event: 'Soft Conversion Form'
+  };
+  trackAnalytics(formObj, 'form', 'formclick', undefined, false, eventObj);
+};
 
 
-// export const changeStepError = function (formName, formStep, formType, areaofInterest, dataObj, errorObj={}) {
-//   const formObj = {
-//     formName: formName,
-//     formStep: formStep,
-//     formType: formType,
-//     areaofInterest: areaofInterest,
-//     formField: [],
-//     formError:errorObj
-//   };
-//   const eventObj = {
-//     eventType: 'formerror',
-//     event: 'Hard Conversion Form'
-//   };
-//   Object.keys(dataObj).forEach(i => {
-//     if(dataObj[i]) {
-//       formObj.formField.push({
-//         formFieldName: i,
-//         formFieldValue: dataObj[i]
-//       });  
-//     }
-//   });
-//   trackAnalytics(formObj, 'form', 'formclick', undefined, false, eventObj);
-// };
-
-
-
-// export const loadThankYou = function (formName, areaofInterest, dataObj) {
-//   const formObj = {
-//     formName: formName,
-//     formStep: 'Thankyou',
-//     formType: 'Thank You',
-//     areaofInterest: areaofInterest,
-//     formField: []
-//   };
-//   const eventObj = {
-//     eventType: 'formcomplete',
-//     event: 'Hard Conversion Form'
-//   };
-//   Object.keys(dataObj).forEach(i => {
-//     if(dataObj[i]) {
-//       formObj.formField.push({
-//         formFieldName: i,
-//         formFieldValue: dataObj[i]
-//       });  
-//     }
-//   });
-//   trackAnalytics(formObj, 'form', 'formload', undefined, false, eventObj);
-// };
+export const changeStepError = function (formName, formStep, formType, dataObj, errorObj={}) {
+  const formObj = {
+    formName: formName,
+    formStep: formStep,
+    formType: formType,
+    formField: [],
+    formError:errorObj
+  };
+  const eventObj = {
+    eventType: 'formerror',
+    event: 'Soft Conversion Form'
+  };
+  Object.keys(dataObj).forEach(i => {
+    if(dataObj[i]) {
+      formObj.formField.push({
+        formFieldName: i,
+        formFieldValue: dataObj[i]
+      });  
+    }
+  });
+  trackAnalytics(formObj, 'form', 'formclick', undefined, false, eventObj);
+};
 
 
 
+export const loadDownloadReady = function (formName, dataObj) {
+  const formObj = {
+    formName: formName,
+    formStep: 'Step 4',
+    formType: 'Download Ready',
+    formField: []
+  };
+  const eventObj = {
+    eventType: 'formcomplete',
+    event: 'Soft Conversion Form'
+  };
+  Object.keys(dataObj).forEach(i => {
+    if(dataObj[i]) {
+      formObj.formField.push({
+        formFieldName: i,
+        formFieldValue: dataObj[i]
+      });  
+    }
+  });
+  trackAnalytics(formObj, 'form', 'formload', undefined, false, eventObj);
+};
 
-// export const newPage = function () {
+export const downloadLinkTrack = function (downObj, eventType) {
+  const eventObj = {
+    eventType: eventType,
+    event: 'Soft Conversion Form'
+  };
 
-//   const linkClickObj = {
-//     linkType: 'internal',
-//     linkSection: 'Hard Conversion Form_button click',
-//     linkParentTitle: '',
-//     linkName: 'New Request'
-//   };
-//   const eventObj = {
-//     eventType: 'linkClick',
-//     event: 'Hard Conversion Form'
-//   };
-//   trackAnalytics(linkClickObj, 'linkClick', 'linkClick', undefined, false, eventObj);
-// };
+  trackAnalytics(downObj, 'linkClick', eventType, undefined, false, eventObj);
+};
+
