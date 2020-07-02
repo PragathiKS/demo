a8c125a8ce6736914f489d59d4ea08b8d5ebb8b9
Abhishek Narula <abhishek.narula@publicissapient.com>
2020-07-02 17:02:09 +0530

    SMAR-14188 | Fixed asset update listener.

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/events/AssetUpdateListner.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/events/AssetUpdateListner.java
@@ -1,13 +1,16 @@
 package com.tetrapak.publicweb.core.events;
 
-import java.util.List;
-import java.util.Objects;
+import com.day.cq.commons.jcr.JcrConstants;
+import com.tetrapak.publicweb.core.constants.PWConstants;
+import com.tetrapak.publicweb.core.utils.GlobalUtil;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.ModifiableValueMap;
 import org.apache.sling.api.resource.PersistenceException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.api.resource.observation.ResourceChange;
 import org.apache.sling.api.resource.observation.ResourceChangeListener;
 import org.osgi.service.component.annotations.Component;
@@ -16,14 +19,14 @@ import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.tetrapak.publicweb.core.constants.PWConstants;
-import com.tetrapak.publicweb.core.utils.GlobalUtil;
+import java.util.List;
+import java.util.Objects;
 
 @Component(
         immediate = true,
         configurationPolicy = ConfigurationPolicy.REQUIRE,
         service = ResourceChangeListener.class,
-        property = { 
+        property = {
                 ResourceChangeListener.PATHS + "=" + "glob:/content/dam/tetrapak/publicweb/**/jcr:content",
                 ResourceChangeListener.PATHS + "=" + "glob:/content/dam/tetrapak/products/**/jcr:content",
                 ResourceChangeListener.PATHS + "=" + "glob:/content/dam/tetrapak/media-box/**/jcr:content",
@@ -41,42 +44,35 @@ public class AssetUpdateListner implements ResourceChangeListener {
     private ResourceResolverFactory resolverFactory;
 
     @Override
-    public void onChange(List<ResourceChange> changes) {
-        LOGGER.info("Coping jcr last modified to cq last modified for search");
-        ResourceResolver resolver = null;
-        try {
-            resolver = GlobalUtil.getResourceResolverFromSubService(resolverFactory);
+    public void onChange(final List<ResourceChange> changes) {
+        LOGGER.info("Copying jcr last modified to cq last modified for search");
+        try (final ResourceResolver resolver = GlobalUtil.getResourceResolverFromSubService(resolverFactory)) {
             if (Objects.nonNull(resolver)) {
-                for (ResourceChange change : changes) {
-                    if (change.getPath().endsWith("jcr:content")) {
-                        LOGGER.info("Asset Path :: {}", change.getPath());
-                        Resource assetJcrResource = resolver.getResource(change.getPath());
+                for (final ResourceChange change : changes) {
+                    if (change.getPath().contains("jcr:content")) {
+                        final String assetJcrContentPath = StringUtils.substringBefore(change.getPath(),
+                                JcrConstants.JCR_CONTENT) + JcrConstants.JCR_CONTENT;
+                        LOGGER.info("Asset Path :: {}", assetJcrContentPath);
+                        final Resource assetJcrResource = resolver.getResource(assetJcrContentPath);
+                        final ValueMap valueMap = assetJcrResource.getValueMap();
                         LOGGER.info("Asset Res :: {}",assetJcrResource);
-                        if (Objects.nonNull(assetJcrResource)
-                                && assetJcrResource.getValueMap().containsKey(PWConstants.JCR_LAST_MODIFIED)) {
-                            ModifiableValueMap map = assetJcrResource.adaptTo(ModifiableValueMap.class);
+                        if (Objects.nonNull(assetJcrResource) && valueMap.containsKey(PWConstants.JCR_LAST_MODIFIED)) {
+                            final ModifiableValueMap map = assetJcrResource.adaptTo(ModifiableValueMap.class);
                             LOGGER.info("Asset map :: {}", map);
-                            LOGGER.info("Asset value map :: {}", assetJcrResource.getValueMap());
-                            LOGGER.info("Asset value map prop :: {}",
-                                    assetJcrResource.getValueMap().get(PWConstants.JCR_LAST_MODIFIED));
+                            LOGGER.info("Asset value map :: {}", valueMap);
+                            LOGGER.info("Asset value map prop :: {}", valueMap.get(PWConstants.JCR_LAST_MODIFIED));
                             if(map.containsKey(PWConstants.CQ_LAST_MODIFIED)) {
                                 map.remove(PWConstants.CQ_LAST_MODIFIED);
                             }
-                            map.put(PWConstants.CQ_LAST_MODIFIED,
-                                    assetJcrResource.getValueMap().get(PWConstants.JCR_LAST_MODIFIED));
+                            map.put(PWConstants.CQ_LAST_MODIFIED, valueMap.get(PWConstants.JCR_LAST_MODIFIED));
                             resolver.commit();
+                            break;
                         }
                     }
                 }
             }
-        } catch (PersistenceException e) {
+        } catch (final PersistenceException e) {
             LOGGER.error("Exception in AssetUpdateListner ::", e.getMessage(), e);
-
-        } finally {
-            if (resolver != null && resolver.isLive()) {
-                resolver.close();
-            }
         }
     }
-
 }
