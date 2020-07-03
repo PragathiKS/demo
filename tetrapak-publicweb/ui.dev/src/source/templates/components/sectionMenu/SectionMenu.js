4365532ec4c340edbf169fb573ec1cc00406fd11
Ashwani Payal <ashwani.payal@publicissapient.com>
2020-07-03 18:39:29 +0530

    dev-build-bug-fix-sectionmenu

--- tetrapak-publicweb/ui.dev/src/source/templates/components/sectionMenu/SectionMenu.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/sectionMenu/SectionMenu.js
@@ -108,7 +108,7 @@ class SectionMenu {
     const $sectionMenuItemAnchor = $this.children('a');
     $this.find('.js-sub-menu-navigation-Link').addClass('show').attr('aria-hidden','false').attr('aria-expanded','true'); // value changed because of one extra div added
     $sectionMenuItemAnchor.children('.with-arrow').addClass('icon-up');
-    $overlay.removeClass('d-none');
+    $sectionMenuItemAnchor.children('.with-arrow').length && $overlay.removeClass('d-none');
     /* check modal view port position */
     const modalPoints =$this.children('.js-sub-menu-navigation-Link')[0] && $this.children('.js-sub-menu-navigation-Link')[0].getBoundingClientRect();
     if(modalPoints && modalPoints.left < 0){
