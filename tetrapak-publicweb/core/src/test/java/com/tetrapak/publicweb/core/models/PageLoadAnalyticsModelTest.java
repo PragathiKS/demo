2b4e7591dc17231d5089264366e5296fab398b3d
Pawan Kumar <pawan.kumar@tetrapak.com>
2020-09-03 00:05:23 +0530

    fixing code coverage

--- tetrapak-publicweb/core/src/test/java/com/tetrapak/publicweb/core/models/PageLoadAnalyticsModelTest.java
+++ tetrapak-publicweb/core/src/test/java/com/tetrapak/publicweb/core/models/PageLoadAnalyticsModelTest.java
@@ -65,7 +65,7 @@ public class PageLoadAnalyticsModelTest {
      */
     @Test
     public void simpleLoadAndGettersTest() throws Exception {
-        String[] methods = new String[]{"isProduction", "isStaging", "isDevelopment", "getDigitalData"};
+        String[] methods = new String[]{"isProduction", "isStaging", "isDevelopment", "getDigitalData" , "getHrefLangFlag" , "getHreflangValues", "getCanonicalURL"};
         Util.testLoadAndGetters(methods, model, resource);
     }
 }
