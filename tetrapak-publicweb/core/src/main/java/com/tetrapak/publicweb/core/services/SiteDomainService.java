59dbafdbe56dc6334e056a060b1cacabff83cd6c
Bimmi Soi <bimmi.soi@publicissapient.com>
2020-09-16 20:57:28 +0530

    changes

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/services/SiteDomainService.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/services/SiteDomainService.java
@@ -1,7 +1,15 @@
 package com.tetrapak.publicweb.core.services;
 
+/**
+ * The Interface SiteDomainService.
+ */
 public interface SiteDomainService {
-    
+
+    /**
+     * Gets the domain.
+     *
+     * @return the domain
+     */
     String getDomain();
 
 }
