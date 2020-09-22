f357b0652eddd8721da7f41e81073ca8e4d4eef6
Pawan Kumar <pawan.kumar@tetrapak.com>
2020-09-22 23:59:34 +0530

    change for sky-blue selection

--- tetrapak-publicweb/ui.apps/src/main/content/jcr_root/apps/publicweb/clientlibs/clientlib-authoring/dropdown-enable-disable.js
+++ tetrapak-publicweb/ui.apps/src/main/content/jcr_root/apps/publicweb/clientlibs/clientlib-authoring/dropdown-enable-disable.js
@@ -45,6 +45,7 @@
                 value = component.getValue();
             }
             if(value == "hero") {
+              $(target +' .coral3-Select-label').text("Sky Blue");
               $(target).first().attr("disabled","true");
             } else {
                $(target).first().removeAttr("disabled","true");
