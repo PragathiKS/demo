af07378d7cffb0d2e95b1d04273953a2b2799148
Ashwani Payal <ashwani.payal@tetrapak.com>
2020-10-15 08:03:56 +0000

    Merged in bugfix/mega-menu-responsive (pull request #923)
    mega menu fixes
    
    Approved-by: Anshika.Joon <anshika.joon@tetrapak.com>

--- tetrapak-publicweb/ui.dev/src/source/templates/components/megamenusolution/MegaMenuSolution.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/megamenusolution/MegaMenuSolution.js
@@ -32,10 +32,9 @@ class MegaMenuSolution {
 
   loadMegaMenuCol = () => {
     const { $megaMenuNavCol } = this.cache;
-    if($megaMenuNavCol.length <= 3 && (window.innerWidth >= 1200 && window.innerWidth <= 1439)) {
+    if(window.innerWidth >= 1200 && window.innerWidth <= 1439) {
       $megaMenuNavCol.each(function(){
         $(this).find('img').addClass('load-mega-menu-dynamic-media');
-        $(this).css({'width':249,'max-width':249});
       });
     }
   }
