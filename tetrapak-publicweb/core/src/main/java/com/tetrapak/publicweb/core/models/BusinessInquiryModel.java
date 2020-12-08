b8ffc2bbf8bd238918615703f4005939aec3f626
Nagavekar Bhakti <INNAGAVEKARB@tetrapak.com>
2020-12-08 13:16:08 +0530

    Code fixes for formatting and correcting dialog field

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/BusinessInquiryModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/BusinessInquiryModel.java
@@ -12,13 +12,16 @@ import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Set;
 import java.util.TreeMap;
+
 import javax.annotation.PostConstruct;
+
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.Self;
+
 import com.day.cq.tagging.Tag;
 import com.day.cq.tagging.TagManager;
 import com.tetrapak.publicweb.core.beans.DropdownOption;
@@ -34,70 +37,62 @@ import com.tetrapak.publicweb.core.utils.PageUtil;
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class BusinessInquiryModel extends FormModel {
 
-    /** The resource. */
-    @Self
-    private Resource resource;
-    
-    
+	/** The resource. */
+	@Self
+	private Resource resource;
 
-    /** The pardot service. */
-    @OSGiService
-    private PardotService pardotService;
+	/** The pardot service. */
+	@OSGiService
+	private PardotService pardotService;
 
-    private FormConfigModel formConfig;
+	private FormConfigModel formConfig;
 
-    private FormConsentConfigModel consentConfig;
-    
-    /** The country options. */
-    private List<DropdownOption> countryOptions;
-    
-    /** The country detail service. */
-    @OSGiService
-    private CountryDetailService countryDetailService;
-    
-    
-    
-    /**
-     * The init method.
-     */
-    @PostConstruct
-    protected void init() {
-        setCountryOptions();
-        setFormConfig();
-  }
+	private FormConsentConfigModel consentConfig;
 
-    /**
-     * Sets the form configs.
-     */
-    public void setFormConfig() {
+	/** The country options. */
+	private List<DropdownOption> countryOptions;
 
-        final Resource formConfigResource = GlobalUtil.fetchConfigResource(resource,
-                "/jcr:content/root/responsivegrid/businessinquiryformc");
-        if (Objects.nonNull(formConfigResource))
-            this.formConfig = formConfigResource.adaptTo(FormConfigModel.class);
+	/** The country detail service. */
+	@OSGiService
+	private CountryDetailService countryDetailService;
 
-        final Resource consentConfigResource = GlobalUtil.fetchConfigResource(resource,
-                "/jcr:content/root/responsivegrid/formconsenttextsconf");
-        if (Objects.nonNull(consentConfigResource))
-            this.consentConfig = consentConfigResource.adaptTo(FormConsentConfigModel.class);
-    }
-    
-    /**
-     * Gets the tags Value and sorting it with Alphabetical order.
-     *
-     */
-    
+	/**
+	 * The init method.
+	 */
+	@PostConstruct
+	protected void init() {
+		setCountryOptions();
+		setFormConfig();
+	}
+
+	/**
+	 * Sets the form configs.
+	 */
+	public void setFormConfig() {
+
+		final Resource formConfigResource = GlobalUtil.fetchConfigResource(resource,
+				"/jcr:content/root/responsivegrid/businessinquiryformc");
+		if (Objects.nonNull(formConfigResource))
+			this.formConfig = formConfigResource.adaptTo(FormConfigModel.class);
+
+		final Resource consentConfigResource = GlobalUtil.fetchConfigResource(resource,
+				"/jcr:content/root/responsivegrid/formconsenttextsconf");
+		if (Objects.nonNull(consentConfigResource))
+			this.consentConfig = consentConfigResource.adaptTo(FormConsentConfigModel.class);
+	}
+
+	/**
+	 * Gets the tags Value and sorting it with Alphabetical order.
+	 *
+	 */
 	public Map<String, String> getTagTitles() {
-		
-
-		String rootTag = formConfig.getTags(); 
+		String rootTag = formConfig.getProfileTags();
 		ResourceResolver resolver = resource.getResourceResolver();
 		TagManager tagManager = resolver.adaptTo(TagManager.class);
 		Tag tag = tagManager.resolve(rootTag);
 		Iterator<Tag> tagIterator = tag.listChildren();
 		Map<String, String> tagsVal = new HashMap<>();
 		while (tagIterator.hasNext()) {
-
 			Tag childtag = tagIterator.next();
 			String tagName = childtag.getName();
 			String defaulTitle = childtag.getTitle();
@@ -108,20 +103,18 @@ public class BusinessInquiryModel extends FormModel {
 			} else {
 				tagsVal.put(tagName, localizedTitle);
 			}
-
 		}
-
 		// sorting of map alphabetically with values:
 		TreeMap<String, String> sorted = new TreeMap<>(tagsVal);
 		Set<Entry<String, String>> mappings = sorted.entrySet();
-		
+
 		Map<String, String> tagsValues = new LinkedHashMap<>();
-		
+
 		Comparator<Entry<String, String>> valueComparator = (e1, e2) -> e1.getValue().compareTo(e2.getValue());
-		
+
 		List<Entry<String, String>> listOfEntries = new ArrayList<>(mappings);
 		Collections.sort(listOfEntries, valueComparator);
-		
+
 		LinkedHashMap<String, String> sortedByValue = new LinkedHashMap<>(listOfEntries.size());
 		for (Entry<String, String> entry : listOfEntries) {
 			sortedByValue.put(entry.getKey(), entry.getValue());
@@ -135,65 +128,65 @@ public class BusinessInquiryModel extends FormModel {
 		}
 		return tagsValues;
 
-	}  
+	}
 
-    public String getApiUrl() {
-        return resource.getPath() + ".pardotbusinessenquiry.json";
-    }  
+	public String getApiUrl() {
+		return resource.getPath() + ".pardotbusinessenquiry.json";
+	}
 
-    /**
-     * Gets the site language.
-     *
-     * @return the site language
-     */
-    public String getSiteLanguage() {
-        return PageUtil.getLanguageCodeFromResource(resource);
-    }
+	/**
+	 * Gets the site language.
+	 *
+	 * @return the site language
+	 */
+	public String getSiteLanguage() {
+		return PageUtil.getLanguageCodeFromResource(resource);
+	}
 
-    /**
-     * Gets the site country.
-     *
-     * @return the site country
-     */
-    public String getSiteCountry() {
-        return PageUtil.getMarketCode(resource);
-    }
+	/**
+	 * Gets the site country.
+	 *
+	 * @return the site country
+	 */
+	public String getSiteCountry() {
+		return PageUtil.getMarketCode(resource);
+	}
 
-    /**
-     * Gets the form config.
-     *
-     * @return the form config
-     */
-    public FormConfigModel getFormConfig() {
-        return formConfig;
-    }
+	/**
+	 * Gets the form config.
+	 *
+	 * @return the form config
+	 */
+	public FormConfigModel getFormConfig() {
+		return formConfig;
+	}
 
-    /**
-     * Gets the consent config.
-     *
-     * @return the consent config
-     */
-    public FormConsentConfigModel getConsentConfig() {
-        return consentConfig;
-    }
-    
-    /**
-     * Gets the country options.
-     *
-     * @return the country options
-     */
-    public List<DropdownOption> getCountryOptions() {
-        return countryOptions;
-    }
+	/**
+	 * Gets the consent config.
+	 *
+	 * @return the consent config
+	 */
+	public FormConsentConfigModel getConsentConfig() {
+		return consentConfig;
+	}
 
+	/**
+	 * Gets the country options.
+	 *
+	 * @return the country options
+	 */
+	public List<DropdownOption> getCountryOptions() {
 
-    /**
-     * Fetches country list from content fragments.
-     *
-     */
-    private void setCountryOptions() {
-        this.countryOptions = countryDetailService.fetchCountryList(resource.getResourceResolver());
-       
-    }
+		return countryOptions;
+	}
+
+	/**
+	 * Fetches country list from content fragments.
+	 *
+	 */
+	private void setCountryOptions() {
+		this.countryOptions = countryDetailService.fetchCountryList(resource.getResourceResolver());
+
+	}
 
 }
--- tetrapak-publicweb/core/src/test/java/com/tetrapak/publicweb/core/models/BusinessInquiryModelTest.java
+++ tetrapak-publicweb/core/src/test/java/com/tetrapak/publicweb/core/models/BusinessInquiryModelTest.java
@@ -24,143 +24,138 @@ import io.wcm.testing.mock.aem.junit.AemContext;
  */
 public class BusinessInquiryModelTest {
 
-    /** The context. */
-    @Rule
-    public AemContext context = new AemContext();
-    
-    /** The context 2. */
-    @Rule
-    public AemContext context2 = new AemContext();
+	/** The context. */
+	@Rule
+	public AemContext context = new AemContext();
 
-    /** The Constant TEST_RESOURCE_CONTENT. */
-    private static final String TEST_RESOURCE_CONTENT = "/businessinquiryform/test-content.json";
+	/** The context 2. */
+	@Rule
+	public AemContext context2 = new AemContext();
 
-    /** The Constant TEST_CONTENT_ROOT. */
-    private static final String TEST_CONTENT_ROOT = "/content/tetrapak/publicweb/gb";
+	/** The Constant TEST_RESOURCE_CONTENT. */
+	private static final String TEST_RESOURCE_CONTENT = "/businessinquiryform/test-content.json";
 
-    /** The Constant CONTACT_US_CONTENT_ROOT. */
-    private static final String CONTACT_US_CONTENT_ROOT = "/content/tetrapak/publicweb/gb/en/contact-us"; 
-    
-    /** The Constant TEST_RESOURCE_CFM. */
-    private static final String TEST_RESOURCE_CFM = "/contactus/test-countries-content.json";
-    
-    /** The Constant COUNTRIES_ROOT. */
-    private static final String COUNTRIES_ROOT = "/content/dam/tetrapak/publicweb/cfm/countries";
-    
-    
-    /** The model class. */
-    Class<BusinessInquiryModel> modelClass = BusinessInquiryModel.class;
+	/** The Constant TEST_CONTENT_ROOT. */
+	private static final String TEST_CONTENT_ROOT = "/content/tetrapak/publicweb/gb";
 
-    /** The model. */
-    private BusinessInquiryModel model;
+	/** The Constant CONTACT_US_CONTENT_ROOT. */
+	private static final String CONTACT_US_CONTENT_ROOT = "/content/tetrapak/publicweb/gb/en/contact-us";
 
-    /** The pardot service. */
-    private PardotService pardotService;
-    
-    
-    /** The country detail service. */
-    private CountryDetailService countryDetailService;
+	/** The Constant TEST_RESOURCE_CFM. */
+	private static final String TEST_RESOURCE_CFM = "/contactus/test-countries-content.json";
 
-    /**
-     * The resource.
-     */
-    private Resource resource;
+	/** The Constant COUNTRIES_ROOT. */
+	private static final String COUNTRIES_ROOT = "/content/dam/tetrapak/publicweb/cfm/countries";
 
-    /**
-     * The Constant PXP_FEATURES.
-     */
-    private static final String RESOURCE = CONTACT_US_CONTENT_ROOT + "/jcr:content/businessinquiryform";
+	/** The model class. */
+	Class<BusinessInquiryModel> modelClass = BusinessInquiryModel.class;
 
-    /**
-     * Sets the up.
-     *
-     * @throws Exception
-     *             the exception
-     */
-    @Before
-    public void setUp() throws Exception {
-        pardotService = new PardotServiceImpl();
-        context.load().json(TEST_RESOURCE_CONTENT, TEST_CONTENT_ROOT);
-        context.load().json(TEST_RESOURCE_CFM, COUNTRIES_ROOT);
-        
-        context.addModelsForClasses(modelClass);
-        context.registerService(PardotService.class, pardotService);
-        // context.registerInjectActivateService(countryDetailService);
-        final Map<String, Object> pardotConfig = new HashMap<>();
-        pardotConfig.put("pardotBusinessInquiryServiceUrl", "http://pardotURL");
-        MockOsgi.activate(context.getService(PardotService.class), context.bundleContext(), pardotConfig);
-        
-        countryDetailService = new CountryDetailServiceImpl();
-        context.registerService(CountryDetailService.class, countryDetailService);
-        // context.registerInjectActivateService(countryDetailService);
-        final Map<String, Object> countryConfig = new HashMap<>();
-        countryConfig.put("getCountriesContentFragmentRootPath",
-                "/content/dam/tetrapak/publicweb/cfm/countries");
-        MockOsgi.activate(context.getService(CountryDetailService.class), context.bundleContext(), countryConfig);
+	/** The model. */
+	private BusinessInquiryModel model;
 
-        resource = context.currentResource(RESOURCE);
-        model = resource.adaptTo(modelClass);
+	/** The pardot service. */
+	private PardotService pardotService;
 
-    }
+	/** The country detail service. */
+	private CountryDetailService countryDetailService;
 
-    /**
-     * Test dailog values.
-     *
-     * @throws Exception
-     *             the exception
-     */
-    @Test
-    public void testDailogValues() throws Exception {
-        assertEquals("Form", "Business Enquiry", model.getFormConfig().getHeading());
-        assertEquals("Form", "title", model.getAnchorTitle());
-        assertEquals("Form", "test01", model.getAnchorId());
-        assertEquals("Form", "Thank you for your request", model.getFormConfig().getThankyouHeading());
-        assertEquals("Form", "/content/dam/tetrapak/publicweb/contactus.PNG", model.getFormConfig().getThankyouImage());
-        assertEquals("Form", "Thank you", model.getFormConfig().getThankyouImageAltText());
-        assertEquals("Form", "We will get back to you as soon as possible",
-                model.getFormConfig().getThankyouDescriptionText());
-        assertEquals("Form",
-                "I agree that the information I have provided will only be used in accordance with Tetra Pak privacy policy.",
-                model.getConsentConfig().getPrivacyPolicy());
-        assertEquals("Form", "/content/dam/tetrapak/publicweb/contactus.PNG", model.getFormConfig().getImage());
-        assertEquals("Form", "Contact us", model.getFormConfig().getAlt());
-        assertEquals("Form", "Description", model.getFormConfig().getDescriptionText());
-        assertEquals("Form", "grayscale-white", model.getPwTheme());
-        assertEquals("Form",
-                "/content/tetrapak/publicweb/gb/en/contact-us/jcr:content/businessinquiryform.pardotbusinessenquiry.json",
-                model.getApiUrl());
-        assertEquals("Form", "Marketing Consent", model.getConsentConfig().getMarketingConsent());
-		assertEquals("Form","tetrapak:job-title", model.getFormConfig().getTags());
-    }
+	/**
+	 * The resource.
+	 */
+	private Resource resource;
 
-    /**
-     * Test fetch language.
-     */
-    @Test
-    public void testFetchLanguage() {
-        assertEquals("en", model.getSiteLanguage());
-    }
+	/**
+	 * The Constant PXP_FEATURES.
+	 */
+	private static final String RESOURCE = CONTACT_US_CONTENT_ROOT + "/jcr:content/businessinquiryform";
 
-    /**
-     * Test fetch country.
-     */
-    @Test
-    public void testFetchCountry() {
-        assertEquals("gb", model.getSiteCountry());
+	/**
+	 * Sets the up.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		pardotService = new PardotServiceImpl();
+		context.load().json(TEST_RESOURCE_CONTENT, TEST_CONTENT_ROOT);
+		context.load().json(TEST_RESOURCE_CFM, COUNTRIES_ROOT);
 
-    }
-       
-    /**
-     * Test countries.
-     *
-     * @throws Exception the exception
-     */
-    @Test
-    public void testCountries() throws Exception {      
+		context.addModelsForClasses(modelClass);
+		context.registerService(PardotService.class, pardotService);
+		// context.registerInjectActivateService(countryDetailService);
+		final Map<String, Object> pardotConfig = new HashMap<>();
+		pardotConfig.put("pardotBusinessInquiryServiceUrl", "http://pardotURL");
+		MockOsgi.activate(context.getService(PardotService.class), context.bundleContext(), pardotConfig);
 
-        assertEquals("ContactUs", 2, model.getCountryOptions().size());
-        assertEquals("ContactUs", "albania", model.getCountryOptions().get(0).getKey());
+		countryDetailService = new CountryDetailServiceImpl();
+		context.registerService(CountryDetailService.class, countryDetailService);
+		// context.registerInjectActivateService(countryDetailService);
+		final Map<String, Object> countryConfig = new HashMap<>();
+		countryConfig.put("getCountriesContentFragmentRootPath", "/content/dam/tetrapak/publicweb/cfm/countries");
+		MockOsgi.activate(context.getService(CountryDetailService.class), context.bundleContext(), countryConfig);
 
-    }
+		resource = context.currentResource(RESOURCE);
+		model = resource.adaptTo(modelClass);
+
+	}
+
+	/**
+	 * Test dailog values.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	public void testDailogValues() throws Exception {
+		assertEquals("Form", "Business Enquiry", model.getFormConfig().getHeading());
+		assertEquals("Form", "title", model.getAnchorTitle());
+		assertEquals("Form", "test01", model.getAnchorId());
+		assertEquals("Form", "Thank you for your requests", model.getFormConfig().getThankyouHeading());
+		assertEquals("Form", "/content/dam/tetrapak/publicweb/contactus.PNG", model.getFormConfig().getThankyouImage());
+		assertEquals("Form", "Thank you", model.getFormConfig().getThankyouImageAltText());
+		assertEquals("Form", "We will get back to you as soon as possible",
+				model.getFormConfig().getThankyouDescriptionText());
+		assertEquals("Form",
+				"I agree that the information I have provided will only be used in accordance with Tetra Pak privacy policy.",
+				model.getConsentConfig().getPrivacyPolicy());
+		assertEquals("Form", "/content/dam/tetrapak/publicweb/contactus.PNG", model.getFormConfig().getImage());
+		assertEquals("Form", "Contact us", model.getFormConfig().getAlt());
+		assertEquals("Form", "Description", model.getFormConfig().getDescriptionText());
+		assertEquals("Form", "grayscale-white", model.getPwTheme());
+		assertEquals("Form",
+				"/content/tetrapak/publicweb/gb/en/contact-us/jcr:content/businessinquiryform.pardotbusinessenquiry.json",
+				model.getApiUrl());
+		assertEquals("Form", "Marketing Consent", model.getConsentConfig().getMarketingConsent());
+		assertEquals("Form", "tetrapak:job-title", model.getFormConfig().getProfileTags());
+	}
+
+	/**
+	 * Test fetch language.
+	 */
+	@Test
+	public void testFetchLanguage() {
+		assertEquals("en", model.getSiteLanguage());
+	}
+
+	/**
+	 * Test fetch country.
+	 */
+	@Test
+	public void testFetchCountry() {
+		assertEquals("gb", model.getSiteCountry());
+
+	}
+
+	/**
+	 * Test countries.
+	 *
+	 * @throws Exception the exception
+	 */
+	@Test
+	public void testCountries() throws Exception {
+
+		assertEquals("ContactUs", 2, model.getCountryOptions().size());
+		assertEquals("ContactUs", "albania", model.getCountryOptions().get(0).getKey());
+
+	}
 
 }
