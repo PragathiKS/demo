1117343c97a1fd97a0917d5400a27f1d957c32ef
sandip kumar <sandip.kumar@tetrapak.com>
2021-01-12 20:12:32 +0530

    Fix for SMAR-18054

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/SectionMenuModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/SectionMenuModel.java
@@ -135,7 +135,7 @@ public class SectionMenuModel {
             if (!nextPage.isHideInNav()) {
                 final SectionMenuBean sectionMenuBean = new SectionMenuBean();
                 sectionMenuBean.setLinkText(NavigationUtil.getNavigationTitle(nextPage));
-                if(request.getPathInfo().contains(nextPage.getPath())) {
+                if(request.getPathInfo().equalsIgnoreCase(nextPage.getPath())) {
                     sectionMenuBean.setHighlighted(true);
                 }
                 // Set external page url
