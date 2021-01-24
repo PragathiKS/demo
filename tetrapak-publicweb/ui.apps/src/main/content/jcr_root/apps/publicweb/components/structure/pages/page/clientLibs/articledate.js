e499fb6274c3cbf64535a8de74345b89bd663519
sandip kumar <sandip.kumar@tetrapak.com>
2021-01-25 00:17:54 +0530

    fix for article date issue

--- tetrapak-publicweb/ui.apps/src/main/content/jcr_root/apps/publicweb/components/structure/pages/page/clientLibs/articledate.js
+++ tetrapak-publicweb/ui.apps/src/main/content/jcr_root/apps/publicweb/components/structure/pages/page/clientLibs/articledate.js
@@ -1,12 +1,12 @@
 (function($, ns, channel, window, undefined) {
-
+	
     $(document).on('foundation-contentloaded', function (e) {
-        if ($('.pw-datepicker').length > 0) {
+        if ($('.pw-datepicker').length > 0 && $('.pw-datepicker').hasClass("cq-msm-lockable-field")) {
 			var articleDate = $('.pw-datepicker').attr("value");
             if(articleDate != undefined && articleDate != ""){
              var articleDateWithOutTime = articleDate.split('T')[0];
              $('.pw-datepicker').find("input[name='./articleDate']").attr("value",articleDate);
-             $('.pw-datepicker').find("input[is='coral-textfield']").attr("placeholder",articleDateWithOutTime)
+             $('.pw-datepicker').find("input[is='coral-textfield']").attr("placeholder",articleDateWithOutTime);
             }
         }
     });
