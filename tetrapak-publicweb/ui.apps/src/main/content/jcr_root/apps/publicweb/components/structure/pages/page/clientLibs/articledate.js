442bb45ce9e8d6771ca4151600f9bc77315d1abe
sandip kumar <sandip.kumar@tetrapak.com>
2021-01-24 23:54:18 +0530

    Fix for article date issue

---
+++ tetrapak-publicweb/ui.apps/src/main/content/jcr_root/apps/publicweb/components/structure/pages/page/clientLibs/articledate.js
@@ -0,0 +1,13 @@
+(function($, ns, channel, window, undefined) {
+
+    $(document).on('foundation-contentloaded', function (e) {
+        if ($('.pw-datepicker').length > 0) {
+			var articleDate = $('.pw-datepicker').attr("value");
+            if(articleDate != undefined && articleDate != ""){
+             var articleDateWithOutTime = articleDate.split('T')[0];
+             $('.pw-datepicker').find("input[name='./articleDate']").attr("value",articleDate);
+             $('.pw-datepicker').find("input[is='coral-textfield']").attr("placeholder",articleDateWithOutTime)
+            }
+        }
+    });
+}(jQuery, Granite.author, jQuery(document), this));
