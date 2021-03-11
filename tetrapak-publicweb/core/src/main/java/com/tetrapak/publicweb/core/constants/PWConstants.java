1d641c7dcd792961f52062febc9e2d9c0962972f
ojaswa-tp <ojaswa.swarnkar@tetrapak.com>
2021-03-11 12:55:44 +0530

    SMAR-16908 backend changes

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/constants/PWConstants.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/constants/PWConstants.java
@@ -262,6 +262,9 @@ public final class PWConstants {
 
     /** The Constant RU_COUNTRY_CODE for Russia, Kazakhstan, Ukraine. */
     public static final String RU_COUNTRY_CODE = "ru";
+    
+    /** The Constant AU_COUNTRY_CODE for Australia, New Zealand */
+    public static final String AU_COUNTRY_CODE = "au";
 
     /** The Constant SE_COUNTRY_CODE for Sweden. */
     public static final String SE_COUNTRY_CODE = "se";
@@ -280,7 +283,7 @@ public final class PWConstants {
     
     /** list exceptionCountriesList contains all countries which are exceptions for country code . **/
     public static final List<String> exceptionCountriesList = Collections.unmodifiableList(
-            Arrays.asList(MAGHREB_COUNTRY_CODE, CAC_COUNTRY_CODE, DE_COUNTRY_CODE, RU_COUNTRY_CODE, SE_COUNTRY_CODE));
+            Arrays.asList(MAGHREB_COUNTRY_CODE, CAC_COUNTRY_CODE, DE_COUNTRY_CODE, RU_COUNTRY_CODE, SE_COUNTRY_CODE, AU_COUNTRY_CODE));
 
     /** list maghrebLocaleValues contains all locales for maghreb. **/
     public static final List<String> maghrebLocaleValues = Collections
@@ -292,6 +295,10 @@ public final class PWConstants {
     /** list deLocaleValues contains all locales for Germany, Austria, Switzerland . **/
     public static final List<String> deLocaleValues = Collections
             .unmodifiableList(Arrays.asList("de-de", "de-at", "de-ch"));
+    
+    /** list auLocaleValues contains locales for Australia and New Zealand. **/
+    public static final List<String> auLocaleValues = Collections
+            .unmodifiableList(Arrays.asList("en-au", "en-nz"));
 
     /** list ruLocaleValues contains all locales for Russia, Kazakhstan, Ukraine. **/
     public static final List<String> ruLocaleValues = Collections
