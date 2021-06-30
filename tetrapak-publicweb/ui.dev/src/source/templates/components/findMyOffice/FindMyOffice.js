de410f552908b44001132301be359ca0107067cd
hirparma1 <hiren.parmar@digitas.com>
2021-06-30 11:47:37 +0530

    Baidu Map - XT Integration on contact us page - WIP

--- tetrapak-publicweb/ui.dev/src/source/templates/components/findMyOffice/FindMyOffice.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/findMyOffice/FindMyOffice.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import $ from 'jquery';
 import 'bootstrap';
 import { trackAnalytics } from '../../../scripts/utils/analytics';
@@ -7,7 +8,6 @@ import { ajaxMethods } from '../../../scripts/utils/constants';
 import { ajaxWrapper } from '../../../scripts/utils/ajax';
 import loadGoogleMapsApi from 'load-google-maps-api';
 import { isMobile } from '../../../scripts/common/common';
-
 class FindMyOffice {
   constructor({ el }) {
     this.root = $(el);
@@ -16,6 +16,8 @@ class FindMyOffice {
 
   initCache() {
     this.cache.map = {};
+    this.cache.baiduMap = {};
+    this.cache.isChinaLocale = $('html').attr('lang') === 'zh' ? true : false;
     this.cache.googleMaps = '';
     this.cache.officesList = [];
     this.cache.officesNameList = [];
@@ -50,16 +52,48 @@ class FindMyOffice {
 
   bindEvents() {
     const googleApiKey = this.cache.googleApi.data('google-api-key');
-    const isChina = window.location.href.indexOf('zh-cn');
-    if(isChina === -1){
+    // const isChina = window.location.href.indexOf('zh-cn');
+    if (!this.cache.isChinaLocale) {
       loadGoogleMapsApi({
         key: googleApiKey,
         libraries: ['places', 'geometry']
       }).then(this.handleGoogleMapApi);
     } else {
       this.getOfficesList();
+      this.initBaiduMap();
     }
     this.root.on('click', '.js-localSiteUrl', this.goToLocalSite);
+  }
+
+  initBaiduMap = () => {
+    this.cache.baiduMap = new window.BMap.Map('pw-find-my-office__map');
+    const point = new window.BMap.Point(this.cache.defaultLongitude, this.cache.defaultLatitude);
+    this.cache.baiduMap.centerAndZoom(point, 1);
+    this.cache.baiduMap.enableDragging();
+    this.cache.baiduMap.enableDoubleClickZoom();
+    this.cache.baiduMap.enableKeyboard();
+    this.baiduMapMarker(this.cache.baiduMap);
+  }
+
+  baiduMapMarker = (map, lat, lng) => {
+    const markerArr = [{
+      icon: { w: 40, h: 55, l: 0, t: 0, x: 6, lb: 5 }
+    }];
+
+    for (let i = 0; i < markerArr.length; i++) {
+      const json = markerArr[i];
+      const p0 = lng;
+      const p1 = lat;
+      const point = new window.BMap.Point(p0, p1);
+      const iconImg = this.createBaiduMapMarkerIcon(json.icon);
+      const marker = new window.BMap.Marker(point, { icon: iconImg });
+      map.addOverlay(marker);
+    }
+  }
+
+  createBaiduMapMarkerIcon = (json) => {
+    const icon = new window.BMap.Icon('/content/dam/tetrapak/publicweb/Pin.png', new window.BMap.Size(json.w, json.h), { imageOffset: new window.BMap.Size(-json.l, -json.t), infoWindowOffset: new window.BMap.Size(json.lb + 5, 1), offset: new window.BMap.Size(json.x, json.h) });
+    return icon;
   }
 
   goToLocalSite = e => {
@@ -144,23 +178,36 @@ class FindMyOffice {
       $('.js-pw-find-my-office__map').height(formHeight);
     }
   };
-
+  
   renderMarkerPosition = (office, options) => {
-    const latLng = new this.cache.googleMaps.LatLng(
-      office.latitude,
-      office.longitude
-    );
-    this.cache.marker = new this.cache.googleMaps.Marker({
-      position: latLng,
-      title: office.name,
-      icon: '/content/dam/tetrapak/publicweb/Pin.png'
-    });
+    if (this.cache.isChinaLocale) {
+      if (office.offices) {
+        console.log('Office Lat Lng >>>', office);
+        const point = new window.BMap.Point(office.longitude, office.latitude);
+        this.cache.baiduMap.centerAndZoom(point, 6);
 
-    // To add the marker to the map, call setMap();
-    this.cache.marker.setMap(this.cache.map);
-    this.cache.marker.setVisible(options.markerVisibility);
-    this.cache.map.setZoom(options.mapZoomLevel || 5);
-    this.cache.map.panTo(this.cache.marker.position);
+      } else {
+        const point = new window.BMap.Point(office.longitude, office.latitude);
+        this.cache.baiduMap.centerAndZoom(point, 11);
+      }
+    } else {
+      const latLng = new this.cache.googleMaps.LatLng(
+        office.latitude,
+        office.longitude
+      );
+
+      this.cache.marker = new this.cache.googleMaps.Marker({
+        position: latLng,
+        title: office.name,
+        icon: '/content/dam/tetrapak/publicweb/Pin.png'
+      });
+
+      // To add the marker to the map, call setMap();
+      this.cache.marker.setMap(this.cache.map);
+      this.cache.marker.setVisible(options.markerVisibility);
+      this.cache.map.setZoom(options.mapZoomLevel || 5);
+      this.cache.map.panTo(this.cache.marker.position);
+    }
   };
 
   resetOfficeDetails = () => {
