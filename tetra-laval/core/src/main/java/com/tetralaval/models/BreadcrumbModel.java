8efa98d3e48f7e5281affaa64a6807ee35e2e4b0
rnecner <rafal.necner@tetrpak.com>
2021-07-12 13:06:59 +0200

    Breadcrumbs component migration - AEM

---
+++ tetra-laval/core/src/main/java/com/tetralaval/models/BreadcrumbModel.java
@@ -0,0 +1,102 @@
+package com.tetralaval.models;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.day.cq.wcm.api.Page;
+import com.tetralaval.constants.PWConstants;
+import com.tetralaval.utils.LinkUtils;
+import com.tetralaval.utils.NavigationUtil;
+
+/**
+ * The Class BreadcrumbModel.
+ */
+@Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class BreadcrumbModel {
+
+    /** The request. */
+    @SlingObject
+    private SlingHttpServletRequest request;
+
+    /** The home page path. */
+    private String homePagePath;
+    /** The current page. */
+    @ScriptVariable
+    private Page currentPage;
+
+    /** The Constant LOGGER. */
+    private static final Logger LOGGER = LoggerFactory.getLogger(BreadcrumbModel.class);
+
+    /** The breadcrumb subpages. */
+    private final Map<String, String> breadcrumbSubpages = new LinkedHashMap<>();
+
+    /**
+     * Inits the.
+     */
+    @PostConstruct
+    protected void init() {
+        LOGGER.debug("Inside init method");
+        final Map<String, String> breadcrumbPages = new LinkedHashMap<>();
+        final String rootPath = LinkUtils.getRootPath(request.getPathInfo());
+        homePagePath = LinkUtils.sanitizeLink(rootPath + PWConstants.SLASH + PWConstants.HOME_PAGE_REL_PATH, request);
+        final String path = currentPage.getPath().replace(rootPath + "/", StringUtils.EMPTY);
+        final String[] pages = path.split("/");
+        final int length = pages.length - 1;
+        Page parent = currentPage.getParent();
+        final String title = NavigationUtil.getNavigationTitle(currentPage);
+
+        breadcrumbPages.put(title, currentPage.getPath());
+        for (int i = 0; i <= length; i++) {
+            if (Objects.nonNull(parent) && !parent.getPath().equalsIgnoreCase(rootPath) && !parent.isHideInNav() && parent.getContentResource()!=null) {
+
+                if (parent.getContentResource().getValueMap().containsKey("disableClickInNavigation")) {
+                    breadcrumbPages.put(NavigationUtil.getNavigationTitle(parent), null);
+                } else {
+                    breadcrumbPages.put(NavigationUtil.getNavigationTitle(parent),
+                            LinkUtils.sanitizeLink(parent.getPath(), request));
+                }
+
+                parent = parent.getParent();
+            }
+        }
+        final List<String> alKeys = new ArrayList<String>(breadcrumbPages.keySet());
+        Collections.reverse(alKeys);
+        for (final String key : alKeys) {
+            breadcrumbSubpages.put(key, breadcrumbPages.get(key));
+        }
+    }
+
+    /**
+     * Gets the breadcrumb subpages.
+     *
+     * @return the breadcrumb subpages
+     */
+    public Map<String, String> getBreadcrumbSubpages() {
+        return breadcrumbSubpages;
+    }
+
+    /**
+     * Gets the home page path.
+     *
+     * @return the home page path
+     */
+    public String getHomePagePath() {
+        return LinkUtils.sanitizeLink(homePagePath, request);
+    }
+
+}
---
+++ tetra-laval/core/src/main/java/com/tetralaval/models/MegaMenuConfigurationModel.java
@@ -0,0 +1,122 @@
+package com.tetralaval.models;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.BooleanUtils;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import com.tetralaval.models.multifield.PseudoCategoryModel;
+
+/**
+ * The Class MegaMenuSolutionModel.
+ */
+@Model(
+        adaptables = { Resource.class },
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class MegaMenuConfigurationModel {
+
+    /** The request. */
+    @SlingObject
+    private SlingHttpServletRequest request;
+
+    /** The top section subtitle. */
+    @ValueMapValue
+    private String topSectionSubtitle;
+
+    /** The bottom section subtitle. */
+    @ValueMapValue
+    private String bottomSectionSubtitle;
+
+    /** The hide food categories. */
+    @ValueMapValue
+    private String hideFoodCategories;
+
+    /** The end to end solution section. */
+    @Inject
+    private List<MegaMenuSolutionModel> endToEndSolutionSection;
+
+    /** The link section. */
+    @Inject
+    private List<LinkModel> linkSection;
+
+    /** The food category section. */
+    @Inject
+    private List<MegaMenuSolutionModel> foodCategorySection;
+
+    /** The pseudo category bean. */
+    @Inject
+    private List<PseudoCategoryModel> pseudoCategoryBean;
+
+    /**
+     * Gets the top section subtitle.
+     *
+     * @return the top section subtitle
+     */
+    public String getTopSectionSubtitle() {
+        return topSectionSubtitle;
+    }
+
+    /**
+     * Gets the bottom section subtitle.
+     *
+     * @return the bottom section subtitle
+     */
+    public String getBottomSectionSubtitle() {
+        return bottomSectionSubtitle;
+    }
+
+    /**
+     * Gets the hide food categories.
+     *
+     * @return the hide food categories
+     */
+    public Boolean getHideFoodCategories() {
+        return BooleanUtils.toBoolean(hideFoodCategories);
+    }
+
+    /**
+     * Gets the end to end solution section.
+     *
+     * @return the end to end solution section
+     */
+    public List<MegaMenuSolutionModel> getEndToEndSolutionSection() {
+        return endToEndSolutionSection;
+    }
+
+    /**
+     * Gets the link section.
+     *
+     * @return the link section
+     */
+    public List<LinkModel> getLinkSection() {
+        return linkSection;
+    }
+
+    /**
+     * Gets the food category section.
+     *
+     * @return the food category section
+     */
+    public List<MegaMenuSolutionModel> getFoodCategorySection() {
+        return foodCategorySection;
+    }
+
+    /**
+     * Gets the pseudo category list.
+     *
+     * @return the pseudo category list
+     */
+    public List<PseudoCategoryModel> getPseudoCategoryList() {
+        final List<PseudoCategoryModel> pseudoCategoryList = new ArrayList<>();
+        if (CollectionUtils.isNotEmpty(pseudoCategoryBean)) {
+            pseudoCategoryList.addAll(pseudoCategoryBean);
+        }
+        return pseudoCategoryList;
+    }
+}
