dde3b3a383173cb96f9cdb998cd844d5e53046e8
rnecner <rnecner@sii.pl>
2021-08-11 13:39:49 +0200

    AEM cookie policy

---
+++ tetra-laval/ui.frontend/src/main/webpack/components/cookiePolicy/CookiePolicy.js
@@ -0,0 +1,26 @@
+import $ from 'jquery';
+
+class CookiePolicy {
+    constructor({ el }) {
+        this.root = $(el);
+    }
+
+    init() {
+        this.$confirmationButton = this.root.find('.confirmation-button');
+        if ( document.cookie.indexOf('cookiePolicy') > -1) {
+            this.removeCookieBar();
+        }
+        this.$confirmationButton.on('click', this.removeCookieBar);
+        this.$confirmationButton.on('click', this.createCookie);
+    }
+
+    removeCookieBar = e => {
+        $('.pc-dark-filter').addClass('hide');
+        $('.otFloatingRoundedCorner').addClass('hide');
+    }
+
+    createCookie  = e => {
+        document.cookie = 'cookiePolicy';
+    };
+}
+export default CookiePolicy;
