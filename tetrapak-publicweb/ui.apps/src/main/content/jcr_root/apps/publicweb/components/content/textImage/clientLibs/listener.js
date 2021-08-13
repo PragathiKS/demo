c9f095251bc011b94ab6e88786d3298340d563a7
Mathur <aalekh.mathur@capgemini.com>
2021-08-13 11:26:21 +0200

    SMAR-22649-method-not-allowed-error-on-page-rollout-develop-stream reverted

--- tetrapak-publicweb/ui.apps/src/main/content/jcr_root/apps/publicweb/components/content/textImage/clientLibs/listener.js
+++ tetrapak-publicweb/ui.apps/src/main/content/jcr_root/apps/publicweb/components/content/textImage/clientLibs/listener.js
@@ -34,10 +34,7 @@
         patterns = {
              pardotUrlRegex: /^(https:\/\/)?go+([\-\.]{1}tetrapak+)*\.[com]{2,5}(:[0-9]{1,5})?(\/.*)?$/
         };
-
-		//if(!isTargetDialog($form,listOfResourceTypes)){
-          //  return;
-        //}else 
+ 
         if(dropdownValue==="enableSoftconversion" && !patterns.pardotUrlRegex.test(pardotUrl)) {
             gAuthor.ui.helpers.prompt({
                 title: "Invalid Pardot URL - Softconversion Form",
