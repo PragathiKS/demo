f4efd3c9da2bf81f3bb4b4b1f10a1b96cfd7757f
Nagavekar Bhakti <INNAGAVEKARB@tetrapak.com>
2021-08-23 13:47:18 +0530

    code commit for SMAR-22827 china wechat new requirement

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/FooterConfigurationModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/FooterConfigurationModel.java
@@ -10,7 +10,6 @@ import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
-
 import com.tetrapak.publicweb.core.constants.PWConstants;
 import com.tetrapak.publicweb.core.models.multifield.FooterLinkModel;
 import com.tetrapak.publicweb.core.models.multifield.SocialLinkModel;
@@ -21,127 +20,135 @@ import com.tetrapak.publicweb.core.models.multifield.SocialLinkModel;
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class FooterConfigurationModel {
 
-    /** The request. */
-    @Self
-    private Resource resource;
+	/** The request. */
+	@Self
+	private Resource resource;
 
-    /** The logo image path. */
-    @ValueMapValue
-    private String logoImagePath;
+	/** The logo image path. */
+	@ValueMapValue
+	private String logoImagePath;
 
-    /** The logo link. */
-    @ValueMapValue
-    private String logoLink;
+	/** The logo link. */
+	@ValueMapValue
+	private String logoLink;
 
-    /** The logo alt. */
-    @ValueMapValue
-    private String logoAlt;
+	/** The logo alt. */
+	@ValueMapValue
+	private String logoAlt;
 
-    /** The logo background. */
-    @ValueMapValue
-    private String logoBackground;
+	/** The logo background. */
+	@ValueMapValue
+	private String logoBackground;
 
-    /** The social links. */
-    @Inject
-    private List<SocialLinkModel> socialLinks;
+	/** The social links. */
+	@Inject
+	private List<SocialLinkModel> socialLinks;
 
-    /** The footer links. */
-    @Inject
-    private List<FooterLinkModel> footerLinks;
+	/** The footer links. */
+	@Inject
+	private List<FooterLinkModel> footerLinks;
 
-    /** The go to top label. */
-    @ValueMapValue
-    private String goToTopLabel;
-    
-    /** QR code Text*/
-    @ValueMapValue
-    private String wechatQrCodeText;
+	/** The go to top label. */
+	@ValueMapValue
+	private String goToTopLabel;
 
-    /** QR code */
-    @ValueMapValue
-    private String wechatQrCodeReference;
+	/** QR code Text */
+	@ValueMapValue
+	private String wechatQrCodeText;
 
-    /** Alt Text*/
-    @ValueMapValue
-    private String qrAltText;
-    
-    /** App Store */
-    @ValueMapValue
-    private String appStoreReference;
-    
-    /** Google play */
-    @ValueMapValue
-    private String googlePlayReference;
+	/** QR code */
+	@ValueMapValue
+	private String wechatQrCodeReference;
 
-    /**
-     * Gets the logo image path.
-     *
-     * @return the logo image path
-     */
-    public String getLogoImagePath() {
-        return logoImagePath;
-    }
+	/** Alt Text */
+	@ValueMapValue
+	private String qrAltText;
 
-    /**
-     * Gets the logo link.
-     *
-     * @return the logo link
-     */
-    public String getLogoLink() {
-        return logoLink;
-    }
+	/** App Store */
+	@ValueMapValue
+	private String appStoreReference;
 
-    /**
-     * Gets the logo alt.
-     *
-     * @return the logo alt
-     */
-    public String getLogoAlt() {
-        return logoAlt;
-    }
+	/** App Store Alt text */
+	@ValueMapValue
+	private String appStoreAltText;
 
-    /**
-     * Gets the logo background.
-     *
-     * @return the logo background
-     */
-    public String getLogoBackground() {
-        return logoBackground;
-    }
+	/** Google play */
+	@ValueMapValue
+	private String googlePlayReference;
 
-    /**
-     * Gets the social links.
-     *
-     * @return the social links
-     */
-    public List<SocialLinkModel> getSocialLinks() {
-        final List<SocialLinkModel> lists = new ArrayList<>();
-        String countryCode = resource.getPath().split(PWConstants.SLASH)[5];
-        if (Objects.nonNull(socialLinks)) {
-        	final String [] valueArray = {"Xing","Vkontakte"};
-        	lists.addAll(socialLinks);
-        	if (countryCode.equalsIgnoreCase(PWConstants.RU_COUNTRY_CODE)) {
-        		modifyList(lists,valueArray[0]);
-            } else if (countryCode.equalsIgnoreCase(PWConstants.DE_COUNTRY_CODE)) {
-            	modifyList(lists,valueArray[1]);
-            } else {
-            	for (String value : valueArray) {
-            		modifyList(lists, value);
-            	}
-            }
-        }
-        return lists;
-    }
-    
-    /**
-     * Modify list.
-     *
-     * @param lists the lists
-     * @param socialName the social name
-     */
-    private void modifyList(final List<SocialLinkModel> lists, String socialName) {
+	/** Google play Alt Text */
+	@ValueMapValue
+	private String googlePlayAltText;
+
+	/**
+	 * Gets the logo image path.
+	 *
+	 * @return the logo image path
+	 */
+	public String getLogoImagePath() {
+		return logoImagePath;
+	}
+
+	/**
+	 * Gets the logo link.
+	 *
+	 * @return the logo link
+	 */
+	public String getLogoLink() {
+		return logoLink;
+	}
+
+	/**
+	 * Gets the logo alt.
+	 *
+	 * @return the logo alt
+	 */
+	public String getLogoAlt() {
+		return logoAlt;
+	}
+
+	/**
+	 * Gets the logo background.
+	 *
+	 * @return the logo background
+	 */
+	public String getLogoBackground() {
+		return logoBackground;
+	}
+
+	/**
+	 * Gets the social links.
+	 *
+	 * @return the social links
+	 */
+	public List<SocialLinkModel> getSocialLinks() {
+		final List<SocialLinkModel> lists = new ArrayList<>();
+		String countryCode = resource.getPath().split(PWConstants.SLASH)[5];
+		if (Objects.nonNull(socialLinks)) {
+			final String[] valueArray = { "Xing", "Vkontakte" };
+			lists.addAll(socialLinks);
+			if (countryCode.equalsIgnoreCase(PWConstants.RU_COUNTRY_CODE)) {
+				modifyList(lists, valueArray[0]);
+			} else if (countryCode.equalsIgnoreCase(PWConstants.DE_COUNTRY_CODE)) {
+				modifyList(lists, valueArray[1]);
+			} else {
+				for (String value : valueArray) {
+					modifyList(lists, value);
+				}
+			}
+		}
+		return lists;
+	}
+
+	/**
+	 * Modify list.
+	 *
+	 * @param lists      the lists
+	 * @param socialName the social name
+	 */
+	private void modifyList(final List<SocialLinkModel> lists, String socialName) {
 		Iterator<SocialLinkModel> itr = lists.iterator();
-		while(itr.hasNext()) {
+		while (itr.hasNext()) {
 			SocialLinkModel modelEntry = itr.next();
 			if (modelEntry.getSocialMedia().equalsIgnoreCase(socialName)) {
 				lists.remove(modelEntry);
@@ -150,55 +157,55 @@ public class FooterConfigurationModel {
 		}
 	}
 
-    /**
-     * Gets the footer link.
-     *
-     * @return the footer link
-     */
-    public List<FooterLinkModel> getFooterLinks() {
-        final List<FooterLinkModel> lists = new ArrayList<>();
-        if (Objects.nonNull(footerLinks)) {
-            lists.addAll(footerLinks);
-        }
-        return lists;
+	/**
+	 * Gets the footer link.
+	 *
+	 * @return the footer link
+	 */
+	public List<FooterLinkModel> getFooterLinks() {
+		final List<FooterLinkModel> lists = new ArrayList<>();
+		if (Objects.nonNull(footerLinks)) {
+			lists.addAll(footerLinks);
+		}
+		return lists;
+	}
 
-    }
+	/**
+	 * Gets the go to top label.
+	 *
+	 * @return the go to top label
+	 */
+	public String getGoToTopLabel() {
+		return goToTopLabel;
+	}
 
-    /**
-     * Gets the go to top label.
-     *
-     * @return the go to top label
-     */
-    public String getGoToTopLabel() {
-        return goToTopLabel;
-    }
+	/**
+	 * Gets the We chat QR code.
+	 *
+	 * @return the We chat QR code
+	 */
+	public String getWechatQrCodeReference() {
+		return wechatQrCodeReference;
+	}
 
-    /**
-     * Gets the We chat QR code.
-     *
-     * @return the We chat QR code
-     */
-    public String getWechatQrCodeReference() {
-        return wechatQrCodeReference;
-    }
+	/**
+	 * Gets the Image Alt text.
+	 *
+	 * @return the Image Alt text
+	 */
+	public String getQrAltText() {
+		return qrAltText;
+	}
 
-    /**
-     * Gets the Image Alt text.
-     *
-     * @return the Image Alt text
-     */
-    public String getQrAltText() {
-        return qrAltText;
-    }
-    /**
-     * Gets the QR code text.
-     * 
-     * @return the QR code text
-     */
-    public String getWechatQrCodeText() {
+	/**
+	 * Gets the QR code text.
+	 * 
+	 * @return the QR code text
+	 */
+	public String getWechatQrCodeText() {
 		return wechatQrCodeText;
 	}
-  
+
 	/**
 	 * Gets the App store.
 	 * 
@@ -206,6 +213,15 @@ public class FooterConfigurationModel {
 	 */
 	public String getAppStoreReference() {
 		return appStoreReference;
+	}
+
+	/**
+	 * Gets the App store Alt text.
+	 * 
+	 * @return the App store Alt text.
+	 */
+	public String getAppStoreAltText() {
+		return appStoreAltText;
 	}
 
 	/**
@@ -217,4 +233,12 @@ public class FooterConfigurationModel {
 		return googlePlayReference;
 	}
 
+	/**
+	 * Gets the Google play Alt text.
+	 * 
+	 * @return the Google play Alt text.
+	 */
+	public String getGooglePlayAltText() {
+		return googlePlayAltText;
+	}
 }
--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/FooterModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/FooterModel.java
@@ -3,9 +3,7 @@ package com.tetrapak.publicweb.core.models;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-
 import javax.annotation.PostConstruct;
-
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
@@ -13,7 +11,6 @@ import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import com.tetrapak.publicweb.core.models.multifield.FooterLinkModel;
 import com.tetrapak.publicweb.core.models.multifield.SocialLinkModel;
 import com.tetrapak.publicweb.core.utils.LinkUtils;
@@ -24,160 +21,169 @@ import com.tetrapak.publicweb.core.utils.LinkUtils;
 @Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class FooterModel {
 
-    /** The Constant LOGGER. */
-    private static final Logger LOGGER = LoggerFactory.getLogger(FooterModel.class);
+	/** The Constant LOGGER. */
+	private static final Logger LOGGER = LoggerFactory.getLogger(FooterModel.class);
 
-    /** The request. */
-    @SlingObject
-    private SlingHttpServletRequest request;
+	/** The request. */
+	@SlingObject
+	private SlingHttpServletRequest request;
 
-    /** The logo image path. */
-    private String logoImagePath;
+	/** The logo image path. */
+	private String logoImagePath;
 
-    /** The logo link. */
-    private String logoLink;
+	/** The logo link. */
+	private String logoLink;
 
-    /** The logo alt. */
-    private String logoAlt;
+	/** The logo alt. */
+	private String logoAlt;
 
-    /** The social links. */
-    private List<SocialLinkModel> socialLinks;
-    
-    /** The footer links. */
-    private List<FooterLinkModel> footerLinksSanitized = new ArrayList<>();
+	/** The social links. */
+	private List<SocialLinkModel> socialLinks;
 
-    /** The go to top label. */
-    private String goToTopLabel;
-    
-    /** We chat QR code Text */
-    private String wechatQrCodeText;
+	/** The footer links. */
+	private List<FooterLinkModel> footerLinksSanitized = new ArrayList<>();
 
-    /** We chat QR code */
-    private String wechatQrCodeReference;
-    
-    /** We chat Alt text */
-    private String qrAltText;
-    
-    /** We chat App Store */
-    private String appStoreReference;
-    
-    /** We chat Google play */
-    private String googlePlayReference;
+	/** The go to top label. */
+	private String goToTopLabel;
 
-    /**
-     * Inits the.
-     */
-    @PostConstruct
-    protected void init() {
-        final List<FooterLinkModel> footerLinks;
-        LOGGER.debug("inside init method");
-        final String rootPath = LinkUtils.getRootPath(request.getPathInfo());
-        final String path = rootPath + "/jcr:content/root/responsivegrid/footerconfiguration";
-        final Resource footerConfigurationResource = request.getResourceResolver().getResource(path);
-        if (Objects.nonNull(footerConfigurationResource)) {
-            final FooterConfigurationModel configurationModel = footerConfigurationResource
-                    .adaptTo(FooterConfigurationModel.class);
-            if (Objects.nonNull(configurationModel)) {
-                logoImagePath = configurationModel.getLogoImagePath();
-                logoLink = LinkUtils.sanitizeLink(configurationModel.getLogoLink(), request);
-                logoAlt = configurationModel.getLogoAlt();
-                socialLinks = configurationModel.getSocialLinks();
-                footerLinks = configurationModel.getFooterLinks();
-                for(FooterLinkModel footerLink:footerLinks) {
-                    String sanitizedPath = LinkUtils.sanitizeLink(footerLink.getLinkPath(), request);
-                    footerLink.setLinkPath(sanitizedPath);
-                    footerLinksSanitized.add(footerLink);
+	/** We chat QR code Text */
+	private String wechatQrCodeText;
+
+	/** We chat QR code */
+	private String wechatQrCodeReference;
+
+	/** We chat Alt text */
+	private String qrAltText;
+
+	/** We chat App Store */
+	private String appStoreReference;
+
+	/** We chat App Store Alt text */
+	private String appStoreAltText;
+
+	/** We chat Google play */
+	private String googlePlayReference;
+
+	/** We chat Google play Alt text */
+	private String googlePlayAltText;
+
+	/**
+	 * Inits the.
+	 */
+	@PostConstruct
+	protected void init() {
+		final List<FooterLinkModel> footerLinks;
+		LOGGER.debug("inside init method");
+		final String rootPath = LinkUtils.getRootPath(request.getPathInfo());
+		final String path = rootPath + "/jcr:content/root/responsivegrid/footerconfiguration";
+		final Resource footerConfigurationResource = request.getResourceResolver().getResource(path);
+		if (Objects.nonNull(footerConfigurationResource)) {
+			final FooterConfigurationModel configurationModel = footerConfigurationResource
+					.adaptTo(FooterConfigurationModel.class);
+			if (Objects.nonNull(configurationModel)) {
+				logoImagePath = configurationModel.getLogoImagePath();
+				logoLink = LinkUtils.sanitizeLink(configurationModel.getLogoLink(), request);
+				logoAlt = configurationModel.getLogoAlt();
+				socialLinks = configurationModel.getSocialLinks();
+				footerLinks = configurationModel.getFooterLinks();
+				for (FooterLinkModel footerLink : footerLinks) {
+					String sanitizedPath = LinkUtils.sanitizeLink(footerLink.getLinkPath(), request);
+					footerLink.setLinkPath(sanitizedPath);
+					footerLinksSanitized.add(footerLink);
 				}
 				goToTopLabel = configurationModel.getGoToTopLabel();
 				wechatQrCodeText = configurationModel.getWechatQrCodeText();
 				wechatQrCodeReference = configurationModel.getWechatQrCodeReference();
 				qrAltText = configurationModel.getQrAltText();
 				appStoreReference = configurationModel.getAppStoreReference();
+				appStoreAltText = configurationModel.getAppStoreAltText();
 				googlePlayReference = configurationModel.getGooglePlayReference();
+				googlePlayAltText = configurationModel.getGooglePlayAltText();
 			}
 		}
-    }
+	}
 
-    /**
-     * Gets the logo image path.
-     *
-     * @return the logo image path
-     */
-    public String getLogoImagePath() {
-        return logoImagePath;
-    }
+	/**
+	 * Gets the logo image path.
+	 *
+	 * @return the logo image path
+	 */
+	public String getLogoImagePath() {
+		return logoImagePath;
+	}
 
-    /**
-     * Gets the logo link.
-     *
-     * @return the logo link
-     */
-    public String getLogoLink() {
-        return logoLink;
-    }
+	/**
+	 * Gets the logo link.
+	 *
+	 * @return the logo link
+	 */
+	public String getLogoLink() {
+		return logoLink;
+	}
 
-    /**
-     * Gets the logo alt.
-     *
-     * @return the logo alt
-     */
-    public String getLogoAlt() {
-        return logoAlt;
-    }
+	/**
+	 * Gets the logo alt.
+	 *
+	 * @return the logo alt
+	 */
+	public String getLogoAlt() {
+		return logoAlt;
+	}
 
-    /**
-     * Gets the social links.
-     *
-     * @return the social links
-     */
-    public List<SocialLinkModel> getSocialLinks() {
-        return new ArrayList<>(socialLinks);
-    }
+	/**
+	 * Gets the social links.
+	 *
+	 * @return the social links
+	 */
+	public List<SocialLinkModel> getSocialLinks() {
+		return new ArrayList<>(socialLinks);
+	}
 
-    /**
-     * Gets the footer links.
-     *
-     * @return the footer links
-     */
-    public List<FooterLinkModel> getFooterLinks() {
-        return new ArrayList<>(footerLinksSanitized);
-    }
+	/**
+	 * Gets the footer links.
+	 *
+	 * @return the footer links
+	 */
+	public List<FooterLinkModel> getFooterLinks() {
+		return new ArrayList<>(footerLinksSanitized);
+	}
 
-    /**
-     * Gets the go to top label.
-     *
-     * @return the go to top label
-     */
-    public String getGoToTopLabel() {
-        return goToTopLabel;
-    }
+	/**
+	 * Gets the go to top label.
+	 *
+	 * @return the go to top label
+	 */
+	public String getGoToTopLabel() {
+		return goToTopLabel;
+	}
 
-    /**
-     * Gets the We chat QR code.
-     *
-     * @return the We chat QR code
-     */
-    public String getWechatQrCodeReference() {
-        return wechatQrCodeReference;
-    }
+	/**
+	 * Gets the We chat QR code.
+	 *
+	 * @return the We chat QR code.
+	 */
+	public String getWechatQrCodeReference() {
+		return wechatQrCodeReference;
+	}
 
-    /**
-     * Gets the Image Alt text.
-     *
-     * @return the Image Alt text
-     */
-    public String getQrAltText() {
-        return qrAltText;
-    }
-    /**
-     * Gets the QR code text.
-     * 
-     * @return the QR code text
-     */
-    public String getWechatQrCodeText() {
+	/**
+	 * Gets the Image Alt text.
+	 *
+	 * @return the Image Alt text.
+	 */
+	public String getQrAltText() {
+		return qrAltText;
+	}
+
+	/**
+	 * Gets the QR code text.
+	 * 
+	 * @return the QR code text.
+	 */
+	public String getWechatQrCodeText() {
 		return wechatQrCodeText;
 	}
-  
+
 	/**
 	 * Gets the App store.
 	 * 
@@ -185,6 +191,15 @@ public class FooterModel {
 	 */
 	public String getAppStoreReference() {
 		return appStoreReference;
+	}
+
+	/**
+	 * Gets the App store Alt text.
+	 * 
+	 * @return the App store Alt text.
+	 */
+	public String getAppStoreAltText() {
+		return appStoreAltText;
 	}
 
 	/**
@@ -196,4 +211,12 @@ public class FooterModel {
 		return googlePlayReference;
 	}
 
+	/**
+	 * Gets the Google play Alt text.
+	 * 
+	 * @return the Google play Alt text.
+	 */
+	public String getGooglePlayAltText() {
+		return googlePlayAltText;
+	}
 }
