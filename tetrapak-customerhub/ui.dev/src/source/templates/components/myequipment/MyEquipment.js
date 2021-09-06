7674c95e5243310a0deafc8957b89b601bd1e67e
Ovidiu Schintee <s.ovidiu@gmail.com>
2021-09-06 16:57:23 +0300

    SMAR-23159 default columns for Installed Equipment

--- tetrapak-customerhub/ui.dev/src/source/templates/components/myequipment/MyEquipment.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/myequipment/MyEquipment.js
@@ -493,11 +493,11 @@ class MyEquipment {
       {key:'countryCode',option:i18nKeys['country'],isChecked:true,index:0},
       {key:'lineName',option:i18nKeys['line'],isChecked:true,index:1},
       {key:'equipmentStatus',option:i18nKeys['equipmentStatus'],isChecked:true,index:2},
-      {key:'functionalLocation',option:i18nKeys['functionalLocation'],isChecked:true,index:3},
+      {key:'functionalLocation',option:i18nKeys['functionalLocation'],isChecked:false,index:3},
       {key:'serialNumber',option:i18nKeys['serialNumber'],isChecked:true,index:4},
       {key:'siteName',option:i18nKeys['site'],isChecked:true,index:5},
-      {key:'siteDesc',option:i18nKeys['siteDescription'],isChecked:true,index:6},
-      {key:'location',option:i18nKeys['location'],isChecked:true,index:7},
+      {key:'siteDesc',option:i18nKeys['siteDescription'],isChecked:false,index:6},
+      {key:'location',option:i18nKeys['location'],isChecked:false,index:7},
       {key:'equipmentTypeDesc',option:i18nKeys['equipmentDescription'],isChecked:true,index:8}
     ];
 
