edb31d9025e5e0bd7e531cf079a09f42e75d6fc8
Mathur <aalekh.mathur@capgemini.com>
2021-09-06 10:00:46 +0200

    SMAR-19404_softconversion_form_improvements

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/SoftConversionFormConfigModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/SoftConversionFormConfigModel.java
@@ -32,6 +32,12 @@ public class SoftConversionFormConfigModel extends FormConfigModel {
 
     @ValueMapValue
     private String noButtonLabel;
+    
+    @ValueMapValue
+    private String positionTagsPath;
+    
+    @ValueMapValue
+    private String functionTagsPath;
 
 
     /**
@@ -86,6 +92,24 @@ public class SoftConversionFormConfigModel extends FormConfigModel {
      */
     public String getNoButtonLabel() {
         return noButtonLabel;
+    }
+    
+    /**
+     * Gets the position tag path.
+     *
+     * @return the position tag path
+     */
+    public String getPositionTagsPath() {
+    	return positionTagsPath;
+    }
+    
+    /**
+     * Gets the function tag path.
+     *
+     * @return the function tag path
+     */
+    public String getFunctionTagsPath() {
+    	return functionTagsPath;
     }
 
 }
