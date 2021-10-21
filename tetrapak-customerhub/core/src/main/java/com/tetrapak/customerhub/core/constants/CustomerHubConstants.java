caeb1d0a64816ca957226d6d15e6d9683d76d812
Ankit Pathak <61694126+ankitpathakcg@users.noreply.github.com>
2021-10-21 18:22:54 +0530

    SMAR-21264 : Added constant for equipment api parameter.

--- tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/constants/CustomerHubConstants.java
+++ tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/constants/CustomerHubConstants.java
@@ -1,111 +1,319 @@
 package com.tetrapak.customerhub.core.constants;
 
 /**
- * The CustomerHubConstants class
+ * The CustomerHubConstants class.
  */
 public final class CustomerHubConstants {
 
+    /**
+     * Instantiates a new customer hub constants.
+     */
     private CustomerHubConstants() {
         throw new IllegalStateException("Utility class");
     }
 
-    public static final String DEFAULT_LOCALE =  "en";
-    
+    /** The Constant DEFAULT_LOCALE. */
+    public static final String DEFAULT_LOCALE = "en";
+
+    /** The Constant GLOBAL_CONFIGURATION_RESOURCE_TYPE. */
     public static final String GLOBAL_CONFIGURATION_RESOURCE_TYPE = "customerhub/components/structure/globalconfiguration";
 
+    /** The Constant CQ_REDIRECT_PROPERTY. */
     public static final String CQ_REDIRECT_PROPERTY = "cq:redirectTarget";
 
+    /** The Constant HTML_EXTENSION. */
     public static final String HTML_EXTENSION = ".html";
 
+    /** The Constant RESPONSE_STATUS_FAILURE. */
     public static final String RESPONSE_STATUS_FAILURE = "failure";
 
+    /** The Constant RESPONSE_STATUS_SUCCESS. */
     public static final String RESPONSE_STATUS_SUCCESS = "success";
 
+    /** The Constant PATH_SEPARATOR. */
     public static final String PATH_SEPARATOR = "/";
 
+    /** The Constant ROOT_NODE. */
     public static final String ROOT_NODE = "root";
 
+    /** The Constant RESPONSIVE_GRID_NODE. */
     public static final String RESPONSIVE_GRID_NODE = "responsivegrid";
 
+    /** The Constant EMPTY_STRING. */
     public static final String EMPTY_STRING = "";
 
+    /** The Constant CQ_TEMPLATE. */
     public static final String CQ_TEMPLATE = "cq:template";
 
+    /** The Constant ORDER_PREFERENCES. */
     public static final String ORDER_PREFERENCES = "orderPreferences";
 
+    /** The Constant LANGUGAGE_PREFERENCES. */
     public static final String LANGUGAGE_PREFERENCES = "languagePreference";
 
+    /** The Constant RESULT. */
     public static final String RESULT = "result";
 
+    /** The Constant STATUS. */
     public static final String STATUS = "status";
-    
+
+    /** The Constant PDF. */
     public static final String PDF = "pdf";
-    
+
+    /** The Constant EXCEL. */
     public static final String EXCEL = "excel";
-    
+
+    /** The Constant RESPONSE_STATUS_OK. */
     public static final String RESPONSE_STATUS_OK = "200";
 
+    /** The Constant BOLD_IDENTIFIER. */
     public static final String BOLD_IDENTIFIER = "<b>";
 
-    public static final String TOKEN= "token";
-    
-    public static final String TEST_TOKEN= "J9ZGhxxIm5uvGgSBIPNqwCV3siBq";
-   
+    /** The Constant TOKEN. */
+    public static final String TOKEN = "token";
+
+    /** The Constant TEST_TOKEN. */
+    public static final String TEST_TOKEN = "J9ZGhxxIm5uvGgSBIPNqwCV3siBq";
+
+    /** The Constant FINANCIALS. */
     public static final String FINANCIALS = "Financials-";
-    
+
+    /** The Constant HYPHEN_STRING. */
     public static final String HYPHEN_STRING = "-";
 
-    public static final String ORDER_NUMBER= "orderNumber";
-    
-    /** Constant DAM_ASSETS_PROPERTY */
+    /** The Constant ORDER_NUMBER. */
+    public static final String ORDER_NUMBER = "orderNumber";
+
+    /** Constant DAM_ASSETS_PROPERTY. */
     public static final String DAM_ASSETS_PROPERTY = "dam:Asset";
-    
-    /** Constant APPLICATION_PDF */
+
+    /** Constant APPLICATION_PDF. */
     public static final String APPLICATION_PDF = "application/pdf";
-    
-    /** Constant DC_TITLE_PROPERTY */
+
+    /** Constant DC_TITLE_PROPERTY. */
     public static final String DC_TITLE_PROPERTY = "dc:title";
-    
-    /** Constant DC_FORMAT_PROPERTY */
+
+    /** Constant DC_FORMAT_PROPERTY. */
     public static final String DC_FORMAT_PROPERTY = "dc:format";
-    
-    /** Constant DAM_SIZE_PROPERTY */
+
+    /** Constant DAM_SIZE_PROPERTY. */
     public static final String DAM_SIZE_PROPERTY = "dam:size";
-    
-    /** Constant ASSET_PATH */
+
+    /** Constant ASSET_PATH. */
     public static final String ASSET_PATH = "assetPath";
-    
-    /** Constant ASSET_SIZE */
+
+    /** Constant ASSET_SIZE. */
     public static final String ASSET_SIZE = "assetSize";
-    
-    /** Constant ASSET_TITLE */
+
+    /** Constant ASSET_TITLE. */
     public static final String ASSET_TITLE = "assetTitle";
-    
-    /** Constant SIZE_MB */
+
+    /** Constant SIZE_MB. */
     public static final String SIZE_MB = "MB";
-    
-    /** Constant SLING_ORDERED_FOLDER */
+
+    /** Constant SLING_ORDERED_FOLDER. */
     public static final String CONSTANT_FOLDER = "folder";
-    
+
+    /** The Constant PW_CONTENT_DAM_PATH. */
     public static final String PW_CONTENT_DAM_PATH = "/content/dam/tetrapak/customerhub";
-    
+
+    /** The Constant JCR_PRIMARY_TYPE. */
     public static final String JCR_PRIMARY_TYPE = "jcr:primaryType";
-    
+
+    /** The Constant SPACE. */
     public static final String SPACE = " ";
     
-    public static final String EXCEL_DOWNLOAD_EXTENSION = ".download.excel";
-    
-    public static final String SKIP ="skip";
-    
+    /** The Constant EQUALS. */
     public static final String EQUALS = "=";
     
+    /** The Constant QUESTION_MARK. */
     public static final String QUESTION_MARK = "?";
     
+    /** The Constant AMPERSAND. */
     public static final String AMPERSAND = "&";
     
+    /** The Constant COUNTRY_CODE. */
     public static final String COUNTRY_CODE = "countrycode";
     
+    /** The Constant EXCEL_DOWNLOAD_EXTENSION. */
+    public static final String EXCEL_DOWNLOAD_EXTENSION = ".download.excel";
+
+    /** The Constant SKIP */
+    public static final String SKIP = "skip";
+
+    /** The Constant COUNT. */
     public static final String COUNT = "count";
-    
+
+    /** The Constant DOWNLOAD_EQUIPMENT_EXCEL_API_PARAMETER. */
     public static final String DOWNLOAD_EQUIPMENT_EXCEL_API_PARAMETER = "results=extended";
+
+    /** The Constant EQUIPMENT_INFORMATION. */
+    public static final String EQUIPMENT_INFORMATION = "equipmentInformation";
+
+    /** The Constant UPDATE_EQUIPMENT_INFORMATION. */
+    public static final String UPDATE_EQUIPMENT_INFORMATION = "updateEquipmentInformation";
+
+    /** The Constant EQUIPMENT_DETAILS. */
+    public static final String EQUIPMENT_DETAILS = "equipmentDetails";
+
+    /** The Constant ADDITIONAL_DETAILS. */
+    public static final String ADDITIONAL_DETAILS = "additionalDetails";
+
+    /** The Constant COUNTRY. */
+    public static final String COUNTRY = "country";
+
+    /** The Constant LOCATION. */
+    public static final String LOCATION = "location";
+
+    /** The Constant SITE. */
+    public static final String SITE = "site";
+
+    /** The Constant LINE. */
+    public static final String LINE = "line";
+
+    /** The Constant EQUIPMENT_STATUS. */
+    public static final String EQUIPMENT_STATUS = "equipmentstatus";
+
+    /** The Constant POSITION. */
+    public static final String POSITION = "position";
+
+    /** The Constant EQUIPMENT_DESCRIPTION. */
+    public static final String EQUIPMENT_DESCRIPTION = "equipmentDescription";
+
+    /** The Constant EQUIPMENT_TYPE. */
+    public static final String EQUIPMENT_TYPE = "equipmentType";
+
+    /** The Constant MACHINE_SYSTEM. */
+    public static final String MACHINE_SYSTEM = "machineSystem";
+
+    /** The Constant MATERIAL. */
+    public static final String MATERIAL = "material";
+
+    /** The Constant MANUFACTURER_MODEL_NUMBER. */
+    public static final String MANUFACTURER_MODEL_NUMBER = "manufacturerModelNumber";
+
+    /** The Constant MANUFACTURER_SERIAL_NUMBER. */
+    public static final String MANUFACTURER_SERIAL_NUMBER = "manufacturerSerialNumber";
+
+    /** The Constant SUPERIOR_EQUIPMENT. */
+    public static final String SUPERIOR_EQUIPMENT = "superiorEquipment";
+
+    /** The Constant FUNCTIONAL_LOCATION. */
+    public static final String FUNCTIONAL_LOCATION = "functionalLocation";
+
+    /** The Constant FUNCTIONAL_LOCATION_DESC. */
+    public static final String FUNCTIONAL_LOCATION_DESC = "functionalLocationDesc";
+
+    /** The Constant MANUFACTURER. */
+    public static final String MANUFACTURER = "manufacturer";
+
+    /** The Constant MANUFACTURER_COUNTRY. */
+    public static final String MANUFACTURER_COUNTRY = "manufacturerCountry";
+
+    /** The Constant CONSTRUCTION_YEAR. */
+    public static final String CONSTRUCTION_YEAR = "constructionYear";
+
+    /** The Constant CUSTOMER_WARRANTY_END_DATE. */
+    public static final String CUSTOMER_WARRANTY_END_DATE = "customerWarrantyEndDate";
+
+    /** The Constant CUSTOMER_WARRANTY_START_DATE. */
+    public static final String CUSTOMER_WARRANTY_START_DATE = "customerWarrantyStartDate";
+
+    /** The Constant MY_EQUIPMENT. */
+    public static final String MY_EQUIPMENT = "myEquipment";
+
+    /** The Constant REMOVE_ALL. */
+    public static final String REMOVE_ALL = "removeAll";
+
+    /** The Constant APPLY_FILTER. */
+    public static final String APPLY_FILTER = "applyFilter";
+
+    /** The Constant SERIAL_NUMBER. */
+    public static final String SERIAL_NUMBER = "serialNumber";
+
+    /** The Constant CUSTOMER. */
+    public static final String CUSTOMER = "customer";
+
+    /** The Constant SEARCH_RESULTS. */
+    public static final String SEARCH_RESULTS = "searchResults";
+
+    /** The Constant BUSINESS_TYPE. */
+    public static final String BUSINESS_TYPE = "businessType";
+
+    /** The Constant EQUIPMENT_CATEGORY. */
+    public static final String EQUIPMENT_CATEGORY = "equipmentCategory";
+
+    /** The Constant CONFIRMATION_DATE. */
+    public static final String CONFIRMATION_DATE = "eofsConfirmationDate";
+
+    /** The Constant VALID_FROM_DATE. */
+    public static final String VALID_FROM_DATE = "eofsValidFromDate";
+
+    /** The Constant FIRST. */
+    public static final String FIRST = "first";
+
+    /** The Constant LAST. */
+    public static final String LAST = "last";
+
+    /** The Constant CUSTOMIZE_TABLE. */
+    public static final String CUSTOMIZE_TABLE = "customizeTable";
+
+    /** The Constant SERIAL_NUM_TOOL_TIP. */
+    public static final String SERIAL_NUM_TOOL_TIP = "serialNumToolTip";
+
+    /** The Constant FUNCTIONAL_LOCATON. */
+    public static final String FUNCTIONAL_LOCATON = "functionalLocation";
+
+    /** The Constant SITE_DESCRIPTION. */
+    public static final String SITE_DESCRIPTION = "siteDescription";
+
+    /** The Constant LOCATION_TOOL_TIP. */
+    public static final String LOCATION_TOOL_TIP = "locationToolTip";
+
+    /** The Constant SITE_DESC_TOOL_TIP. */
+    public static final String SITE_DESC_TOOL_TIP = "siteDescToolTip";
+
+    /** The Constant FUNCTIONAL_LOCATION_TOOL_TIP. */
+    public static final String FUNCTIONAL_LOCATION_TOOL_TIP = "functionalLocationToolTip";
+
+    /** The Constant LINE_TOOL_TIP. */
+    public static final String LINE_TOOL_TIP = "lineToolTip";
+
+    /** The Constant SITE_TOOL_TIP. */
+    public static final String SITE_TOOL_TIP = "siteToolTip";
+
+    /** The Constant COUNTRY_TOOL_TIP. */
+    public static final String COUNTRY_TOOL_TIP = "countryToolTip";
+
+    /** The Constant EQUIPMENT_DESC_TOOL_TIP. */
+    public static final String EQUIPMENT_DESC_TOOL_TIP = "equipDescToolTip";
+
+    /** The Constant EQUIP_STAT_TOOL_TIP. */
+    public static final String EQUIP_STAT_TOOL_TIP = "equipStatToolTip";
+
+    /** The Constant API_ERROR_CODES. */
+    public static final String API_ERROR_CODES = "apiErrorCodes";
+
+    /** The Constant HIDE_AND_SHOW_CTA. */
+    public static final String HIDE_AND_SHOW_CTA = "hideAndShowCta";
+
+    /** The Constant NO_DATA_FOUND. */
+    public static final String NO_DATA_FOUND = "noDataFound";
+
+    /** The Constant PERMANENT_VOLUME_CONV. */
+    public static final String PERMANENT_VOLUME_CONV = "permanentVolumeConversion";
+
+    /** The Constant SHOW_ALL_FILTERS. */
+    public static final String SHOW_ALL_FILTERS = "showallfilters";
+
+    /** The Constant HIDE_ALL_FILTERS. */
+    public static final String HIDE_ALL_FILTERS = "hideallfilters";
+
+    /** The Constant REMOVE_ALL_FILTERS. */
+    public static final String REMOVE_ALL_FILTERS = "removeallfilters";
+    
+    /** The Constant EQUIPMENT_LIST_API. */
+    public static final String EQUIPMENT_LIST_API = "myequipment-equipmentlist";
+
 }
+
