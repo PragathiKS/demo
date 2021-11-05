288d54111022a1258ddc2a08d61bad2d7441f97f
rafalmeller <rafal.meller@tetrapak.com>
2021-11-05 16:39:24 +0100

    feat: SMAR-19697 | component create

---
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/addequipment/AddEquipment.js
@@ -0,0 +1,33 @@
+import $ from 'jquery';
+
+class AddEquipment {
+  constructor({ el }) {
+    this.root = $(el);
+  }
+  cache = {};
+  initCache() {
+    /* Initialize selector cache here */
+    /**
+     * Use "this.root" to find elements within current component
+     * Example:
+     * this.cache.$submitBtn = this.root.find('.js-submit-btn');
+     */
+  }
+  bindEvents() {
+    /* Bind jQuery events here */
+    /**
+     * Example:
+     * const { $submitBtn } = this.cache;
+     * $submitBtn.on('click', () => { ... });
+     */
+    // eslint-disable-next-line no-console
+    console.log('THIS IS MY ADD EQUIPMENT');
+  }
+  init() {
+    /* Mandatory method */
+    this.initCache();
+    this.bindEvents();
+  }
+}
+
+export default AddEquipment;
---
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/addequipment/AddEquipment.spec.js
@@ -0,0 +1,9 @@
+import AddEquipment from './AddEquipment';
+
+describe('AddEquipment', function () {
+  before(function () {
+    this.addEquipment = new AddEquipment({
+      el: document.body
+    });
+  });
+});
