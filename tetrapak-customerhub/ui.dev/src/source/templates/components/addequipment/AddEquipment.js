ea2cd025fb7a809c10ccd5e2ba980115fa9f7de9
rafalmeller <rafal.meller@tetrapak.com>
2021-11-08 16:32:54 +0100

    feat: SMAR-19697 | add equipment layout

--- tetrapak-customerhub/ui.dev/src/source/templates/components/addequipment/AddEquipment.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/addequipment/AddEquipment.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
+import { render } from '../../../scripts/utils/render';
+import { logger } from '../../../scripts/utils/logger';
 
 class AddEquipment {
   constructor({ el }) {
@@ -6,12 +8,15 @@ class AddEquipment {
   }
   cache = {};
   initCache() {
-    /* Initialize selector cache here */
-    /**
-     * Use "this.root" to find elements within current component
-     * Example:
-     * this.cache.$submitBtn = this.root.find('.js-submit-btn');
-     */
+    this.cache.$contentWrapper = this.root.find('.tp-add-equipment__content-wrapper');
+    this.cache.$spinner = this.root.find('.tp-spinner');
+    this.cache.configJson = this.root.find('.js-tp-add-equipment__config').text();
+    try {
+      this.cache.i18nKeys = JSON.parse(this.cache.configJson);
+    } catch (e) {
+      this.cache.i18nKeys = {};
+      logger.error(e);
+    }
   }
   bindEvents() {
     /* Bind jQuery events here */
@@ -20,12 +25,22 @@ class AddEquipment {
      * const { $submitBtn } = this.cache;
      * $submitBtn.on('click', () => { ... });
      */
-    // eslint-disable-next-line no-console
-    console.log('THIS IS MY ADD EQUIPMENT');
+  }
+  renderLayout() {
+    const $this = this;
+    render.fn({
+      template: 'addEquipmentForm',
+      target: '.js-tp-add-equipment__form',
+      data: $this.cache.i18nKeys
+    }, () => {
+      $this.cache.$contentWrapper.removeClass('d-none');
+      $this.cache.$spinner.addClass('d-none');
+    });
   }
   init() {
     /* Mandatory method */
     this.initCache();
+    this.renderLayout();
     this.bindEvents();
   }
 }
