f2eea707ed298444d6a0e3102c1a2d595da546a3
Ovidiu Schintee <s.ovidiu@gmail.com>
2021-11-09 22:57:44 +0200

    SMAR-24620 Fix analytics tracking object for Soft conversion

--- tetrapak-publicweb/ui.dev/src/source/templates/components/softconversion/Softconversion.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/softconversion/Softconversion.js
@@ -278,7 +278,17 @@ class Softconversion {
     if(this.root.find(`#market-consent-${this.cache.$componentName}`).is(':checked')){
       apiPayload.marketingConsent = this.root.find(`#market-consent-${this.cache.$componentName}`).is(':checked');
     }
-    loadDownloadReady(this.mainHeading, { 'Marketing Consent': apiPayload.marketingConsent ? 'Checked':'Unchecked' }, this.cache.$parentComponent);
+
+    const dataObj = {
+      'E-mail': apiPayload.email,
+      'First name': apiPayload.firstName,
+      'Last name': apiPayload.lastName,
+      'Country/Location': apiPayload.country,
+      'Visitor Type': apiPayload.visitorType,
+      'Marketing Consent': apiPayload.marketingConsent ? 'Checked':'Unchecked'
+    };
+
+    loadDownloadReady(this.mainHeading, dataObj, this.cache.$parentComponent);
 
     // IF UTM fields in URL
     const params = {};
