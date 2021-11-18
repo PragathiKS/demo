8859c20edca2aa642bbf370d9218544ae0eecc2b
Sameer Srivastava <sameer.srivastava@publicissapient.com>
2021-11-18 13:26:55 +0530

    SMAR-23007 - Incorporated review comments

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/servlets/SubscriptionFormPardotServlet.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/servlets/SubscriptionFormPardotServlet.java
@@ -1,9 +1,9 @@
 package com.tetrapak.publicweb.core.servlets;
 
 import java.io.IOException;
+
 import javax.servlet.Servlet;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.http.auth.AuthenticationException;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
@@ -15,9 +15,8 @@ import org.osgi.service.component.annotations.Reference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.tetrapak.publicweb.core.constants.PWConstants;
 import com.tetrapak.publicweb.core.services.PardotService;
-import com.tetrapak.publicweb.core.utils.LinkUtils;
+import com.tetrapak.publicweb.core.utils.GlobalUtil;
 
 /**
  * The Class SubscriptionFormPardotServlet.
@@ -50,11 +49,8 @@ public class SubscriptionFormPardotServlet extends SlingAllMethodsServlet {
      */
     @Override
     protected void doPost(final SlingHttpServletRequest request, final SlingHttpServletResponse resp) {
-        try {           
-            String marketPath = LinkUtils.getCountryPath(request.getPathInfo());
-            final String country = StringUtils.substringAfterLast(marketPath, PWConstants.SLASH);
-            if (country.equalsIgnoreCase(PWConstants.CHINA_COUNTRY_CODE)
-                    || (request.getParameterMap().get(PWConstants.COUNTRY_TITLE)[0]).equalsIgnoreCase(PWConstants.CHINA)) {
+        try {                       
+            if (GlobalUtil.isChinaDataFlow(request)) {
                 submitCustomFormData(request);
             } else {
                 pardotService.submitPardotPostRespose(request.getParameterMap());
@@ -89,6 +85,6 @@ public class SubscriptionFormPardotServlet extends SlingAllMethodsServlet {
             throws IOException {
         resp.setContentType("text/html; charset=UTF-8");
         resp.setCharacterEncoding("UTF-8");
-        resp.getWriter().write("Success");
+        resp.getWriter().write(resp.getStatus());
     }
 }
