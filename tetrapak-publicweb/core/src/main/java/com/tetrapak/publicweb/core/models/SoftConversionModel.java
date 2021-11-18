77aa9114717aae4a752576b776bbbb7a91bbdb21
Sameer Srivastava <sameer.srivastava@publicissapient.com>
2021-11-19 00:40:13 +0530

    SMAR-24834 - Updated key value

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/SoftConversionModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/SoftConversionModel.java
@@ -215,7 +215,7 @@ public class SoftConversionModel extends FormModel {
 		    	while(childTagsIterator.hasNext()) {
 		    		DropdownOption option = new DropdownOption();
 		    		final Tag tag = childTagsIterator.next();
-		    		option.setKey(tag.getLocalizedTitle(Locale.ENGLISH));
+		    		option.setKey(tag.getTitle());
 		    		option.setValue(tag.getTitle(PageUtil.getPageLocale(PageUtil.getCurrentPage(resource))));
 		    		tagOptions.add(option);
 		    	}
