da112fd6e5e5333ce05f63472ab6a48f31c69af4
Piotr Baran <sebaranp@tetrapak.com>
2021-12-07 21:46:00 +0530

    SMAR-25129|soft-conversion-analytics

---
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/cotssupport/CotsSupport.spec.js
@@ -0,0 +1,92 @@
+import $ from 'jquery';
+import CotsSupport from './CotsSupport';
+import cotsSupportTemplate from '../../../test-templates-hbs/cotsSupport.hbs';
+
+const helpers = {
+  fillEmptyInputs() {
+    $('input:text')
+        .filter((idx, el) => !$(el).val())
+        .val('Test data');
+  }
+}
+
+describe('CotsSupport', function () {
+  before(function () {
+    $(document.body).empty().html(cotsSupportTemplate());
+    this.cotsSupport = new CotsSupport({
+      el: document.body,
+    });
+    this.initSpy = sinon.spy(this.cotsSupport, 'init');
+    this.renderTypeOfQueryForm = sinon.spy(
+      this.cotsSupport,
+      'renderTypeOfQueryForm'
+    );
+    this.renderConfirmationDetailsFormSpy = sinon.spy(
+      this.cotsSupport,
+      'renderConfirmationDetailsForm'
+    );
+    this.renderSuccessMessageSpy = sinon.spy(
+      this.cotsSupport,
+      'renderSuccessMessage'
+    );
+    this.addErrorMsgSpy = sinon.spy(this.cotsSupport, 'addErrorMsg');
+    this.cotsSupport.init();
+  });
+
+  after(function () {
+    $(document.body).empty();
+    this.initSpy.restore();
+    this.renderTypeOfQueryForm.restore();
+    this.renderConfirmationDetailsFormSpy.restore();
+    this.renderSuccessMessageSpy.restore();
+    this.addErrorMsgSpy.restore();
+  });
+
+  it('should initialize', function (done) {
+    expect(this.initSpy.called).to.be.true;
+    done();
+  });
+
+  it('should render type of query form', function (done) {
+    expect(this.renderTypeOfQueryForm.called).to.be.true;
+    done();
+  });
+
+  it('should render form validation errors', function (done) {
+    $('.js-tp-cots-support__submit-type-of-query').trigger('click');
+    expect(this.renderConfirmationDetailsFormSpy.called).to.be.false;
+    expect(this.addErrorMsgSpy.called).to.be.true;
+    done();
+  });
+
+  it('should render confirmation details form after type of query submit', function (done) {
+    $('#technicalIssues[value=technicalIssues]').prop('checked', true);
+    $('#urgency option:eq(2)').prop('selected', true);
+    $('#affectedSystems').val('System 1');
+    $('#softwareVersion').val('1.0.0');
+    $('#description').val('Test description');
+    $('#comments').val('Test content');
+    $('.js-tp-cots-support__submit-type-of-query').trigger('click');
+    expect(this.renderConfirmationDetailsFormSpy.called).to.be.true;
+    done();
+  });
+
+  it('should render success message after confirmation detail submit', function (done) {
+    $('#emailAddress').val('example@example.com');
+    $('#telephone').val('461234567890');
+    helpers.fillEmptyInputs();
+    $('.js-tp-cots-support__submit-confirmation-details').trigger('click');
+    expect(this.renderSuccessMessageSpy.called).to.be.true;
+    done();
+  });
+
+  it('should handle config read error', function () {
+    $(document.body).empty().html(cotsSupportTemplate());
+    $('.js-tp-cots-support__config').remove();
+    this.cotsSupport = new CotsSupport({
+      el: document.body,
+    });
+    this.cotsSupport.init();
+    expect(this.cotsSupport.cache.i18nKeys).to.be.deep.equal({});
+  });
+});
