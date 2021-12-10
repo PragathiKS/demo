cfbe953b19c0fb0be30e88f04f8a6940ba87407d
Hiren Parmar <hiren.parmar@digitas.com>
2021-12-10 19:33:25 +0530

    Extra semicolumn removed

--- tetrapak-publicweb/ui.dev/src/source/templates/components/businessinquiryform/Businessinquiryform.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/businessinquiryform/Businessinquiryform.js
@@ -543,7 +543,7 @@ class Businessinquiryform {
 
         if(target === '#bef-step-2') {
           formTypeTitle = $(this).closest('.tab-content-steps').find('h4').eq(0).text();
-          formStepNumber = $(this).closest('.bef-tab-pane').attr('id') === 'bef-step-1' ? 'Step 1':'Step 2';;
+          formStepNumber = $(this).closest('.bef-tab-pane').attr('id') === 'bef-step-1' ? 'Step 1':'Step 2';
         }
         
         switch (target) {
