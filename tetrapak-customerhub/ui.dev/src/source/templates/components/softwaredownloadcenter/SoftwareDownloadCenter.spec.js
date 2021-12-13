2bc974e8d00784184d4952caff3be3c7e56a0efe
sonmitta2 <sonal.mittal1@publicissapient.com>
2021-12-13 17:25:04 +0530

    SMAR-19535 | Software-Download-Center

---
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/softwaredownloadcenter/SoftwareDownloadCenter.spec.js
@@ -0,0 +1,9 @@
+import SoftwareDownloadCenter from './SoftwareDownloadCenter';
+
+describe('SoftwareDownloadCenter', function () {
+  before(function () {
+    this.softwareDownloadCenter = new SoftwareDownloadCenter({
+      el: document.body
+    });
+  });
+});
