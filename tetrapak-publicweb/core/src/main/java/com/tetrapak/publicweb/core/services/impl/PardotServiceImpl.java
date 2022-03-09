406a2c619378e56a110ced76e373431f9f5ef0c1
Nagavekar Bhakti <INNAGAVEKARB@tetrapak.com>
2022-03-09 12:40:55 +0530

    SMAR-26186 code commit to add property eventPublished true when there are no subscriber

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/services/impl/PardotServiceImpl.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/services/impl/PardotServiceImpl.java
@@ -162,11 +162,11 @@ public class PardotServiceImpl implements PardotService {
      * @param locale the locale
      * @param interestAreas the interest areas
      * @return the subscriber mail addresses
+     * @throws JSONException 
      */
-    public List<String> getSubscriberMailAddresses(String locale, List<String> interestAreas) {
+    public List<String> getSubscriberMailAddresses(String locale, List<String> interestAreas) throws JSONException {
         LOGGER.debug("Inside Get SubscriberMailAddresses method");
         List<String> mailAddresses = null;
-        try {
             String apiUrl = config.pardotSubscribersApiURL() + "/subscribers?";
             BearerToken token = getBearerToken();
             String areas = StringUtils.EMPTY;
@@ -187,9 +187,6 @@ public class PardotServiceImpl implements PardotService {
                     mailAddresses = getMailAddresses(jsonObject);
                 }
             }
-        } catch (JSONException e) {
-            LOGGER.error("Error while fetching Subscriber data {}", e.getMessage());
-        }
         return mailAddresses;
     }
 
