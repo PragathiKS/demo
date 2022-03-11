ffff449b9fb2d24c221a8b211137005068ab20db
Ankit Pathak <ankit.pathak2@tetrapak.com>
2022-03-11 14:50:06 +0100

    Added & reorganised PM licenses component dialog.

--- tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/models/SiteLicenseModel.java
+++ tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/models/SiteLicenseModel.java
@@ -20,27 +20,6 @@ import java.util.List;
 public class SiteLicenseModel {
     private static final Logger LOGGER = LoggerFactory.getLogger(SiteLicenseModel.class);
 
-    public static final String TITLE_JSON_KEY = "title";
-    public static final String NAME_OF_SITE_JSON_KEY = "nameOfSite";
-    public static final String LOCATION_OF_SITE_JSON_KEY = "locationOfSite";
-    public static final String APPLICATION_JSON_KEY = "application";
-    public static final String APPLICATION_DROPDOWN_JSON_KEY = "applicationDropDown";
-    public static final String APPLICATION_DROPDOWN_PLACEHOLDER_JSON_KEY = "dropdownPlaceholder";
-    public static final String PLC_TYPE_JSON_KEY = "plcType";
-    public static final String HMI_TYPE_JSON_KEY = "HMIType";
-    public static final String MES_TYPE_JSON_KEY = "mesType";
-    public static final String NUMBER_OF_BASIC_UNIT_JSON_KEY = "numberOfBasicUnit";
-    public static final String BASIC_UNIT_TOOLTIP_HEADING_JSON_KEY = "basicUnitToolTipHeading";
-    public static final String BASIC_UNIT_TOOLTIP_DESCRIPTION_JSON_KEY = "basicUnitToolTipDescription";
-    public static final String NUMBER_OF_ADVANCED_UNIT_JSON_KEY = "numberOfAdvancedUnit";
-    public static final String ADVANCED_UNIT_TOOLTIP_HEADING_JSON_KEY = "advancedUnitToolTipHeading";
-    public static final String ADVANCED_UNIT_TOOLTIP_DESCRIPTION_JSON_KEY = "advancedUnitToolTipDescription";
-    public static final String SUBMIT_BUTTON_LABEL_JSON_KEY = "submitButtonLabel";
-    public static final String SUCCESS_MESSAGE_JSON_KEY = "successMessage";
-    public static final String IMPORTANT_INFORMATION_TITLE_JSON_KEY = "impInformationTitle";
-    public static final String IMPORTANT_INFORMATION_DESCRIPTION_JSON_KEY = "impInformationDescription";
-    public static final String SITE_LICENSE_DESCRIPTION_HEADING_JSON_KEY = "siteLicensesDescriptionHeading";
-
     /** The resource. */
     @SlingObject
     private Resource resource;
@@ -50,103 +29,92 @@ public class SiteLicenseModel {
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(TITLE_JSON_KEY)
     private String title;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(NAME_OF_SITE_JSON_KEY)
     private String nameOfSite;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(LOCATION_OF_SITE_JSON_KEY)
     private String locationOfSite;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(APPLICATION_JSON_KEY)
     private String application;
 
     @Expose(serialize = true)
-    @SerializedName(APPLICATION_DROPDOWN_JSON_KEY)
     @ChildResource
     private List<SiteLicenseApplication> applicationDropdown;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(APPLICATION_DROPDOWN_PLACEHOLDER_JSON_KEY)
     private String applicationDropdownPlaceholder;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(PLC_TYPE_JSON_KEY)
     private String plcType;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(HMI_TYPE_JSON_KEY)
     private String hmiType;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(MES_TYPE_JSON_KEY)
     private String mesType;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(NUMBER_OF_BASIC_UNIT_JSON_KEY)
     private String numberOfBasicUnit;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(BASIC_UNIT_TOOLTIP_HEADING_JSON_KEY)
     private String basicUnitToolTipHeading;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(BASIC_UNIT_TOOLTIP_DESCRIPTION_JSON_KEY)
     private String basicUnitToolTipDescription ;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(NUMBER_OF_ADVANCED_UNIT_JSON_KEY)
     private String numberOfAdvancedUnit;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(ADVANCED_UNIT_TOOLTIP_HEADING_JSON_KEY)
     private String advancedUnitToolTipHeading;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(ADVANCED_UNIT_TOOLTIP_DESCRIPTION_JSON_KEY)
     private String advancedUnitToolTipDescription;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(SUBMIT_BUTTON_LABEL_JSON_KEY)
     private String submitButtonLabel;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(SUCCESS_MESSAGE_JSON_KEY)
     private String successMessage;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(IMPORTANT_INFORMATION_TITLE_JSON_KEY)
     private String impInformationTitle;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(IMPORTANT_INFORMATION_DESCRIPTION_JSON_KEY)
     private String impInformationDescription;
 
     @ValueMapValue
     @Expose(serialize = true)
-    @SerializedName(SITE_LICENSE_DESCRIPTION_HEADING_JSON_KEY)
     private String siteLicensesDescriptionHeading;
+
+    @ValueMapValue
+    @Expose(serialize = true)
+    private String inputFieldError;
+
+    @ValueMapValue
+    @Expose(serialize = true)
+    private String selectFieldError;
+
 
     @ValueMapValue
     private String subject;
@@ -261,5 +229,13 @@ public class SiteLicenseModel {
 
     public String getApplication() {
         return application;
+    }
+
+    public String getInputFieldError() {
+        return inputFieldError;
+    }
+
+    public String getSelectFieldError() {
+        return selectFieldError;
     }
 }
