5ade48de63bb7244717c9e79f8ca162246665b6b
Ovidiu Schintee <s.ovidiu@gmail.com>
2022-07-10 00:11:58 +0300

    SMAR-27019 Add analytics tracking for Site Licenses

--- tetrapak-customerhub/ui.dev/src/source/templates/components/plantmasterlicenses/PlantMasterLicenses-site.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/plantmasterlicenses/PlantMasterLicenses-site.js
@@ -4,7 +4,11 @@ import {ajaxWrapper} from '../../../scripts/utils/ajax';
 import {REG_NUM, ajaxMethods} from '../../../scripts/utils/constants';
 import {logger} from '../../../scripts/utils/logger';
 import {render} from '../../../scripts/utils/render';
-
+import {
+  _trackFormComplete,
+  _trackFormError,
+  _trackFormStart
+} from './PlantMasterLicenses-site.analytics';
 
 function _renderSiteLicensesData() {
   render.fn({
@@ -74,6 +78,7 @@ class PlantMasterLicensesSite {
     this.cache.submitApi = aipLicenseObj.data('submit-api');
     const configJson = aipLicenseObj.find('.js-aip-licenses__config').text();
     this.cache.siteLicenseUerGroup = this.siteLicenseUerGroup;
+    this.cache.formError = [];
     try {
       this.cache.i18nKeys = JSON.parse(configJson);
     } catch (e) {
@@ -108,11 +113,21 @@ class PlantMasterLicensesSite {
   }
 
   addErrorMsg(ele, errorMsgSelector) {
-    $(ele)
+    const $el = $(ele);
+    const $this = this;
+    const formErrorMessage = $el.closest('.js-tp-aip-licenses__form-element').find(errorMsgSelector).text();
+    const formErrorField = $el.attr('id').split('-')[0];
+
+    $el
       .closest('.js-tp-aip-licenses__form-element')
       .addClass('tp-aip-licenses__form-element--error')
       .find(errorMsgSelector)
       .addClass('error-msg--active');
+
+    $this.cache.formError.push({
+      formErrorMessage,
+      formErrorField
+    });
   }
 
   removeAllErrorMessages() {
@@ -120,6 +135,7 @@ class PlantMasterLicensesSite {
     this.root
       .find('.tp-aip-licenses__form-element--error')
       .removeClass('tp-aip-licenses__form-element--error');
+    this.cache.formError = [];
   }
 
   submitRequestForm = (e) => {
@@ -166,15 +182,38 @@ class PlantMasterLicensesSite {
           showLoader: true
         }).done(() => {
           this.renderSuccessMessage();
+          this.trackFormComplete(object);
         }).fail(() => {
           this.showContent();
         });
       });
+    } else {
+      const { formError } = this.cache;
+      this.trackFormError(formError);
     }
   }
 
   bindEvents() {
     this.root.on('click', '.js-tp-aip-licenses-site__btn', this.submitRequestForm);
+
+    // track Form Start analytics
+    this.root.on('input', '.tpatom-input-box__input', e => {
+      const $formWrapper = $(e.currentTarget).parents('form');
+
+      if (!$formWrapper.data('form-touched')) {
+        $formWrapper.attr('data-form-touched', true);
+        this.trackFormStart();
+      }
+    });
+
+    this.root.on('change', '.tpatom-dropdown__select-box', e => {
+      const $formWrapper = $(e.currentTarget).parents('form');
+
+      if (!$formWrapper.data('form-touched')) {
+        $formWrapper.attr('data-form-touched', true);
+        this.trackFormStart();
+      }
+    });
   }
 
   renderSiteLicensesData() {
@@ -183,6 +222,18 @@ class PlantMasterLicensesSite {
 
   getSiteLicensesData() {
     return _getSiteLicensesData.apply(this, arguments);
+  }
+
+  trackFormComplete() {
+    return _trackFormComplete.apply(this, arguments);
+  }
+
+  trackFormError() {
+    return _trackFormError.apply(this, arguments);
+  }
+
+  trackFormStart() {
+    return _trackFormStart.apply(this, arguments);
   }
 
   showTooltip(){
