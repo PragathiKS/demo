4a609362a79ab360f38077036ae874fd50147e0d
Kumar Rajeev <kumrajee@publicisgroupe.net>
2022-07-28 18:48:52 +0530

    Added nvmbuild.sh

--- tetra-laval/nvmbuild.sh
+++ tetra-laval/nvmbuild.sh
@@ -6,4 +6,5 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install 10.16.0
+nvm use 10.16.0
 npm rebuild node-sass
