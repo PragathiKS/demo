4479a8916e4da1a71a7c4b11e30a193f932e5888
Pawan Kumar <pawan.kumar@tetrapak.com>
2022-08-29 20:54:15 +0530

    Revert "SMAR-28371 not needed commit"
    This reverts commit 0b5fddafd8485c1fc74ceaa50ffbf6e7c72a7d2e.

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/BannerModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/BannerModel.java
@@ -1,20 +1,9 @@
 package com.tetrapak.publicweb.core.models;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Objects;
-import java.util.UUID;
-import com.auth0.jwt.algorithms.Algorithm;
 
 import javax.annotation.PostConstruct;
 
-import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
@@ -25,15 +14,6 @@ import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import com.tetrapak.publicweb.core.utils.LinkUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import java.security.interfaces.RSAPrivateKey;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.JwtBuilder;
-import io.jsonwebtoken.Jwts;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.DataInputStream;
 
 /**
  * The Class BannerModel.
@@ -131,79 +111,16 @@ public class BannerModel {
     /** The enable softcoversion. */
     @ValueMapValue
     private String enableSoftcoversion;
-
-    /** The Constant LOGGER. */
-    private static final Logger LOGGER = LoggerFactory.getLogger(AnchorModel.class);
-
+    
     /**
      * The init method.
      */
     @PostConstruct
-    protected void init() throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+    protected void init() {
         resource = request.getResource();
         final ValueMap vMap = resource.getValueMap();
         enableSoftcoversion = vMap.get("enableSoftcoversion", StringUtils.EMPTY);
         formType = vMap.get("formType", StringUtils.EMPTY);
-        UUID uuid = UUID.randomUUID();
-        int variant = uuid.variant();
-        int version = uuid.version();
-        LOGGER.error("checking guid variant",variant);
-        LOGGER.error("chcking guid version",version);
-        Resource resource1 = resource.getResourceResolver().getResource("/etc/oneTrustkey/TetraPakCookieMgmtPrivateKey.pem");
-       // File file  = resource1.adaptTo(File.class);
-        File file1 = new File(resource1.getPath());
-       // File file1 = new File(resource1.getName());
-        RSAPrivateKey privateKey = (RSAPrivateKey) getPrivateKey("D:\\private_key.pem");
-        String jwtToken = createJWTAndSign("SneppetsMobileApp", "admin@sneppets.com", "sneppets.com", "sneppets_device_100", "2.1.3", "ios", "student", "SneppetsMobileApp", "1234", privateKey);
-
-    }
-
-    private static String createJWTAndSign(String issuer, String subject, String server, String deviceid, String appversion,
-                                           String os, String userType, String clientid, String pin, RSAPrivateKey privateKey) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-
-
-        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.RS256;
-
-        JwtBuilder builder = Jwts.builder().claim("issuer", issuer)
-                .claim("subject", subject)
-                .claim("server", server)
-                .claim("device_id", deviceid)
-                .claim("app_version", appversion)
-                .claim("os", os)
-                .claim("user_type", userType)
-                .claim("client_id", clientid)
-                .claim("pin", pin)
-                .signWith(privateKey,signatureAlgorithm);
-
-        return builder.compact();
-
-    }
-
-    private static RSAPrivateKey getPrivateKey(String filename) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
-
-        File file = new File(filename);
-        FileInputStream fis = new FileInputStream(file);
-        DataInputStream dis = new DataInputStream(fis);
-
-        byte[] keyBytes = new byte[(int) file.length()];
-        dis.readFully(keyBytes);
-        dis.close();
-        String key = new String(Files.readAllBytes(file.toPath()), Charset.defaultCharset());
-        String privateKeyPEM = key
-                .replace("-----BEGIN PRIVATE KEY-----", "")
-                .replaceAll(System.lineSeparator(), "")
-                .replace("-----BEGIN PRIVATE KEY-----", "");
-        byte[] encoded = Base64.decodeBase64(privateKeyPEM);
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encoded);
-        return (RSAPrivateKey) keyFactory.generatePrivate(keySpec);
-
-
-       // PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(keyBytes);
-      //  KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-      //  RSAPrivateKey privateKey = (RSAPrivateKey) keyFactory.generatePrivate(spec);
-
-       // return privateKey;
     }
 
     /**
