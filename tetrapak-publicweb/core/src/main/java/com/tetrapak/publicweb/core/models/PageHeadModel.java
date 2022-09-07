69fa2c68744572a83fd2ddc03f4b00a17f57c6db
Pawan Kumar <pawan.kumar@tetrapak.com>
2022-09-08 01:07:38 +0530

    SMAR-28371 fixing coverage and providing servlet path parameter

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/PageHeadModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/PageHeadModel.java
@@ -38,7 +38,7 @@ public class PageHeadModel {
     @PostConstruct
     public void initModel() {
         final String path = request.getResource().getPath();
-        cookieTokenServletUrl = path.concat(".pardotsubscription.json");
+        cookieTokenServletUrl = path.concat(".onetrustcookietoken.json");
         if(baiduMapService.getBaiduMapKey() != null && path.contains("/cn")) {
             baiduMapkey = baiduMapService.getBaiduMapKey();
         }
@@ -53,6 +53,10 @@ public class PageHeadModel {
         return baiduMapkey;
     }
 
+    /**
+     * Get the One Trust Cookie Token Servlet url
+     * @return cookieTokenServleturl
+     */
     public String getCookieTokenServletUrl() {
         return cookieTokenServletUrl;
     }
