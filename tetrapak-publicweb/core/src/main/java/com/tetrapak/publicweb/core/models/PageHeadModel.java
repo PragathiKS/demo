3fc80da286d28445ebff6d6aa54ad4891fa04d6e
Pawan Kumar <pawan.kumar@tetrapak.com>
2022-09-07 17:55:56 +0530

    SMAR-28371

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/PageHeadModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/PageHeadModel.java
@@ -2,6 +2,7 @@ package com.tetrapak.publicweb.core.models;
 
 import javax.annotation.PostConstruct;
 
+import com.tetrapak.publicweb.core.constants.PWConstants;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
@@ -29,12 +30,16 @@ public class PageHeadModel {
     /** The Baidu Map Key. */
     private String baiduMapkey;
 
+    /** The One Trust Servlet call api. */
+    private String cookieTokenServletUrl;
+
     /**
      * Inits the model.
      */
     @PostConstruct
     public void initModel() {
         final String path = request.getResource().getPath();
+        cookieTokenServletUrl = path.concat(".pardotsubscription.json");
         if(baiduMapService.getBaiduMapKey() != null && path.contains("/cn")) {
             baiduMapkey = baiduMapService.getBaiduMapKey();
         }
@@ -47,5 +52,9 @@ public class PageHeadModel {
      */
     public String getBaiduMapkey() {
         return baiduMapkey;
+    }
+
+    public String getCookieTokenServletUrl() {
+        return cookieTokenServletUrl;
     }
 }
