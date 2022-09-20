481f26d032dc86fb9fdd8c4840150fe93522260b
Pawan Kumar <pawan.kumar@tetrapak.com>
2022-09-20 14:45:52 +0530

    SMAR-28371 handling preset cookie

--- tetrapak-commons/core/src/main/java/com/tetrapak/commons/core/constants/CommonsConstants.java
+++ tetrapak-commons/core/src/main/java/com/tetrapak/commons/core/constants/CommonsConstants.java
@@ -3,26 +3,6 @@ package com.tetrapak.commons.core.constants;
 /**
  * The Class v.
  */
-public final class CommonsConstants {
-
-    /** The Constant USER_ID. */
-    public static final String USER_ID = "UserID";
-
-    /** The Constant APPLICATION_JSON. */
-    public static final String APPLICATION_JSON = "application/json";
-
-    /** Constant AEM_CONTENT_PATH */
-    public static final String AEM_CONTENT_PATH = "/content/";
-
-    /** The Constant HTML. */
-    public static final String HTML = ".html";
-
-    /**
-     * Instantiates a new Commons constants.
-     */
-    private CommonsConstants() {
-        /*
-         * adding a private constructor to hide the implicit one
-         */
-    }
+public class CommonsConstants {
+    
 }
