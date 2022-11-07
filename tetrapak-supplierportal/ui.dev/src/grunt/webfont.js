84d171283d54071a024faf9ce71f26b7f0c792b5
Piotr Winiarski <piowinia@WKMZT565EDA6.global.publicisgroupe.net>
2022-11-07 16:34:40 +0100

    feat: SP-2 update frontend structure to align with customerhub/publicweb

--- tetrapak-supplierportal/ui.dev/src/grunt/webfont.js
+++ tetrapak-supplierportal/ui.dev/src/grunt/webfont.js
@@ -1,5 +1,4 @@
 var config = require("../config.json").webfont;
-var pw_config = require("../config.json").pw_webfont;
 module.exports = {
   common_icons: {
     src: config.src,
@@ -10,17 +9,6 @@ module.exports = {
       syntax: "bootstrap",
       stylesheets: ["scss"],
       relativeFontPath: config.relativePath
-    }
-  },
-  pw_icons: {
-    src: pw_config.src,
-    dest: pw_config.target,
-    destScss: pw_config.scss.target,
-    options: {
-      font: pw_config.fontFile,
-      syntax: "bootstrap",
-      stylesheets: ["scss"],
-      relativeFontPath: pw_config.relativePath
     }
   }
 };
