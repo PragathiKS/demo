9cdddabc0d5240bf154170f846688286e759d82e
vishalinpublicis <vishal.singh3@publicissapient.com>
2022-11-18 16:59:04 +0530

    incremental changes like unused test case remove along with js code

--- tetrapak-supplierportal/ui.dev/src/source/templates/components/footer/footer.spec.js
+++ tetrapak-supplierportal/ui.dev/src/source/templates/components/footer/footer.spec.js
@@ -9,30 +9,14 @@ describe('Footer', function () {
       el: document.body
     });
     this.initSpy = sinon.spy(this.footer, 'init');
-    this.trackAnalyticsSpy = sinon.spy(this.footer, 'trackAnalytics');
-    this.openStub = sinon.stub(window, 'open');
-    this.isExternalStub = sinon.stub(this.footer, 'isExternal');
-    this.isExternalStub.returns(true);
-    window.digitalData = {};
-    window._satellite = {
-      track() { /* Dummy method */ }
-    };
     this.footer.init();
   });
   after(function () {
     $(document.body).empty();
     this.initSpy.restore();
-    this.trackAnalyticsSpy.restore();
-    this.openStub.restore();
-    this.isExternalStub.restore();
   });
   it('should initialize', function (done) {
     expect(this.footer.init.called).to.be.true;
-    done();
-  });
-  it('should call track analytics on click', function (done) {
-    $('.tp-pw-footer-data-analytics').trigger('click');
-    expect(this.footer.trackAnalytics.called).to.be.true;
     done();
   });
 });
