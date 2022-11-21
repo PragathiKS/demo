5ae50fa87b861bb609f763c743503de9847e7263
Kostiantyn Seledkov <SESELEDKOVK@tetrapak.com>
2022-11-22 00:05:15 +0100

    SP-98: add header.

---
+++ tetrapak-supplierportal/core/src/main/java/com/tetrapak/supplierportal/core/constants/SupplierPortalConstants.java
@@ -0,0 +1,59 @@
+package com.tetrapak.supplierportal.core.constants;
+
+/**
+ * The SupplierPortalConstants class.
+ */
+public final class SupplierPortalConstants {
+
+    /**
+     * The Constant HTML_EXTENSION.
+     */
+    public static final String HTML_EXTENSION = ".html";
+
+    /**
+     * The Constant CONTENT_PATH .
+     */
+    public static final String CONTENT_PATH = "/content/";
+
+    /**
+     * The Constant HTTP.
+     */
+    public static final String HTTP = "http";
+
+    /**
+     * The Constant WWW.
+     */
+    public static final String WWW = "www";
+
+    /**
+     * The Constant CONTENT_DAM_PATH.
+     */
+    public static final String CONTENT_DAM_PATH = "/content/dam/";
+
+    /**
+     * The Constant DOWNLOAD_LINK.
+     */
+    public static final String DOWNLOAD_LINK = "download";
+
+    /**
+     * The Constant DOWNLOAD_LINK.
+     */
+    public static final String EXTERNAL_DOWNLOAD_LINK = "externalDownload";
+
+    /**
+     * The Constant INTERNAL_LINK.
+     */
+    public static final String INTERNAL_LINK = "internal";
+
+    /**
+     * The Constant EXTERNAL_LINK.
+     */
+    public static final String EXTERNAL_LINK = "external";
+
+    /**
+     * Instantiates a new supplier portal constants.
+     */
+    private SupplierPortalConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+}
--- tetrapak-supplierportal/core/src/main/java/com/tetrapak/supplierportal/core/models/HeaderModel.java
+++ tetrapak-supplierportal/core/src/main/java/com/tetrapak/supplierportal/core/models/HeaderModel.java
@@ -1,14 +1,92 @@
 package com.tetrapak.supplierportal.core.models;
 
+import com.day.cq.wcm.api.Page;
+import com.tetrapak.supplierportal.core.beans.HeaderBean;
+import com.tetrapak.supplierportal.core.utils.LinkUtil;
+import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class HeaderModel {
 
+    private static final String LOGO_TEXT_I18N_KEY = "supo.logotext.text";
+    private static final String USER_INFO_I18N = "Charlie Svensson";
+
+    @SlingObject private SlingHttpServletRequest request;
+
+    @Inject
+    private Page currentPage;
+
+    private String logoUrl;
+
+    private String mLogoLink;
+
+    private String dLogoLink;
+
+    private List<HeaderBean> headerNavLinks = new ArrayList<>();
+
     @PostConstruct protected void init() {
+
+        Resource headerConfigurationResource = request.getResourceResolver()
+                .getResource(currentPage.getPath() + "/jcr:content/root/responsivegrid/headerconfiguration");
+        if (null != headerConfigurationResource) {
+            ValueMap map = headerConfigurationResource.getValueMap();
+            logoUrl = (String) map.get("logoUrl");
+            mLogoLink = LinkUtil.getValidLink(request.getResource(), (String) map.get("mLogoLink"));
+            dLogoLink = LinkUtil.getValidLink(request.getResource(), (String) map.get("dLogoLink"));
+            Resource links = headerConfigurationResource.getChild("headerNavLinks");
+            if (null == links) {
+                return;
+            }
+            Iterator<Resource> itr = links.listChildren();
+            while (itr.hasNext()) {
+                Resource res = itr.next();
+                ValueMap valueMap = res.getValueMap();
+                HeaderBean headerBean = new HeaderBean();
+                headerBean.setHref(LinkUtil.getValidLink(request.getResource(), (String) valueMap.get("href")));
+                headerBean.setName((String) valueMap.get("name"));
+                headerBean.setTargetNew(StringUtils.equalsIgnoreCase("true", (String) valueMap.get("targetNew")));
+                headerNavLinks.add(headerBean);
+            }
+        }
+    }
+
+    public String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public String getMLogoLink() {
+        return mLogoLink;
+    }
+
+    public String getDLogoLink() {
+        return dLogoLink;
+    }
+
+    public List<HeaderBean> getHeaderNavLinks() {
+        return headerNavLinks;
+    }
+
+    public String getLogoTextI18n() {
+        return LOGO_TEXT_I18N_KEY;
+    }
+
+    public String getGetUserInfoI18n() {
+        return USER_INFO_I18N;
+    }
+
+    public boolean getRightNavDisplay() {
+        return true;
     }
 }
