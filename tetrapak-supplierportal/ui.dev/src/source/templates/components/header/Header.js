de338a0064158088a3b300f86a1ee5d5148a0f36
Piotr Winiarski <piowinia@WKMZT565EDA6.global.publicisgroupe.net>
2022-11-22 12:22:21 +0100

    feat:SP-25 update header ui

--- tetrapak-supplierportal/ui.dev/src/source/templates/components/header/Header.js
+++ tetrapak-supplierportal/ui.dev/src/source/templates/components/header/Header.js
@@ -4,6 +4,16 @@ class Header {
   constructor({ el }) {
     this.root = $(el);
   }
+
+  bindEvents() {
+    this.root.find('.tp-header__burger-menu').on('click', () => {
+      document.body.trigger('showleftnav');
+    });
+  }
+
+  init() {
+    this.bindEvents();
+  }
 }
 
 export default Header;
