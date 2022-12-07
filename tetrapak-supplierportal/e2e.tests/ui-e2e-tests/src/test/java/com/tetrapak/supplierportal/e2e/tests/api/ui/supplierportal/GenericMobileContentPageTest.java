429261ad43f1aeb1b203944814cd29b236984f62
Filip Cynarski <filip@cynarski.eu>
2022-12-07 16:52:57 +0100

    SP-171 invalid login case added mobile test cases added

--- tetrapak-supplierportal/e2e.tests/ui-e2e-tests/src/test/java/com/tetrapak/supplierportal/e2e/tests/api/ui/supplierportal/GenericMobileContentPageTest.java
+++ tetrapak-supplierportal/e2e.tests/ui-e2e-tests/src/test/java/com/tetrapak/supplierportal/e2e/tests/api/ui/supplierportal/GenericMobileContentPageTest.java
@@ -3,9 +3,12 @@ package com.tetrapak.supplierportal.e2e.tests.api.ui.supplierportal;
 import com.tetrapak.supplierportal.e2e.tests.api.ui.engine.AbstractMobilePlaywrightE2EConfig;
 import org.junit.jupiter.api.Test;
 
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
 public class GenericMobileContentPageTest extends AbstractMobilePlaywrightE2EConfig {
     @Test
     void genericContentPageTest() {
-        new GenericMobileContentPage(page).goTo();
+        GenericContentPage genericMobileContentPage = new GenericMobileContentPage(page).goTo();
+        assertThat(genericMobileContentPage.getMobileLogo()).isVisible();
     }
 }
