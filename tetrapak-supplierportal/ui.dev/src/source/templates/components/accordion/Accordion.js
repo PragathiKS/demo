ecc3d40c7e9de6f9039c21af4c9a2b5d26e7ddb5
vishalinpublicis <vishal.singh3@publicissapient.com>
2022-12-15 12:29:44 +0530

    basic functionality added for accordion

--- tetrapak-supplierportal/ui.dev/src/source/templates/components/accordion/Accordion.js
+++ tetrapak-supplierportal/ui.dev/src/source/templates/components/accordion/Accordion.js
@@ -1,4 +1,12 @@
 import $ from 'jquery';
+import 'bootstrap';
+import { logger } from '../../../scripts/utils/logger';
+
+function _renderFirstTab() {
+  const { componentId } = this.cache;
+  const $tabSection = this.root.find('.js-accordion__events-sidesection');
+  $tabSection.html($(`#tab_${componentId}_0`).html());
+}
 
 class Accordion {
   constructor({ el }) {
@@ -6,25 +14,37 @@ class Accordion {
   }
   cache = {};
   initCache() {
-    /* Initialize selector cache here */
-    /**
-     * Use "this.root" to find elements within current component
-     * Example:
-     * this.cache.$submitBtn = this.root.find('.js-submit-btn');
-     */
+    this.cache.selectedTabIndex = 0;
+    this.cache.componentId = this.root.find('#tabComponentId').val();
+    this.cache.heading = $.trim(this.root.find('.js-accordion__heading').text());
   }
   bindEvents() {
-    /* Bind jQuery events here */
-    /**
-     * Example:
-     * const { $submitBtn } = this.cache;
-     * $submitBtn.on('click', () => { ... });
-     */
+    const $this = this;
+    logger.log('bind evnt');
+
+    this.root
+      .on('click', '.js-accordion__event', function () {
+        logger.log('clicked');
+        const self = $(this);
+        if (!self.hasClass('active')) {
+          $this.showTabDetail(self.data('target'));
+        }
+        $this.root.find('.js-accordion__event').removeClass('active');
+        self.addClass('active').toggleClass('m-active');
+      })
+
+      .on('hidden.bs.collapse', '.collapse', this.pauseVideoIfExists);
   }
+  showTabDetail = (el) => {
+    const $tabSection = this.root.find('.js-accordion__events-sidesection');
+    $tabSection.html($(el).html());
+  }
+  renderFirstTab = () => _renderFirstTab.call(this);
+
   init() {
-    /* Mandatory method */
     this.initCache();
     this.bindEvents();
+    this.renderFirstTab();
   }
 }
 
--- tetrapak-supplierportal/ui.dev/src/source/templates/components/accordion/Accordion.spec.js
+++ tetrapak-supplierportal/ui.dev/src/source/templates/components/accordion/Accordion.spec.js
@@ -1,9 +1,22 @@
+import $ from 'jquery';
 import Accordion from './Accordion';
+import accordionTemplate from '../../../test-templates-hbs/accordion.hbs';
 
 describe('Accordion', function () {
   before(function () {
+    $(document.body).empty().html(accordionTemplate());
     this.accordion = new Accordion({
       el: document.body
     });
+    this.initSpy = sinon.spy(this.accordion, 'init');
+    this.accordion.init();
+  });
+  after(function () {
+    $(document.body).empty();
+    this.initSpy.restore();
+  });
+  it('should initialize', function (done) {
+    expect(this.accordion.init.called).to.be.true;
+    done();
   });
 });
