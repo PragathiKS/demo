314df7984fc40902ee4b6389a6b4edac97008796
Surbhi Gupta <surbhi.gupta1@publicissapient.com>
2022-12-16 12:53:08 +0530

    committing review comments

--- tetrapak-supplierportal/core/src/main/java/com/tetrapak/supplierportal/core/models/AccordionModel.java
+++ tetrapak-supplierportal/core/src/main/java/com/tetrapak/supplierportal/core/models/AccordionModel.java
@@ -25,7 +25,7 @@ public class AccordionModel {
 	private String heading;
 
 	@Inject
-	private List<FAQModel> questionlist;
+	private List<FAQModel> questionList;
 
 	public String getHeading() {
 		return heading;
@@ -33,8 +33,8 @@ public class AccordionModel {
 
 	public List<FAQModel> getQuestionList() {
 		final List<FAQModel> lists = new ArrayList<>();
-		if (Objects.nonNull(questionlist)) {
-			lists.addAll(questionlist);
+		if (Objects.nonNull(questionList)) {
+			lists.addAll(questionList);
 		}
 		return lists;
 
