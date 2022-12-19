40d1160876260ad5f5a6838af7dc2012676c6855
Hiren Parmar <hiren.parmar@digitas.com>
2022-12-20 01:17:56 +0530

    SMAR-30197 - My tetrapak - Footer component - Implementation - Done

--- tetrapak-customerhub/ui.dev/src/source/templates/components/footer/Footer.spec.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/footer/Footer.spec.js
@@ -1,9 +1,24 @@
+import $ from 'jquery';
 import Footer from './Footer';
+import footerTemplate from '../../../test-templates-hbs/footer.hbs';
 
 describe('Footer', function () {
   before(function () {
+    $(document.body).empty().html(footerTemplate());
     this.footer = new Footer({
       el: document.body
     });
+    this.initSpy = sinon.spy(this.footer, 'init');
+    this.footer.init();
+  });
+  after(function () {
+    $(document.body).empty();
+    this.initSpy.restore();
+  });
+  it('should trigger "showlanuagepreferencepopup" event on click of change language', function (done) {
+    $('.js-footer__change-lang').on('click', () => {
+      done();
+    });
+    $('.js-footer__change-lang').trigger('click');
   });
 });
