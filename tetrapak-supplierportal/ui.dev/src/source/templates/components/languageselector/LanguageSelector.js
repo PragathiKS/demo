f2c2c496996a421b5d673c3e6433f1456d0efdd1
vishalinpublicis <vishal.singh3@publicissapient.com>
2022-12-22 16:26:18 +0530

    basic structure added

---
+++ tetrapak-supplierportal/ui.dev/src/source/templates/components/languageselector/LanguageSelector.js
@@ -0,0 +1,5 @@
+import LanguageSelectorParent from 'tpCustomerhub/templates/components/languageselector/LanguageSelector';
+
+class LanguageSelector extends LanguageSelectorParent {}
+
+export default LanguageSelector;
