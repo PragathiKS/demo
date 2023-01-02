97697e55ab02891f87a4dd982cc45bafb1f60995
Surbhi Gupta <surbhi.gupta1@publicissapient.com>
2023-01-02 15:46:24 +0530

    Change Language text on footer

--- tetrapak-supplierportal/core/src/main/java/com/tetrapak/supplierportal/core/models/FooterModel.java
+++ tetrapak-supplierportal/core/src/main/java/com/tetrapak/supplierportal/core/models/FooterModel.java
@@ -6,14 +6,10 @@ import java.util.Objects;
 
 import javax.annotation.PostConstruct;
 
-import com.tetrapak.supplierportal.core.services.UserPreferenceService;
-import com.tetrapak.supplierportal.core.utils.GlobalUtil;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,48 +21,51 @@ import com.tetrapak.supplierportal.core.utils.LinkUtil;
 @Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class FooterModel {
 
-    /** The Constant LOGGER. */
-    private static final Logger LOGGER = LoggerFactory.getLogger(FooterModel.class);
+	/** The Constant LOGGER. */
+	private static final Logger LOGGER = LoggerFactory.getLogger(FooterModel.class);
 
-    private static final String CONFIGURATION_PATH = "/jcr:content/root/responsivegrid/footerconfiguration";
+	private static final String CONFIGURATION_PATH = "/jcr:content/root/responsivegrid/footerconfiguration";
 
-    /** The request. */
-    @SlingObject
-    private SlingHttpServletRequest request;
+	/** The request. */
+	@SlingObject
+	private SlingHttpServletRequest request;
 
-    @OSGiService
-    private UserPreferenceService userPreferenceService;
+	/** The footer links. */
+	private List<FooterLinkModel> footerValidLinks = new ArrayList<>();
+	
+	private String changeLanguage;
 
-    /** The footer links. */
-    private List<FooterLinkModel> footerValidLinks = new ArrayList<>();
+	/**
+	 * Inits the.
+	 */
+	@PostConstruct
+	protected void init() {
+		final List<FooterLinkModel> footerLinks;
+		LOGGER.debug("inside init method");
 
-    /**
-     * Inits the.
-     */
-    @PostConstruct
-    protected void init() {
-        final List<FooterLinkModel> footerLinks;
-        LOGGER.debug("inside init method");
+		final String path = SupplierPortalConstants.CONTENT_ROOT + CONFIGURATION_PATH;
+		final Resource footerConfigurationResource = request.getResourceResolver().getResource(path);
+		if (Objects.nonNull(footerConfigurationResource)) {
+			final FooterConfigurationModel configurationModel = footerConfigurationResource
+					.adaptTo(FooterConfigurationModel.class);
+			if (Objects.nonNull(configurationModel)) {
+				changeLanguage = configurationModel.getChangeLanguage();
+				footerLinks = configurationModel.getFooterLinks();
+				for (FooterLinkModel footerLink : footerLinks) {
+					String validLink = LinkUtil.getValidLink(request.getResource(), footerLink.getLinkPath());
+					footerLink.setLinkPath(validLink);
+					footerValidLinks.add(footerLink);
+				}
 
-        String language = GlobalUtil.getSelectedLanguage(request, userPreferenceService);
-        final String path = SupplierPortalConstants.SUPPLIER_PATH + language + CONFIGURATION_PATH;
-        final Resource footerConfigurationResource = request.getResourceResolver().getResource(path);
-        if (Objects.nonNull(footerConfigurationResource)) {
-            final FooterConfigurationModel configurationModel = footerConfigurationResource.adaptTo(
-                    FooterConfigurationModel.class);
-            if (Objects.nonNull(configurationModel)) {
-                footerLinks = configurationModel.getFooterLinks();
-                for (FooterLinkModel footerLink : footerLinks) {
-                    String validLink = LinkUtil.getValidLink(request.getResource(), footerLink.getLinkPath());
-                    footerLink.setLinkPath(validLink);
-                    footerValidLinks.add(footerLink);
-                }
+			}
+		}
+	}
 
-            }
-        }
-    }
-
-    public List<FooterLinkModel> getFooterLinks() {
-        return new ArrayList<>(footerValidLinks);
+	public List<FooterLinkModel> getFooterLinks() {
+		return new ArrayList<>(footerValidLinks);
+	}
+	
+	public String getChangeLanguage() {
+        return changeLanguage;
     }
 }
