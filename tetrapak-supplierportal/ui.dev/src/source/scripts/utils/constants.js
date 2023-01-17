1e6b5dadaa8d315db691b521af533c35fc4aacb6
Piotr Winiarski <piowinia@WKMZT565EDA6.global.publicisgroupe.net>
2023-01-17 09:14:29 +0100

    feat: SP-173 setup contact support request form

--- tetrapak-supplierportal/ui.dev/src/source/scripts/utils/constants.js
+++ tetrapak-supplierportal/ui.dev/src/source/scripts/utils/constants.js
@@ -10,3 +10,6 @@ export const orientationTypes = {
   UNSUPPORTED: 'unsupported'
 };
 export const TRANSITION_END = 'webkitTransitionEnd mozTransitionEnd oTransitionEnd transitionend';
+// Regular expressions
+export const REG_EMAIL = /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i;
+export const REG_NUM = /^\d+$/;
