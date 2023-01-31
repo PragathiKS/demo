e4213823dc048509787dd8a76a4a63fd2a87672c
Piotr Winiarski <piowinia@WKMZT565EDA6.global.publicisgroupe.net>
2023-01-31 12:09:31 +0100

    feat: SP-173 fix ajax call, some cleanup and fix files render styles

--- tetrapak-supplierportal/ui.dev/src/source/scripts/utils/constants.js
+++ tetrapak-supplierportal/ui.dev/src/source/scripts/utils/constants.js
@@ -13,3 +13,9 @@ export const TRANSITION_END = 'webkitTransitionEnd mozTransitionEnd oTransitionE
 // Regular expressions
 export const REG_EMAIL = /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i;
 export const REG_NUM = /^\d+$/;
+export const ajaxMethods = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE'
+};
