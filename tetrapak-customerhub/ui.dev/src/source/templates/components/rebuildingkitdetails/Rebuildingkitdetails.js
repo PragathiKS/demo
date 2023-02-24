721a02fe5269e0c417f1978647e604a881ef538b
Piotr Winiarski <piowinia@WKMZT565EDA6.global.publicisgroupe.net>
2023-02-24 14:03:52 +0100

    feat: SMAR-28546 setup ReportImplementationStatus Modal and button to trigger it

--- tetrapak-customerhub/ui.dev/src/source/templates/components/rebuildingkitdetails/Rebuildingkitdetails.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/rebuildingkitdetails/Rebuildingkitdetails.js
@@ -35,6 +35,9 @@ function _renderRebuildingKitDetails({ isNotConfirmed }) {
   $('.js-rebuilding-details__update').on('click', function() {
     $this.renderRebuildingKitReportModal();
   });
+  $('.js-rebuilding-details__update').on('click', function() {
+    $this.renderRebuildingKitReportModal();
+  });
 }
 
 function _renderCtiDocuments(langAvailable, otherLang, reqOtherLang) {
@@ -157,6 +160,9 @@ function  _renderRebuildingKitReportModal() {
         showLoader: true
       })
       .done((res) => {
+        $this.cache.$rebuildingData = res.data;
+
+        logger.log('i18nKeys: ', i18nKeys);
         $this.cache.$contentWrapper.removeClass('d-none');
         $this.cache.$spinner.addClass('d-none');
         render.fn({
