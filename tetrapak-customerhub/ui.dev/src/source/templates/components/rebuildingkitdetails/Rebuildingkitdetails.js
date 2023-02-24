a752bbd9fe37cab2ed7b88a25929c61f731ae6d7
Piotr Winiarski <piowinia@WKMZT565EDA6.global.publicisgroupe.net>
2023-02-24 14:03:52 +0100

    feat: SMAR-28546 setup ReportImplementationStatus Modal and button to trigger it

--- tetrapak-customerhub/ui.dev/src/source/templates/components/rebuildingkitdetails/Rebuildingkitdetails.js
+++ tetrapak-customerhub/ui.dev/src/source/templates/components/rebuildingkitdetails/Rebuildingkitdetails.js
@@ -35,6 +35,9 @@ function _renderRebuildingKitDetails({ isNotConfirmed }) {
   $('.js-rebuilding-details__update').on('click', function() {
     $this.renderRebuildingKitReportModal();
   });
+  $('.js-rebuilding-details__update').on('click', function() {
+    $this.renderRebuildingKitReportModal();
+  });
 }
 
 function _renderCtiDocuments(langAvailable, otherLang, reqOtherLang) {
