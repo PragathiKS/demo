8081a5d98994aee1c90177e7d6585ad4d9cee2b3
Hiren Parmar <hiren.parmar@digitas.com>
2023-03-01 01:33:53 +0530

    youtube cookie

--- tetrapak-publicweb/ui.dev/src/source/templates/components/textVideo/TextVideo.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/textVideo/TextVideo.js
@@ -1,6 +1,6 @@
 
 import $ from 'jquery';
-import { render } from '../../../scripts/utils/render';
+// import { render } from '../../../scripts/utils/render';
 import { getLinkClickAnalytics,addLinkAttr } from '../../../scripts/common/common';
 class TextVideo {
   constructor({ el }) {
@@ -12,24 +12,6 @@ class TextVideo {
   initCache() {
     this.cache.$textVideoButton = this.root.find('.js-textVideo-analytics');
     this.cache.componentName = this.root.find('.componentName-textvideo').val();
-  }
-
-  toggleVideoContent(noVideo) {
-    if(noVideo === ',1,' || noVideo === ',1,2,') {
-      $('.pw-text-video__video').css('display', 'none');
-      if(!$('.pw-text-video_content').children().length) {
-        render.fn({
-          template: 'noVideoContent',
-          data: {  },
-          target: '.pw-text-video_content',
-          hidden: false
-        });
-      }
-      $('.pw-text-video__novideo').css('display', 'block');
-    } else {
-      $('.pw-text-video__video').css('display', 'block');
-      $('.pw-text-video__novideo').css('display', 'none');
-    }
   }
 
   bindEvents() {
@@ -45,6 +27,7 @@ class TextVideo {
       getLinkClickAnalytics(e,'video-title','Text & Video','.js-subscription-pw-textvideo', false);
       $('body').find('.'+this.cache.componentName).trigger('showSubscription-pw');
     });
+<<<<<<< HEAD
 
     setTimeout(() => {
       $('#accept-recommended-btn-handler').on('click', () => {
@@ -71,6 +54,8 @@ class TextVideo {
         }
       });
     },2500);
+=======
+>>>>>>> a0ca03a61 (SMAR-27628 - OneTrust cookie - Youtube video handling)
   }
 
 
