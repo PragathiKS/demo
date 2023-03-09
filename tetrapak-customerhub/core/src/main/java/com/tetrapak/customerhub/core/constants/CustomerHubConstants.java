20437964bcc19050a04b30e61f5568f2fea29d5d
Sreenivas <sebrahmanays@tetrapak.com>
2023-03-09 10:30:23 +0100

    Initial Commit for RK Liability Conditions - Model, Servlet, Component

--- tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/constants/CustomerHubConstants.java
+++ tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/constants/CustomerHubConstants.java
@@ -958,12 +958,6 @@ public final class CustomerHubConstants {
 
     public static final String RK_MANDATORY_KITS_TEXT = "rkMandatoryKitsText";
 
-    public static final String READ_GL_USER = "readGenericListsUser";
-
-    public static final String ENGLISH_LANGUAGE = "en";
-
-    public static final String RK_LIABILITY_CONDITIONS_DEFAULT_TITLE = "Liability Conditions";
-
 
 
     /**
--- tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/models/RKLiabilityConditionsModel.java
+++ tetrapak-customerhub/core/src/main/java/com/tetrapak/customerhub/core/models/RKLiabilityConditionsModel.java
@@ -3,10 +3,8 @@ package com.tetrapak.customerhub.core.models;
 import com.google.gson.Gson;
 import com.tetrapak.customerhub.core.constants.CustomerHubConstants;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
-import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import javax.annotation.PostConstruct;
@@ -14,18 +12,11 @@ import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.tetrapak.customerhub.core.constants.CustomerHubConstants.DOT;
-import static com.tetrapak.customerhub.core.servlets.RKLiabilityConditionsServlet.JSON_EXTENSION;
-import static com.tetrapak.customerhub.core.servlets.RKLiabilityConditionsServlet.PDF_LINKS_SELECTOR;
-
 @Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class RKLiabilityConditionsModel {
 
     @ValueMapValue
     private String rkMandatoryKitsText;
-
-    @Self
-    private Resource resource;
 
     private String i18nKeys;
 
@@ -35,11 +26,6 @@ public class RKLiabilityConditionsModel {
         i18KeyMap.put(CustomerHubConstants.RK_MANDATORY_KITS_TEXT, getRkMandatoryKitsText());
         Gson gson = new Gson();
         i18nKeys = gson.toJson(i18KeyMap);
-    }
-
-    public String getApiPath() {
-        ResourceResolver resolver = resource.getResourceResolver();
-        return resolver.map(resource.getPath()+DOT+PDF_LINKS_SELECTOR+DOT+JSON_EXTENSION);
     }
 
     public String getRkMandatoryKitsText() {
