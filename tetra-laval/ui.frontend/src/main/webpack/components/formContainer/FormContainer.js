2a629dc8f6c22c6273b686e89c820ed6bdaeca16
Hiren Parmar <hiren.parmar@digitas.com>
2023-03-21 17:43:18 +0530

    SMAR-30118 - TetraLaval - Contact Form - Submit form functionality

--- tetra-laval/ui.frontend/src/main/webpack/components/formContainer/FormContainer.js
+++ tetra-laval/ui.frontend/src/main/webpack/components/formContainer/FormContainer.js
@@ -1,24 +1,62 @@
-/* eslint-disable */
 import $ from 'jquery';
 import 'bootstrap';
 
 class FormContainer {
     cache = {};
+    resetForm(isForm) {
+        const { $form, $formThankYou, $heading, $headingText } = this.cache;
+        if(isForm) {
+            $heading.show().text($headingText);
+            $form.show();
+            $formThankYou.hide();
+        }
+        if(!isForm) {
+            $heading.text('');
+            $form.hide();
+            $formThankYou.show();
+        }
+    }
+    
+    bindEvents() {
+        const $this = this;
+        const { $contactBtn, $modal, $closeBtn, $form } = this.cache;
+        
+        $contactBtn.on('click', function() {
+            $modal.modal();
+            $this.resetForm(true);
+        });
+        $closeBtn.on('click', function() {
+            $modal.modal('hide');
+            $this.resetForm();
+        });
+
+        $form.submit(function(e) {
+            e.preventDefault();
+            const form = $(this);
+            $.ajax({
+                type: form.attr('method'),
+                url: form.attr('action'),
+                data: form.serialize()
+            }).done(function(res) {
+                console.log('Hiren Parmar | Data', res);
+                form[0].reset();
+                $this.resetForm(false);
+            }).fail(function(res) {
+                console.log('Hiren Parmar | Error Data', res);
+            });
+        });
+    }
+
     initCache() {
+        this.cache.$heading = $('.pw-lang-selector__header__heading');
+        this.cache.$headingText = this.cache.$heading.text();
+        this.cache.$form = $('.tl-contactForm').find('.cmp-form');
+        this.cache.$formThankYou = $('.tl-contactForm__thankyou');
         this.cache.$contactBtn = $('.js-tpatom-btn__tl-contactUs');
         this.cache.$modal = $('.js-tp-contact__modal');
         this.cache.$closeBtn = $('.js-close-btn');
     }
-    bindEvents() {
-        const { $contactBtn, $modal, $closeBtn } = this.cache;
-        $contactBtn.on('click', function() {
-            $modal.modal();
-        });
-        $closeBtn.on('click', function() {
-            $modal.modal('hide');
-        });
-    }
-
+    
     init() {
         this.initCache();
         this.bindEvents();
