91779bf0540a2c9ef5ea63af7cdd96a238753f14
vishalinpublicis <vishal.singh3@publicissapient.com>
2023-03-22 17:52:03 +0530

    component added

---
+++ tetrapak-supplierportal/ui.dev/src/source/templates/components/chatbotAnchor/ChatbotAnchor.spec.js
@@ -0,0 +1,9 @@
+import ChatbotAnchor from './ChatbotAnchor';
+
+describe('ChatbotAnchor', function () {
+  before(function () {
+    this.chatbotAnchor = new ChatbotAnchor({
+      el: document.body
+    });
+  });
+});
