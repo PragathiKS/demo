888d2606ea8d1eca56199d510cb130b80eb42658
vishalinpublicis <vishal.singh3@publicissapient.com>
2023-04-06 15:44:31 +0530

    code beautification done for pagination

--- tetrapak-publicweb/ui.dev/src/source/templates/components/searchresults/Searchresults.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/searchresults/Searchresults.js
@@ -6,81 +6,8 @@ import { render } from '../../../scripts/utils/render';
 import { ajaxWrapper } from '../../../scripts/utils/ajax';
 import { ajaxMethods } from '../../../scripts/utils/constants';
 import { getI18n, parseQueryString } from '../../../scripts/common/common';
+import { _paginate } from '../../../scripts/utils/paginateData';
 
-/**
- * Returns an obj containing pagination data, based on totalItems, currentPage, pageSize, maxPages
- */
-function paginate(totalItems, currentPage, pageSize, maxPages) {
-  // calculate total pages
-  const totalPages = Math.ceil(totalItems / pageSize);
-  // ensure current page isn't out of range
-  if (currentPage < 1) {
-    currentPage = 1;
-  } else if (currentPage > totalPages) {
-    currentPage = totalPages;
-  }
-
-  let startPage, endPage;
-  if (totalPages <= maxPages) {
-    // total pages less than max so show all pages
-    startPage = 1;
-    endPage = totalPages;
-  } else {
-    // total pages more than max so calculate start and end pages
-    const maxPagesBeforeCurrentPage = Math.floor(maxPages / 2);
-    const maxPagesAfterCurrentPage = Math.ceil(maxPages / 2) - 1;
-    if (currentPage <= maxPagesBeforeCurrentPage) {
-      // current page near the start
-      startPage = 1;
-      endPage = maxPages;
-    } else if (currentPage + maxPagesAfterCurrentPage >= totalPages) {
-      // current page near the end
-      startPage = totalPages - maxPages + 1;
-      endPage = totalPages;
-    } else {
-      // current page somewhere in the middle
-      startPage = currentPage - maxPagesBeforeCurrentPage;
-      endPage = currentPage + maxPagesAfterCurrentPage;
-    }
-  }
-
-  // calculate start and end item indexes
-  const startIndex = (currentPage - 1) * pageSize;
-  const endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
-
-  // create an array of pages
-  const allPages = Array.from(Array((endPage + 1) - startPage).keys()).map(i => startPage + i);
-
-  const pagesHbsArr = [];
-  allPages.forEach(page => {
-    const pageObj = {
-      isPage: true,
-      isSelected: page === currentPage,
-      pageNumber: page,
-      skip: (page - 1) * pageSize
-    };
-    pagesHbsArr.push(pageObj);
-  });
-
-  return {
-    totalItems: totalItems,
-    currentPage: currentPage,
-    pageSize: pageSize,
-    totalPages: totalPages,
-    startPage: startPage,
-    endPage: endPage,
-    startIndex: startIndex,
-    endIndex: endIndex,
-    prevDisabled: currentPage === 1,
-    nextDisabled: currentPage === totalPages,
-    prevPage: currentPage - 1,
-    nextPage: currentPage + 1,
-    prevSkip: (currentPage - 2) * pageSize,
-    nextSkip: currentPage * pageSize,
-    lastSkip: (totalPages - 1) * pageSize,
-    pages: pagesHbsArr
-  };
-}
 class Searchresults {
   constructor({ el }) {
     this.root = $(el);
@@ -543,7 +470,7 @@ class Searchresults {
       nextDisabled: currentPage >= totalPages ? true : false
     };
 
-    const paginationObj = paginate(totalPages, this.cache.activePage, this.cache.itemsPerPage, 3);
+    const paginationObj = _paginate(totalPages, this.cache.activePage, this.cache.itemsPerPage, 3);
 
     if(this.cache.searchLandingType === 'events' || this.cache.searchLandingType === 'cases' ||
       this.cache.searchLandingType === 'news'){
