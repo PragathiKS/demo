50f67e3025c82179ec3b57fa9dd97a0a96d20ea4
Hiren Parmar <hiren.parmar@digitas.com>
2023-04-17 12:50:43 +0530

    SMAR-30118 - TL - Contact US - XT changes

--- tetra-laval/ui.frontend/src/main/webpack/components/formContainer/FormContainer.js
+++ tetra-laval/ui.frontend/src/main/webpack/components/formContainer/FormContainer.js
@@ -5,7 +5,8 @@ import { logger } from 'tpPublic/scripts/utils/logger';
 class FormContainer {
   cache = {};
   resetForm(isForm) {
-    const { $form, $formThankYou, $thankYouText, $heading, $headingText } = this.cache;
+    const { $form, $formThankYou, $thankYouText, $heading, $headingText, $errorMessage } = this.cache;
+    $errorMessage.addClass('d-none');
     if (isForm) {
       $heading.show().text($headingText);
       $heading.removeClass('text-center');
@@ -22,12 +23,13 @@ class FormContainer {
 
   bindEvents() {
     const $this = this;
-    const { $contactBtn, $modal, $closeBtn, $form } = this.cache;
+    const { $contactBtn, $modal, $closeBtn, $form, $errorMessage } = this.cache;
 
     $contactBtn.on('click', function () {
       $modal.modal();
       $this.resetForm(true);
     });
+
     $closeBtn.on('click', function () {
       $modal.modal('hide');
       $this.resetForm();
@@ -36,10 +38,11 @@ class FormContainer {
     $form.submit(function (e) {
       e.preventDefault();
       const form = $(this);
+      const formData = form.serialize() +'&'+$contactBtn.attr('data-companykey')+'='+$contactBtn.attr('data-companyvalue');
       $.ajax({
         type: form.attr('method'),
         url: form.attr('action'),
-        data: form.serialize()
+        data: formData
       })
         .done(function (res) {
           form[0].reset();
@@ -47,7 +50,10 @@ class FormContainer {
           logger?.log(res);
         })
         .fail(function (res) {
-            logger?.error(res);
+          const errResponse = JSON.parse(res.responseText);
+          $errorMessage.removeClass('d-none');
+          $errorMessage.text(errResponse.statusMessage);
+          logger?.error(res);
         });
     });
   }
@@ -57,6 +63,7 @@ class FormContainer {
     this.cache.$headingText = this.cache.$heading.text();
     this.cache.$form = $('.tl-contactForm').find('.cmp-form');
     this.cache.$formThankYou = $('.tl-contactForm__thankyou');
+    this.cache.$errorMessage = $('.tl-contactForm__error');
     this.cache.$thankYouText = $('.tl-contactForm__thankyou-heading').text();
     this.cache.$contactBtn = $('.js-tpatom-btn__tl-contactUs');
     this.cache.$modal = $('.js-tp-contact__modal');
