56952cfad29d81ffc9f442a615c1ab8c8484772c
vishalinpublicis <vishal.singh3@publicissapient.com>
2023-04-19 09:36:53 +0530

    basic skeleton added for accordion

---
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/accordion/Accordion.js
@@ -0,0 +1,32 @@
+import $ from 'jquery';
+import 'bootstrap';
+import { logger } from '../../../scripts/utils/logger';
+
+class Accordion {
+  constructor({ el }) {
+    this.root = $(el);
+  }
+  cache = {};
+  initCache() {
+    /* Initialize selector cache here */
+    /**
+     * Use "this.root" to find elements within current component
+     * Example:
+     * this.cache.$submitBtn = this.root.find('.js-submit-btn');
+     */
+  }
+  bindEvents() {
+    /* Bind jQuery events here */
+    /**
+     * Example:
+     * const { $submitBtn } = this.cache;
+     * $submitBtn.on('click', () => { ... });
+     */
+  }
+  init() {
+    /* Mandatory method */
+    logger.log('Accordion Init');
+  }
+}
+
+export default Accordion;
