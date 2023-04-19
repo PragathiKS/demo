8a341c2e0bd57bca8b52c41fa47d2a019f97fdf9
vishalinpublicis <vishal.singh3@publicissapient.com>
2023-04-19 09:36:53 +0530

    basic skeleton added for accordion

---
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/accordion/Accordion.spec.js
@@ -0,0 +1,9 @@
+import Accordion from './Accordion';
+
+describe('Accordion', function () {
+  before(function () {
+    this.accordion = new Accordion({
+      el: document.body
+    });
+  });
+});
