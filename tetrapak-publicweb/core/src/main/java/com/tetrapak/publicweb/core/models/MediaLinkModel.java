c737406e3ad3a1d4f6c67536b90818b5c2e7c834
Surbhi Gupta <surbhi.gupta1@publicissapient.com>
2023-04-25 16:39:32 +0530

    SMAR-31618 AEM Commit

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/MediaLinkModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/MediaLinkModel.java
@@ -22,7 +22,7 @@ import com.tetrapak.publicweb.core.utils.LinkUtils;
 @Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class MediaLinkModel {
 
-    /** The request. */
+	/** The request. */
     @SlingObject
     private SlingHttpServletRequest request;
 
@@ -30,6 +30,10 @@ public class MediaLinkModel {
     @ValueMapValue
     private String heading;
 
+    /** The heading Description. */
+    @ValueMapValue
+    private String headingDesc;
+    
     /** The anchor id. */
     @ValueMapValue
     private String anchorId;
@@ -45,14 +49,26 @@ public class MediaLinkModel {
     /** The column one title. */
     @ValueMapValue
     private String columnOneTitle;
+    
+    /** The column one description. */
+    @ValueMapValue
+    private String columnOneDesc;
 
     /** The column two title. */
     @ValueMapValue
     private String columnTwoTitle;
 
+    /** The column two Desc. */
+    @ValueMapValue
+    private String columnTwoDesc;
+    
     /** The column three title. */
     @ValueMapValue
     private String columnThreeTitle;
+    
+    /** The column one title. */
+    @ValueMapValue
+    private String columnThreeDesc;
 
     /** The column one list. */
     @Inject
@@ -78,6 +94,42 @@ public class MediaLinkModel {
         return heading;
     }
 
+    /**
+     * Gets the headingDesc
+     * 
+     * @return headingDesc
+     */
+    public String getHeadingDesc() {
+		return headingDesc;
+	}
+
+    /**
+     * Gets the columnOneDesc
+     * 
+     * @return columnOneDesc
+     */
+	public String getColumnOneDesc() {
+		return columnOneDesc;
+	}
+
+	/**
+     * Gets the columnTwoDesc
+     * 
+     * @return columnTwoDesc
+     */
+	public String getColumnTwoDesc() {
+		return columnTwoDesc;
+	}
+
+	/**
+     * Gets the columnThreeDesc
+     * 
+     * @return columnThreeDesc
+     */
+	public String getColumnThreeDesc() {
+		return columnThreeDesc;
+	}
+	
     /**
      * Gets the anchor id.
      *
