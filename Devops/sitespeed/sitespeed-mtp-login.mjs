c79d9f28ce00d810fa4d66bc951bc4648c6df6ae
Supratim Patra <supratim.patra@tetrapak.com>
2023-04-27 08:09:13 +0530

    Set credential dynamic

--- Devops/sitespeed/sitespeed-mtp-login.mjs
+++ Devops/sitespeed/sitespeed-mtp-login.mjs
@@ -1,14 +1,17 @@
 export default async function (context, commands) {
-    await commands.navigate(
-      context.options.test.url
-    );
-  
+    const env = context.options.test.env;
+    const root_url = `'https://mypages-${env}.tetrapak.com'`; 
     try {
-      await commands.addText.byId("aemext099", "username");
-      await commands.addText.byId("T3traP@kr0ck$", "password");
-      await commands.click.byIdAndWait('signInButton', 2000); 
-      await commands.wait.bySelector('.tp-cookie-consent__btn', 2000);
-      await commands.click.bySelectorAndWait('.tp-cookie-consent__btn', 1000);
+      await commands.navigate(root_url);
+      await commands.addText.byId(context.options.uname, "username");
+      await commands.addText.byId(context.options.upass, "password");
+      await commands.click.byIdAndWait('signInButton', 2000);
+      if(env == 'dev' || env == 'qa') {
+        await commands.click.byIdAndWait('onetrust-accept-btn-handler', 1000);
+      } else  {
+        await commands.wait.bySelector('.tp-cookie-consent__btn', 2000);
+        await commands.click.bySelectorAndWait('.tp-cookie-consent__btn', 1000);
+      }
     } catch (e) {
     }
   };
