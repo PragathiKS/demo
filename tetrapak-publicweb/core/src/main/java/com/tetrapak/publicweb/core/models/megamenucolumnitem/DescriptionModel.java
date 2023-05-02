a9eccf160c8b0216e5920aaa061501ae1c7d8ff6
Sreenivas <sebrahmanays@tetrapak.com>
2023-05-02 14:29:59 +0200

    Commit of Junits

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/megamenucolumnitem/DescriptionModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/megamenucolumnitem/DescriptionModel.java
@@ -1,11 +1,12 @@
 package com.tetrapak.publicweb.core.models.megamenucolumnitem;
 
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-@Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class DescriptionModel {
 
     @ValueMapValue
