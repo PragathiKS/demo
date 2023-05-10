3003d4b4df71e49f450b210bc4d1b74241e96db7
Supratim Patra <supratim.patra@tetrapak.com>
2023-05-10 12:13:11 +0530

    fix var name

--- Devops/pa11y/index.cjs
+++ Devops/pa11y/index.cjs
@@ -16,6 +16,6 @@ if (process.argv[2] == 'pw') {
     const env = process.argv[3];
     const url_list_file_nm = process.argv[4];
     const out_dir = process.argv[5];
-    testPW(env, url_list_file_n, out_dir);
+    testPW(env, url_list_file_nm, out_dir);
 }
 
