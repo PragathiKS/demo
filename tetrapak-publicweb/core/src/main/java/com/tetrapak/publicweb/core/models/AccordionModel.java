239c3baf544b283c3d72cb271478ee23e09c0c39
Pratik Gujarathi <INGUJARATHIP@tetrapak.com>
2023-05-12 12:01:50 +0530

    Added extented accordion component

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/AccordionModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/AccordionModel.java
@@ -1,20 +1,32 @@
 package com.tetrapak.publicweb.core.models;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-
-import javax.inject.Inject;
-
+import com.adobe.cq.wcm.core.components.models.Accordion;
+import com.adobe.cq.wcm.core.components.models.ListItem;
+import com.drew.lang.annotations.NotNull;
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.models.annotations.via.ResourceSuperType;
 
-import com.tetrapak.publicweb.core.models.multifield.AccordionListModel;
+@Model(adaptables = SlingHttpServletRequest.class,
+        adapters = Accordion.class,
+        resourceType = "publicweb/components/content/accordion",
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 
-@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
-public class AccordionModel {
+public class AccordionModel implements Accordion {
+
+    @Self @Via(type = ResourceSuperType.class)
+    private Accordion accordion;
+
+    @NotNull @Override
+    public List<ListItem> getItems() {
+        return accordion.getItems();
+    }
 
     /**
      * The request.
@@ -25,20 +37,15 @@ public class AccordionModel {
     /**
      * Heading.
      */
-    @Inject
+    @ValueMapValue
     private String heading;
 
     /**
      * Description.
      */
-    @Inject
+    @ValueMapValue
     private String description;
 
-    /**
-     * Accordion List.
-     */
-    @Inject
-    private List<AccordionListModel> accordionList;
 
     /**
      * Gets the heading.
@@ -58,16 +65,5 @@ public class AccordionModel {
         return description;
     }
 
-    /**
-     * Gets the accordion List.
-     *
-     * @return lists
-     */
-    public List<AccordionListModel> getAccordionList() {
-        final List<AccordionListModel> lists = new ArrayList<>();
-        if (Objects.nonNull(accordionList)) {
-            lists.addAll(accordionList);
-        }
-        return lists;
-    }
+
 }
