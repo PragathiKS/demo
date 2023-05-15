3869ce9951db701fad329317ceb40872a3fbe39a
Kanifnath Mule <kanifnath.mule@tetrapak.com>
2023-05-15 10:25:12 +0530

    Updated menu indicator on scroll

--- tetrapak-publicweb/ui.dev/src/source/templates/components/anchormenu/AnchorMenu.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/anchormenu/AnchorMenu.js
@@ -9,51 +9,53 @@ class AnchorMenu {
   cache = {};
   initCache() {
     this.cache.$anchorLink = this.root.find('a');
-    this.cache.$anchorMenu = $('body').find('.pw-anchor-menu');
-    this.cache.$offsetTop = this.cache.$anchorMenu.offsetTop;
+    this.cache.$anchorMenu = document.getElementsByClassName('pw-anchor-menu');
+    this.cache.$progressIndicator = this.root.find('.pw-anchor-menu-progress-indicator');
+    this.cache.$anchorMenuContent = this.root.find('.pw-anchor-menu-content');
+    this.cache.$offsetTop = this.cache.$anchorMenu[0].offsetTop;
+
+    //Default selection
+    if (this.cache.$progressIndicator && this.cache.$anchorMenuContent) {
+      this.cache.$progressIndicator.css('width', `${this.cache.$anchorMenuContent[0].children[0].offsetWidth}px`);
+      this.cache.$progressIndicator.css('margin-left',`${this.cache.$anchorMenuContent[0].children[0].offsetLeft}px`);
+      this.setAnchorMenuSelection(0);
+    }
   }
   bindEvents() {
-    const { $anchorLink } = this.cache;
+    const { $anchorLink, $anchorMenu } = this.cache;
     $anchorLink.on('click', this.scrollToSection);
-    if(this.cache.$anchorMenu) {
-      // window.onscroll = function() {
-      //   this.cache.$anchorMenu.classList.add('sticky');
-      // };
-      //window.addEventListener('scroll', this.onPageScroll);
-
+    if($anchorMenu) {
       const doc = document.getElementsByClassName('anchormenu');
-      if ( doc && doc.length > 1 ) {
-        window.addEventListener('scroll', this.onPageScroll);
-      } 
+      if ( doc && doc.length > 0 ) {
+        window.addEventListener('scroll', this.onPageScroll, false);
+      }
+    }
+  }
 
-
-      // $(window).on('scroll', () => {
-      //   this.cache.$anchorMenu.classList.add('sticky');
-      // });
+  onPageScroll = () => {
+    const { $anchorMenu, $offsetTop, $anchorMenuContent } = this.cache;
+    if (window.pageYOffset >= $offsetTop) {
+      $anchorMenu[0].classList.add('sticky-anchor-menu');
+      $anchorMenu[0].classList.remove('tp-container');
+    } else {
+      $anchorMenu[0].classList.remove('sticky-anchor-menu');
+      $anchorMenu[0].classList.add('tp-container');
     }
 
-
+    //Update progress indicator
+    for(let index=0; index<$anchorMenuContent.length; index++) {
+      if (this.isElementInViewport(document.querySelector(`#${$anchorMenuContent[index].children[0].dataset.linkSection}`))) {
+        this.setAnchorMenuSelection(index);
+      } 
+    }
   }
-
-  onPageScroll() { 
-    this.cache.$anchorMenu.classList.add('sticky');
-  }
-
-  // onPageScroll = e => {
-  //   e.preventDefault();
-  //   // if (window.pageYOffset >= this.cache.$offsetTop) {
-  //   //   this.cache.$anchorMenu.classList.add('sticky');
-  //   // } else {
-  //   //   this.cache.$anchorMenu.classList.remove('sticky');
-  //   // }
-  //   this.cache.$anchorMenu.classList.add('sticky');
-  // }
 
   scrollToSection = e => {
     e.preventDefault();
     const $this = $(e.target);
     const anchorId = $this.data('link-section');
     const linkName = $this.data('link-name');
+    const currentIndex = $this.parents('li').index();
 
     const trackingObj = {
       linkType: 'internal',
@@ -71,7 +73,34 @@ class AnchorMenu {
     //end of analytics call
     location.hash = anchorId;
     scrollToElement(null,`#${anchorId}`);
+
+    //Set selection
+    this.setAnchorMenuSelection(currentIndex);
   };
+
+  setAnchorMenuSelection(currentIndex) {
+    const { $progressIndicator, $anchorMenuContent } = this.cache;
+    //Progress indicator
+    $progressIndicator.css('margin-left', `${$anchorMenuContent[currentIndex].children[0].offsetLeft}px`);
+    $progressIndicator.css('width', `${$anchorMenuContent[currentIndex].children[0].offsetWidth}px`);
+    for(let index=0; index<$anchorMenuContent.length; index++) {
+      if (currentIndex === index) {
+        $anchorMenuContent[index].children[0].classList.add('active');
+      } else {
+        $anchorMenuContent[index].children[0].classList.remove('active');
+      }
+    }
+  }
+
+  isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
 
   init() {
     /* Mandatory method */
