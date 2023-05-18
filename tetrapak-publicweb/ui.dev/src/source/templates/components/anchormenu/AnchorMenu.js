1530bfef3b0596c5cd99f196a07897d47446d213
Kanifnath Mule <kanifnath.mule@tetrapak.com>
2023-05-18 15:49:15 +0530

    Added overflow menu with selection

--- tetrapak-publicweb/ui.dev/src/source/templates/components/anchormenu/AnchorMenu.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/anchormenu/AnchorMenu.js
@@ -13,12 +13,77 @@ class AnchorMenu {
     this.cache.$progressIndicator = this.root.find('.pw-anchor-menu-progress-indicator');
     this.cache.$anchorMenuContent = this.root.find('.pw-anchor-menu-content');
     this.cache.$offsetTop = this.cache.$anchorMenu[0].offsetTop;
+    this.cache.$maxPrimaryLinks = 7;
+    this.cache.$isAnchorMenuClicked = false;
 
     //Default selection
     if (this.cache.$progressIndicator && this.cache.$anchorMenuContent) {
       this.cache.$progressIndicator.css('width', `${this.cache.$anchorMenuContent[0].children[0].offsetWidth}px`);
       this.cache.$progressIndicator.css('margin-left',`${this.cache.$anchorMenuContent[0].children[0].offsetLeft}px`);
       this.setAnchorMenuSelection(0);
+    }
+
+    if (document.querySelector('.pw-anchor-menu__regular')) {
+      const container = document.querySelector('.pw-anchor-menu__regular');
+      const primary = container.querySelector('.pw-anchor-menu-list');
+      const primaryItems = container.querySelectorAll('.pw-anchor-menu-list > li:not(.more)');
+      const allItems = container.querySelectorAll('li');
+      if(allItems.length > this.cache.$maxPrimaryLinks) {
+        primary.insertAdjacentHTML('beforeend', `
+          <li class="more">
+            <button type="button" aria-haspopup="true" aria-expanded="true"><span>⋮</span></button>
+            <ul class="secondary">
+              ${primary.innerHTML}
+            </ul>
+          </li>
+        `);
+
+        const secondary = container.querySelector('.secondary');
+        const secondaryItems = secondary.querySelectorAll('li');
+        const moreLi = primary.querySelector('.more');
+        const moreBtn = moreLi.querySelector('button');
+        moreBtn.addEventListener('click', (e) => {
+          e.preventDefault();
+          container.classList.toggle('show-secondary');
+          moreBtn.setAttribute('aria-expanded', container.classList.contains('show-secondary'));
+        });
+        const hiddenItems = [];
+        primaryItems.forEach((item, i) => {
+          if(i > this.cache.$maxPrimaryLinks - 1) {
+            item.classList.add('hiddenItem');
+            hiddenItems.push(i);
+          }
+        });
+
+        // toggle the visibility of More button and items in Secondary
+        if(!hiddenItems.length) {
+          moreLi.classList.add('hiddenItem');
+          container.classList.remove('show-secondary');
+          moreBtn.setAttribute('aria-expanded', false);
+        } else {
+          secondaryItems.forEach((item, i) => {
+            if(!hiddenItems.includes(i)) {
+              item.classList.add('hiddenItem');
+            } else {
+              item.children[0].addEventListener('click', this.scrollToSection, false);
+            }
+          });
+        }
+
+        document.addEventListener('click', (e) => {
+          let el = e.target;
+          while(el) {
+            if(el === secondary || el === moreBtn) {
+              return;
+            }
+            el = el.parentNode;
+          }
+          container.classList.remove('show-secondary');
+          moreBtn.setAttribute('aria-expanded', false);
+        });
+        //Find child after creation of secondary anchor menus
+        this.cache.$secondaryAnchorMenuContent = this.root.find('.secondary')[0].children;
+      }
     }
   }
   bindEvents() {
@@ -43,13 +108,14 @@ class AnchorMenu {
     }
 
     //Update progress indicator
-    for(let index=0; index<$anchorMenuContent.length; index++) {
-      if (this.isElementInViewport(document.querySelector(`#${$anchorMenuContent[index].children[0].dataset.linkSection}`))) {
-        this.setAnchorMenuSelection(index);
-      } 
+    if (!this.cache.$isAnchorMenuClicked) {
+      for(let index=0; index<$anchorMenuContent.length; index++) {
+        if (this.isElementInViewport(document.querySelector(`#${$anchorMenuContent[index].children[0].dataset.linkSection}`))) {
+          this.setAnchorMenuSelection(index);
+        }
+      }
     }
   }
-
   scrollToSection = e => {
     e.preventDefault();
     const $this = $(e.target);
@@ -72,22 +138,30 @@ class AnchorMenu {
 
     //end of analytics call
     location.hash = anchorId;
-    scrollToElement(null,`#${anchorId}`);
 
     //Set selection
+    this.cache.$isAnchorMenuClicked = true;
     this.setAnchorMenuSelection(currentIndex);
+    scrollToElement(()=>{
+      this.cache.$isAnchorMenuClicked = false;
+    },`#${anchorId}`);
   };
 
   setAnchorMenuSelection(currentIndex) {
-    const { $progressIndicator, $anchorMenuContent } = this.cache;
+    const { $progressIndicator, $anchorMenuContent, $maxPrimaryLinks, $secondaryAnchorMenuContent } = this.cache;
     //Progress indicator
     $progressIndicator.css('margin-left', `${$anchorMenuContent[currentIndex].children[0].offsetLeft}px`);
     $progressIndicator.css('width', `${$anchorMenuContent[currentIndex].children[0].offsetWidth}px`);
     for(let index=0; index<$anchorMenuContent.length; index++) {
-      if (currentIndex === index) {
+      //Reset active state
+      $anchorMenuContent[index].children[0].classList.remove('active');
+      if ($secondaryAnchorMenuContent) {
+        $secondaryAnchorMenuContent[index].children[0].classList.remove('active');
+      }
+      if (currentIndex >= $maxPrimaryLinks && currentIndex === index) {
+        $secondaryAnchorMenuContent[index].children[0].classList.add('active');
+      } else if (currentIndex === index) {
         $anchorMenuContent[index].children[0].classList.add('active');
-      } else {
-        $anchorMenuContent[index].children[0].classList.remove('active');
       }
     }
   }
