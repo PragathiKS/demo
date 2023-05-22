d64bdf38bb4a07e40ac2700cb122b4c00d876646
Hiren Parmar <hiren.parmar@digitas.com>
2023-05-22 16:31:09 +0530

    SMAR-32047 - UI Dev build failed issue fixed

--- tetrapak-publicweb/ui.dev/src/source/templates/components/megamenuconfigurationv2/MegaMenuConfigurationV2.spec.js
+++
@@ -1,9 +0,0 @@
-import MegaMenuConfigurationV2 from './MegaMenuConfigurationV2';
-
-describe('MegaMenuConfigurationV2', function () {
-  before(function () {
-    this.megaMenuConfigurationV2 = new MegaMenuConfigurationV2({
-      el: document.body
-    });
-  });
-});
