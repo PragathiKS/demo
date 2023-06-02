462614e8324af1d0d7b42ba87d2e7963cb544e74
Anshika Joon <ansjoon@WKMZTDF342BE.global.publicisgroupe.net>
2023-06-02 17:06:56 +0530

    created new component with the same name as the back end component

---
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/megamenuconfig/Megamenuconfig.spec.js
@@ -0,0 +1,9 @@
+import Megamenuconfig from './Megamenuconfig';
+
+describe('Megamenuconfig', function () {
+  before(function () {
+    this.megamenuconfig = new Megamenuconfig({
+      el: document.body
+    });
+  });
+});
