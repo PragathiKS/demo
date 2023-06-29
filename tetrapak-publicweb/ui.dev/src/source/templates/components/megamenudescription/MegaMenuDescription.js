9eec57154f61c2f110b5a5ca4eda8da84644ba3d
saskul <filipsaskulczycki@gmail.com>
2023-06-29 10:03:24 +0200

    remove js logic for html decoding

--- tetrapak-publicweb/ui.dev/src/source/templates/components/megamenudescription/MegaMenuDescription.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/megamenudescription/MegaMenuDescription.js
@@ -4,35 +4,9 @@ class MegaMenuDescription {
   constructor({ el }) {
     this.root = $(el);
   }
-
-  swapDescriptionStringWithHTML = () => {
-    const markupString = this.root.html();
-    this.root.html(this.htmlDecode(markupString));
-  }
-
-  htmlDecode = (input) => {
-    var doc = new DOMParser().parseFromString(input, 'text/html');
-    return doc.documentElement.textContent;
-  }
-
-  watchChanges = (cb) => {
-    const config = { childList: true, subtree: true };
-
-    const observer = new MutationObserver(cb);
-
-    observer.observe(this.root, config);
-  }
-
-  isEditMode = () => window.location.href.includes(
-    '/editor.html/content/experience-fragments/publicweb/en/solutions-mega-menu/'
-  );
-
+  
   bindEvents() {
-    if (this.isEditMode()) {
-      this.watchChanges(this.root, () => this.swapDescriptionStringWithHTML());
-    } else {
-      this.swapDescriptionStringWithHTML();
-    }
+    /* */
   }
 
   init() {
