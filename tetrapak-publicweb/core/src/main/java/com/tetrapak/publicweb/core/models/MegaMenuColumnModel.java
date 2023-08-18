05b3bd6b70002f86da8547d03d09f4c0a7b3ae3b
Sreenivas <sebrahmanays@tetrapak.com>
2023-08-18 12:18:19 +0200

    Changes for mega menu mobile

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/MegaMenuColumnModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/MegaMenuColumnModel.java
@@ -1,37 +1,72 @@
 package com.tetrapak.publicweb.core.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.tetrapak.publicweb.core.models.megamenucolumnitem.MegaMenuAccordion;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.models.factory.ModelFactory;
+import org.json.JSONException;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 @Model(
         adaptables = { Resource.class },
-        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL, resourceType = "publicweb/components/structure/megamenucolumn")
+@Exporter(name = "jackson", extensions = "json")
 public class MegaMenuColumnModel {
 
     @SlingObject
     private Resource resource;
 
+    @JsonIgnore
     private List<Resource> items;
+
+    private Map<String,Object> columnItems;
+
+    private List<MegaMenuAccordion> megaMenuAccordions;
+
+    @OSGiService
+    private ModelFactory modelFactory;
 
     @ValueMapValue
     private String bgColor;
 
     @PostConstruct
-    protected void init(){
+    protected void init() throws JSONException {
         items = new ArrayList<>();
+        columnItems = new HashMap<>();
         if(resource!=null) {
             Iterator<Resource> children = resource.listChildren();
+            megaMenuAccordions = new ArrayList<>();
+            MegaMenuAccordion megaMenuAccordion = null;
             while (children.hasNext()) {
-                items.add(children.next());
+                Resource itemResource = children.next();
+                items.add(itemResource);
+                if (itemResource.isResourceType("publicweb/components/structure/megamenucolumnitems/subheading")){
+                    megaMenuAccordion = new MegaMenuAccordion();
+                    Map<String,Object> accordionItems = new HashMap<>();
+                    accordionItems.put(itemResource.getName(),itemResource.adaptTo(modelFactory.getModelFromResource(itemResource).getClass()));
+                    megaMenuAccordion.setSubHeadings(accordionItems);
+                    megaMenuAccordions.add(megaMenuAccordion);
+                }else if(megaMenuAccordion !=null && !itemResource.isResourceType("publicweb/components/structure/megamenucolumnitems/heading")) {
+                    Map<String,Object> accordionItems = megaMenuAccordion.getSubHeadings();
+                    accordionItems.put(itemResource.getName(),itemResource.adaptTo(modelFactory.getModelFromResource(itemResource).getClass()));
+                    megaMenuAccordion.setSubHeadings(accordionItems);
+                }else{
+                    columnItems.put(itemResource.getName(),itemResource.adaptTo(modelFactory.getModelFromResource(itemResource).getClass()));
+                }
             }
+            columnItems.put("megaMenuAccordions",megaMenuAccordions);
         }
     }
 
@@ -41,5 +76,9 @@ public class MegaMenuColumnModel {
 
     public List<Resource> getItems() {
         return items;
+    }
+
+    public Map<String,Object> getColumnItems() {
+        return columnItems;
     }
 }
--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/v2/MegaMenuConfigModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/v2/MegaMenuConfigModel.java
@@ -1,22 +1,61 @@
 package com.tetrapak.publicweb.core.models.v2;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.tetrapak.publicweb.core.models.MegaMenuColumnModel;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ChildResource;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import java.util.List;
 
 /**
  * The Class MegaMenuSolutionModel.
  */
 @Model(
         adaptables = { Resource.class },
-        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,resourceType = "publicweb/components/structure/megamenuconfigv2")
+@Exporter(name = "jackson", extensions = "json")
 public class MegaMenuConfigModel {
 
     @ValueMapValue
     private int numberOfColumns;
 
+    @ChildResource
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private MegaMenuColumnModel col1;
+
+    @ChildResource
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private MegaMenuColumnModel col2;
+
+    @ChildResource
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private MegaMenuColumnModel col3;
+
+    @ChildResource
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private MegaMenuColumnModel col4;
+
     public int getNumberOfColumns() {
         return numberOfColumns;
+    }
+
+    public MegaMenuColumnModel getCol1() {
+        return col1;
+    }
+
+    public MegaMenuColumnModel getCol2() {
+        return col2;
+    }
+
+    public MegaMenuColumnModel getCol3() {
+        return col3;
+    }
+
+    public MegaMenuColumnModel getCol4() {
+        return col4;
     }
 }
