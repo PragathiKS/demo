a0a25445ff7f6de649641c6d2ae71c6fbda14336
Sreenivas <sebrahmanays@tetrapak.com>
2023-08-22 10:55:34 +0200

    Changes for mega menu mobile, sling model exporter

--- tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/megamenucolumnitem/SubheadingModel.java
+++ tetrapak-publicweb/core/src/main/java/com/tetrapak/publicweb/core/models/megamenucolumnitem/SubheadingModel.java
@@ -4,11 +4,13 @@ import com.tetrapak.publicweb.core.utils.LinkUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-@Model(adaptables = SlingHttpServletRequest.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,resourceType = "publicweb/components/structure/megamenucolumnitems/subheading")
+@Model(adaptables = {SlingHttpServletRequest.class, Resource.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,resourceType = "publicweb/components/structure/megamenucolumnitems/subheading")
+@Exporter(name = "jackson", extensions = "json")
 public class SubheadingModel {
 
     /** The request. */
