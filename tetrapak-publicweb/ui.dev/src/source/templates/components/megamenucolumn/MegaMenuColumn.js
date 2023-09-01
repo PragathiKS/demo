f4e6207c908a76ec487846a848c245e4bfcf744f
Kanifnath Mule <kanifnath.mule@tetrapak.com>
2023-09-01 13:36:34 +0530

    Fix issue of not showing market selector for mobile

--- tetrapak-publicweb/ui.dev/src/source/templates/components/megamenucolumn/MegaMenuColumn.js
+++ tetrapak-publicweb/ui.dev/src/source/templates/components/megamenucolumn/MegaMenuColumn.js
@@ -20,7 +20,7 @@ class MegaMenuColumn {
     const teaserHeader = this.cache.$elements.teaserHeader.get(0);
 
     if (!header && !teaserHeader) {
-      this.root.css('padding-top', '74px');
+      this.root.css('padding-top', '72px');
     }
   }
 
