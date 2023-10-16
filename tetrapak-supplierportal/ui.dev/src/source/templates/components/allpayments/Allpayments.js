290c2987738a137b971519f8f1f2720171cec3a2
Vinithra <vin1@WKMZT6FC14E9.global.publicisgroupe.net>
2023-10-16 18:17:43 +0530

    SP-359 Access of all payments (and sort) - Removing supplier and company from sorting list

--- tetrapak-supplierportal/ui.dev/src/source/templates/components/allpayments/Allpayments.js
+++ tetrapak-supplierportal/ui.dev/src/source/templates/components/allpayments/Allpayments.js
@@ -14,7 +14,7 @@ class AllPayments {
   initCache() {
     const config = {
       showFields: ['documentDate', 'dueCalculationBaseDate', 'companyName', 'companyCode','companyCountry', 'amountInTransactionCurrency', 'withholdingTaxAmmount', 'invoiceStatusCode', 'documentReferenceID', 'supplierName', 'supplier', 'purchasingDocuments'],
-      sortableKeys: ['documentDate', 'dueCalculationBaseDate', 'companyName', 'companyCode', 'companyCountry', 'amountInTransactionCurrency', 'documentReferenceID', 'supplierName', 'supplier'],
+      sortableKeys: ['documentDate', 'dueCalculationBaseDate', 'companyCode', 'companyCountry', 'amountInTransactionCurrency', 'documentReferenceID', 'supplier'],
       currentPageNumber: 1,
       itemsPerPage: 25,
       activePage: 1,
