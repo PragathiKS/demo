34c7c0e29cd3774f4705c0fb2a41951cb51f3a90
Mohd Danish <mohdanis1@Mohds-MacBook-Pro.local>
2023-10-19 12:41:50 +0530

    update status API integration

--- tetrapak-supplierportal/ui.dev/src/source/templates/components/paymentdetails/Paymentdetails.js
+++ tetrapak-supplierportal/ui.dev/src/source/templates/components/paymentdetails/Paymentdetails.js
@@ -24,6 +24,8 @@ class Paymentdetails {
     this.cache.header = this.root.find('.tp-payment-details__header-actions');
     this.cache.headerAction = this.root.find('.tp-payment-details__header-action-wrapper');
     this.cache.mobileHeadersActions = this.root.find('.js-mobile-header-actions');
+    this.cache.statusApiUrl = this.root.data('status-api');
+    this.cache.statusMapping = {};
     try {
       this.cache.i18nKeys = JSON.parse(this.cache.configJson);
     } catch (e) {
@@ -85,7 +87,8 @@ class Paymentdetails {
     // TODO: Need to remove count query param. these are added only for testing purpose.
     const url = `${this.cache.detailsApi}?documentreferenceid=${documentreferenceid}&count=1${dateRange}`;
     const paymentApiPromise = fetch(url, fetchHeaderOption).then(resp => resp.json());
-    return paymentApiPromise;
+    const statusApiPromise = fetch(this.cache.statusApiUrl).then(resp => resp.json());
+    return [paymentApiPromise, statusApiPromise];
   }
   showLoader = (isShow) => {
     if (isShow) {
@@ -119,14 +122,31 @@ class Paymentdetails {
     }
     this.cache.contentWrapper.removeClass('d-none');
   }
+  getStatusName = (statusCode) => {
+    const data = this.cache.statusMapping;
+
+    if (statusCode) {
+      for (const key in data) {
+        if (data[key].includes(statusCode)) {
+          return key;
+        }
+      }
+    }
+    return '';
+  };
   renderPaymentDetails = () => {
     this.showLoader(true);
     auth.getToken(({ data: authData }) => {
-      this.getApiPromise(authData)
+      Promise.all(this.getApiPromise(authData))
         .then(response => {
           this.showLoader(false);
-          const paymentData = response?.data[0] || {};
-          this.renderPaymentDetailsData(paymentData);
+          const paymentData = response[0]?.data[0] || {};
+          this.cache.statusMapping = response[1];
+          const data = {
+            ...paymentData,
+            invoiceStatusName: this.getStatusName(paymentData?.invoiceStatusCode)
+          };
+          this.renderPaymentDetailsData(data);
         })
         .catch(() => {
           this.showLoader(false);
